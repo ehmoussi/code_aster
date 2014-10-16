9190d61997ff62fdae852ae8267818987eb8d1bc
Natacha Bereux <natacha.bereux@edf.fr>
2014-10-16 14:32:18 +0200

    [#23037] Move ElementaryLoad to UnitaryLoad
    Former-commit-id: 5b49441407a40485a17138d85ccc1bca2f57d25c

--- bibcxx/Loads/UnitaryLoad.h
+++ bibcxx/Loads/UnitaryLoad.h
@@ -1,15 +1,15 @@
-#ifndef ELEMENTARYLOAD_H_
-#define ELEMENTARYLOAD_H_
+#ifndef UNITARYLOAD_H_
+#define UNITARYLOAD_H_
 
 template< class PhysicalQuantityType >
-class UnitaryLoadInstance: private AllowedElementaryLoadType< PhysicalQuantityType >
+class UnitaryLoadInstance: private AllowedUnitaryLoadType< PhysicalQuantityType >
 {
     private:
         string    _name;
         PhysicalQuantityType::valueType
 
     public:
-        ElementaryLoad(string name): _name( name )
+        UnitaryLoad(string name): _name( name )
         {}
 
         void setValue(ValueType valeur)
@@ -22,4 +22,4 @@ typedef UnitaryLoadInstance< double > DoubleLoadInstance;
 
 class LoadInstance
 
-#endif /* ELEMENTARYLOAD_H_ */
+#endif /* UNITARYLOAD_H_ */
