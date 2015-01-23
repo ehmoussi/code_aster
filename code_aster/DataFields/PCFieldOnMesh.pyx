f4327b356a162d7845564318445b492148077db0
Mathieu Courtois <mathieu.courtois@edf.fr>
2015-01-23 15:04:46 +0100

    [#23037] add PCFieldOnMeshDouble cython wrapper
    Former-commit-id: 00ce23767a1bf137fd99f1158de80041c95afa96

---
+++ code_aster/DataFields/PCFieldOnMesh.pyx
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+# Copyright (C) 1991 - 2015  EDF R&D                www.code-aster.org
+#
+# This file is part of Code_Aster.
+#
+# Code_Aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Code_Aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Code_Aster.  If not, see <http://www.gnu.org/licenses/>.
+
+from libcpp.string cimport string
+from cython.operator cimport dereference as deref
+
+from code_aster.Mesh.Mesh cimport Mesh
+
+
+cdef class PCFieldOnMeshDouble:
+    """Python wrapper on the C++ PCFieldOnMeshDouble object"""
+
+    def __cinit__( self, string name ):
+        """Initialization: stores the pointer to the C++ object"""
+        self._cptr = new cPCFieldOnMeshDouble( name )
+
+    cdef copy( self, cPCFieldOnMeshDouble& other ):
+        """Refer to an existing C++ object"""
+        self._cptr.copy( other )
+
+    def __dealloc__( self ):
+        """Destructor"""
+        if self._cptr:
+            del self._cptr
+
+    def isEmpty( self ):
+        """Tell if the object is empty"""
+        return self._cptr.isEmpty()
+
+    def setSupportMesh( self, Mesh mesh ):
+        """Set the support mesh of the model"""
+        ok = self._cptr.getInstance().setSupportMesh( deref(mesh._cptr) )
+        return ok
