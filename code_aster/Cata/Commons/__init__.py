321601833d31288ac7d1ed76d101351563bb06cb
Mathieu Courtois <mathieu.courtois@edf.fr>
2015-03-09 16:05:20 +0100

    [#23037] fix cata.py creation, import generated C_MFRONT_OFFICIAL in Commons
    Former-commit-id: 06338ee49fb612942cb10be4f15c07b21552819c

--- code_aster/Cata/Commons/__init__.py
+++ code_aster/Cata/Commons/__init__.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Code_Aster.  If not, see <http://www.gnu.org/licenses/>.
 
+from code_aster.Cata.Commons.c_mfront_official import C_MFRONT_OFFICIAL
 
 from code_aster.Cata.Commons.c_affichage import C_AFFICHAGE
 from code_aster.Cata.Commons.c_archivage import C_ARCHIVAGE
