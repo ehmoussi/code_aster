ac86eddc1134c6b8bd74c397f2b2967c476f7234
Nicolas Sellenet <nicolas.sellenet at edf.fr>
2015-07-17 14:26:57 +0200

    [#23037] Change in iterator over TimeStepper
    Former-commit-id: 5f148962ae0ec8f83ba5b023a66b2d6c92a12371

--- bibcxx/Algorithms/GenericAlgorithm.h
+++ bibcxx/Algorithms/GenericAlgorithm.h
@@ -57,6 +57,7 @@ class Algorithm
             {
                 try
                 {
+                    algo.prepareStep();
                     algo.oneStep();
                 }
                 catch( AlgoException& exc )
