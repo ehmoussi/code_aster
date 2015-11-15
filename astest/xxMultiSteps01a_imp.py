f839cf9a8ae2eb0e5889e0ab2da74720aaa18efc
Mathieu Courtois <mathieu.courtois@edf.fr>
2015-11-15 12:18:28 +0100

    [#23037] improve pickling by getting the context from the caller
    Former-commit-id: 8c78f0b7dcb38f1ee748ebc0cb7578da9c739329

---
+++ astest/xxMultiSteps01a_imp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from math import pi
+import numpy as np
+
+import code_aster
+
+fcos = code_aster.Function()
+fcos.setParameterName("INST")
+fcos.setResultName("TEMP")
+
+# values assignment
+n = 10
+valx = np.arange( n ) * 2. * pi / n
+valy = np.cos( valx )
+
+fcos.setValues(valx, valy)
