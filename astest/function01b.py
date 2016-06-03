4a82d5250a9f5cb1684792bc85158f82b2530f75
Mathieu Courtois <mathieu.courtois@edf.fr>
2016-06-03 11:29:51 +0200

    [#23037] add testcase in function01b
    Former-commit-id: 8a774bed1c6945e3621a6a4de38d5ced2eba23f8

--- astest/function01b.py
+++ astest/function01b.py
@@ -7,51 +7,53 @@ import code_aster
 from code_aster.Commands import *
 
 
+test = code_aster.TestCase()
+
 tps = DEFI_LIST_REEL(VALE=(0,0.5,1,),)
-assert tps.size == 3
+test.assertEqual( tps.size, 3 )
 
 tps = DEFI_LIST_REEL(DEBUT=0,
                      INTERVALLE=_F(JUSQU_A=3,
                                    NOMBRE=3,),)
-assert tps.size == 4
+test.assertEqual( tps.size, 4 )
 
 tps = DEFI_LIST_REEL(DEBUT=0.0,
                      INTERVALLE=_F(JUSQU_A=1.0,
                                    NOMBRE=10,),)
-assert tps.size == 11
-assert tps.max() == 1.0
+test.assertEqual( tps.size, 11 )
+test.assertAlmostEqual( tps.max(), 1.0 )
 
 
 tps = DEFI_LIST_REEL(DEBUT=0.0,
                      INTERVALLE=_F(JUSQU_A=10.0,
                                    PAS=0.7,),)
-assert tps.size == 16
-assert tps.max() == 10.0
+test.assertEqual( tps.size, 16 )
+test.assertAlmostEqual( tps.max(), 10.0 )
 
 tps = DEFI_LIST_REEL(DEBUT=0.0,
                      INTERVALLE=_F(JUSQU_A=10.0,
                                    PAS=0.9999,),)
-assert tps.size == 12
-assert tps.max() == 10.0
+test.assertEqual( tps.size, 12 )
+test.assertAlmostEqual( tps.max(), 10.0 )
 
 # check last step
 tps = DEFI_LIST_REEL(DEBUT=0.0,
                      INTERVALLE=_F(JUSQU_A=10.0,
                                    PAS=0.999999,),)
-assert tps.size == 11
-assert tps.max() == 10.0
+test.assertEqual( tps.size, 11 )
+test.assertAlmostEqual( tps.max(), 10.0 )
 
 tps = DEFI_LIST_REEL(DEBUT=0.0,
                      INTERVALLE=_F(JUSQU_A=10.0,
                                    PAS=1.01),)
-assert tps.size == 11
-assert tps.max() == 10.0
+test.assertEqual( tps.size, 11 )
+test.assertAlmostEqual( tps.max(), 10.0 )
 
 tps = DEFI_LIST_REEL(DEBUT=0.0,
                      INTERVALLE=_F(JUSQU_A=10.0,
                                    PAS=1.000001),)
-assert tps.size == 11
-assert tps.max() == 10.0
+test.assertEqual( tps.size, 11 )
+test.assertAlmostEqual( tps.max(), 10.0 )
 
 # values assignment
 n = 10
@@ -79,17 +81,14 @@ fsin = DEFI_FONCTION(NOM_PARA="INST",
                      VALE=values,
                      INTERPOL=("LIN", "LOG"),)
 
-try:
+with test.assertRaisesRegexp(RuntimeError, "length.*must be equal"):
     fsin = DEFI_FONCTION(NOM_PARA="INST",
                          NOM_RESU="TEMP",
                          ABSCISSE=valx,
                          ORDONNEE=np.arange( n - 1. ),
                          INTERPOL=("LIN", "LOG"),)
-    raise AssertionError("should have failed!")
-except RuntimeError:
-    pass
 
-try:
+with test.assertRaisesRegexp(TypeError, "str"):
     bad_type = valy.tolist()
     bad_type[5] = "a"
     fsin = DEFI_FONCTION(NOM_PARA="INST",
@@ -97,6 +96,5 @@ try:
                          ABSCISSE=valx,
                          ORDONNEE=bad_type,
                          INTERPOL=("LIN", "LOG"),)
-    raise AssertionError("should have failed!")
-except TypeError:
-    pass
+
+test.printSummary()
