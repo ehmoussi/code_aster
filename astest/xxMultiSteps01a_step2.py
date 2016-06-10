c6f2e523e04d839fe509d0200e22701b53d96a90
Mathieu Courtois <mathieu.courtois@edf.fr>
2016-06-10 09:15:38 +0200

    [#23037] fix xxMultiSteps01a testcase
    Former-commit-id: 18b718e8862f5aab1b173406b2615b1d34efa187

--- astest/xxMultiSteps01a_step2.py
+++ astest/xxMultiSteps01a_step2.py
@@ -8,8 +8,8 @@ import code_aster
 
 test = code_aster.TestCase()
 
-test.assertIsTrue( type(fsin) is code_aster.Function )
-test.assertIsTrue( type(fcos) is code_aster.Function )
+test.assertTrue( type(fsin) is code_aster.Function )
+test.assertTrue( type(fcos) is code_aster.Function )
 
 fsin.debugPrint( 6 )
 test.assertEqual( fsin.size(), 10 )
