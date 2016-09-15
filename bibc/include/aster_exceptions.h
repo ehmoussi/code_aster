2b9f982f04d0f5a6d237420444f585756c0d2e34
Mathieu Courtois <mathieu.courtois@edf.fr>
2016-09-15 17:39:35 +0200

    [#23037] Remove compilation warning.
    Former-commit-id: 746de64d4b7f986c6fcf3462ac54440da6830954

--- bibc/include/aster_exceptions.h
+++ bibc/include/aster_exceptions.h
@@ -23,6 +23,7 @@
 #include "aster.h"
 #include "asterc_debug.h"
 #include "aster_module.h"
+#include "RunManager/Exceptions.h"
 
 #define FatalError 18   /* kept for backward compatibility only */
 #define EOFError   19
