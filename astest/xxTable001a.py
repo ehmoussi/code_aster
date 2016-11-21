7f3f1102de97b51a66caf13b75c2a208280288fb
Nicolas Sellenet <nicolas.sellenet at edf.fr>
2016-11-21 15:18:58 +0100

    [#25769] Add of a Table testcase
    Former-commit-id: 400aace8466984b88d4c199cd8fde89c9ba050f7

---
+++ astest/xxTable001a.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+import code_aster
+from code_aster.Commands import *
+
+table1 = [0, 1, 2,]
+table2 = ["00", "01", "02",]
+
+TAB=CREA_TABLE(LISTE=(_F(PARA='NUMERO',
+                         LISTE_I=table1,),
+                      _F(PARA='NOM_NUM',
+                         LISTE_K=table2,
+                         TYPE_K='K24',),),)
+
+TAB.debugPrint()
