cb2c80ba09d23b5f18517a80850df7371c1d6739
Nicolas Sellenet <nicolas.sellenet at edf.fr>
2017-04-19 09:23:53 +0200

    [#26137] add of libAsterFortran
    Former-commit-id: a7478ff57b4380fde5d7c50fe699a14ae7e114a8

--- code_aster/Discretization/DOFNumberingBuilder.py
+++ code_aster/Discretization/DOFNumberingBuilder.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Code_Aster.  If not, see <http://www.gnu.org/licenses/>.
 
-from code_aster.Supervis.libtest import python_execop
+from code_aster.Supervis.libAsterFortran import python_execop
 from code_aster.Supervis.libCommandSyntax import CommandSyntax
 from code_aster import DOFNumbering
 
