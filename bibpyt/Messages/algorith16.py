4199fbbc5a824773007ab388caa8837f6f2c0df2
Mickael Abbas <mickael.abbas@edf.fr>
2017-06-11 16:18:40 +0200

    [#26573] No more read final saturation in thmrcp
    Former-commit-id: d801e213a4fedda90610af8f43a1945c09047e8f

--- bibpyt/Messages/algorith16.py
+++ bibpyt/Messages/algorith16.py
@@ -290,10 +290,6 @@ Risques & conseils :
    On passe outre car VERI_PAS = NON
   """),
 
-    95 : _(u"""
-  Van Genuchten non autorisé pour ce modèle de couplage
- """),
-
     96 : _(u"""
   Comportement pour le zirconium: la dérivée est nulle.
 """),
