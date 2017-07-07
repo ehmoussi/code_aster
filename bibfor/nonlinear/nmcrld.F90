ab8c1c19d12d2a762fc4c113b2d91c5e6343ab88
Mickael Abbas <mickael.abbas@edf.fr>
2017-07-07 10:25:03 +0200

    [#25928] Simplification of automatic stop error defining
    Former-commit-id: 62917a510ce7a4c88ffd1d90abf033668c093c70

--- bibfor/nonlinear/nmcrld.F90
+++ bibfor/nonlinear/nmcrld.F90
@@ -23,7 +23,6 @@ implicit none
 !
 #include "asterf_types.h"
 #include "event_def.h"
-#include "asterfort/dfllsv.h"
 #include "asterfort/jedema.h"
 #include "asterfort/jemarq.h"
 #include "asterfort/utdidt.h"
@@ -43,24 +42,13 @@ character(len=19) :: sddisc
 !
 ! --------------------------------------------------------------------------------------------------
 !
-    integer :: i_fail_save, nb_fail
-    character(len=24) :: sddisc_evenr
-    real(kind=8), pointer :: v_sddisc_evenr(:) => null()
-    character(len=24) :: sddisc_evenk
-    character(len=16), pointer :: v_sddisc_evenk(:) => null()
-    character(len=24) :: sddisc_subdr
-    real(kind=8), pointer :: v_sddisc_subdr(:) => null()
-    character(len=24) :: sddisc_infor
-    real(kind=8), pointer :: v_sddisc_infor(:) => null()
-    character(len=16) :: event_type, action_type
-    real(kind=8) :: dtmin, vale_ref
-    character(len=16) :: nom_cham, nom_cmp,crit_cmp
-    real(kind=8) :: pene_maxi, resi_glob_maxi
-    character(len=16) :: subd_methode
-    real(kind=8) :: subd_pas_mini
-    integer :: subd_niveau, subd_pas
-    character(len=16) :: subd_auto
-    real(kind=8) :: subd_inst, subd_duree, pcent_iter_plus, coef_maxi
+    integer :: nb_fail
+    character(len=24) :: sddisc_eevenr
+    real(kind=8), pointer :: v_sddisc_eevenr(:) => null()
+    character(len=24) :: sddisc_eevenk
+    character(len=16), pointer :: v_sddisc_eevenk(:) => null()
+    character(len=24) :: sddisc_esubdr
+    real(kind=8), pointer :: v_sddisc_esubdr(:) => null()
 !
 ! --------------------------------------------------------------------------------------------------
 !
@@ -68,50 +56,17 @@ character(len=19) :: sddisc
 !
 ! - Initializations
 !
-    nb_fail     = 2
-    i_fail_save = 1
+    nb_fail     = 1
     call utdidt('E', sddisc, 'LIST', 'NECHEC', vali_ = nb_fail)
-    call utdidt('L', sddisc, 'LIST', 'DTMIN' , valr_ = dtmin)
 !
 ! - Create datastructure
 !
-    sddisc_infor = sddisc(1:19)//'.LINF'
-    sddisc_evenr = sddisc(1:19)//'.EEVR'
-    sddisc_evenk = sddisc(1:19)//'.EEVK'
-    sddisc_subdr = sddisc(1:19)//'.ESUR'
-    call wkvect(sddisc_evenr, 'V V R'  , nb_fail*SIZE_LEEVR, vr   = v_sddisc_evenr)
-    call wkvect(sddisc_evenk, 'V V K16', nb_fail*SIZE_LEEVK, vk16 = v_sddisc_evenk)
-    call wkvect(sddisc_subdr, 'V V R'  , nb_fail*SIZE_LESUR, vr   = v_sddisc_subdr)
-!
-! - Create reference error
-!
-    event_type      = 'ERREUR'
-    action_type     = 'ARRET'
-    subd_methode    = ' '
-    subd_auto       = ' '
-    subd_pas_mini   = 0.d0
-    subd_pas        = 0
-    subd_niveau     = 0
-    pcent_iter_plus = 0.d0
-    coef_maxi       = 0.d0
-    subd_inst       = 0.d0
-    subd_duree      = 0.d0
-    pene_maxi       = 0.d0
-    crit_cmp        = ' '
-    vale_ref        = 0.d0
-    nom_cham        = ' '
-    nom_cmp         = ' '
-    resi_glob_maxi  = 0.d0
-!
-! - Create reference error
-!
-    call dfllsv(v_sddisc_infor, v_sddisc_evenr, v_sddisc_evenk, v_sddisc_subdr,&
-                i_fail_save   ,&
-                event_type    , vale_ref    , nom_cham        , nom_cmp       ,&
-                crit_cmp      , pene_maxi   , resi_glob_maxi  ,&
-                action_type   , subd_methode, subd_auto       , subd_pas_mini ,&
-                subd_pas      , subd_niveau , pcent_iter_plus , coef_maxi     ,&
-                subd_inst     , subd_duree)
+    sddisc_eevenr = sddisc(1:19)//'.EEVR'
+    sddisc_eevenk = sddisc(1:19)//'.EEVK'
+    sddisc_esubdr = sddisc(1:19)//'.ESUR'
+    call wkvect(sddisc_eevenr, 'V V R'  , nb_fail*SIZE_LEEVR, vr   = v_sddisc_eevenr)
+    call wkvect(sddisc_eevenk, 'V V K16', nb_fail*SIZE_LEEVK, vk16 = v_sddisc_eevenk)
+    call wkvect(sddisc_esubdr, 'V V R'  , nb_fail*SIZE_LESUR, vr   = v_sddisc_esubdr)
 !
     call jedema()
 !
