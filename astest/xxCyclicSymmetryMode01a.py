36e6455f133b77e6f98c0fd4a70cbdc77de39688
Natacha Bereux <natacha.bereux@edf.fr>
2017-07-18 16:56:33 +0200

    [#26712] Add tests CyclicSymmetryMode01*
    Former-commit-id: a56d2a8eaec2b2f036f1667dc890e6a53aad5973

---
+++ astest/xxCyclicSymmetryMode01a.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+# coding: utf-8
+
+# Cas-test issu de sdlv134a
+# Validation de la sous-structuration cyclique
+
+import code_aster
+from code_aster.Commands import *
+
+
+test = code_aster.TestCase()
+
+secteur = code_aster.Mesh.create()
+secteur.readMedFile("sdlv134a.mmed")
+
+modele = code_aster.Model.create()
+modele.setSupportMesh(secteur)
+modele.addModelingOnAllMesh(code_aster.Physics.Mechanics,
+                           code_aster.Modelings.Tridimensional)
+modele.build()
+
+Young = 2.E11
+Poisson = 0.3
+Rho = 7800.0
+
+materElas = code_aster.ElasMaterialBehaviour.create()
+materElas.setDoubleValue("E", Young)
+materElas.setDoubleValue("Nu", Poisson)
+materElas.setDoubleValue("Rho", Rho)
+
+acier = code_aster.Material.create()
+acier.addMaterialBehaviour(materElas)
+acier.build()
+
+affeMat = code_aster.MaterialOnMesh.create()
+affeMat.setSupportMesh(secteur)
+affeMat.addMaterialOnAllMesh(acier)
+affeMat.build()
+
+charCine = code_aster.KinematicsLoad.create()
+charCine.setSupportModel(modele)
+charCine.addImposedMechanicalDOFOnNodes(code_aster.PhysicalQuantityComponent.Dx, 
+                                        0., ["ENCAS","AXE","Droite","Gauche"])
+charCine.addImposedMechanicalDOFOnNodes(code_aster.PhysicalQuantityComponent.Dy, 
+                                        0., ["ENCAS","AXE","Droite","Gauche"])
+charCine.addImposedMechanicalDOFOnNodes(code_aster.PhysicalQuantityComponent.Dz, 
+                                        0., ["ENCAS","AXE","Droite","Gauche"])
+charCine.build()
+
+study = code_aster.StudyDescription.create(modele, affeMat)
+dProblem = code_aster.DiscreteProblem.create(study)
+rigiElem = dProblem.computeMechanicalRigidityMatrix()
+massElem = dProblem.computeMechanicalMassMatrix()
+
+numeDDL = code_aster.DOFNumbering.create()
+numeDDL.setElementaryMatrix(rigiElem)
+numeDDL.computeNumerotation()
+
+rigiAsse = code_aster.AssemblyMatrixDouble.create()
+rigiAsse.setElementaryMatrix(rigiElem)
+rigiAsse.setDOFNumbering(numeDDL)
+rigiAsse.addKinematicsLoad(charCine)
+rigiAsse.build()
+
+massAsse = code_aster.AssemblyMatrixDouble.create()
+massAsse.setElementaryMatrix(massElem)
+massAsse.setDOFNumbering(numeDDL)
+massAsse.addKinematicsLoad(charCine)
+massAsse.build()
+
+structInterf = code_aster.StructureInterface.create()
+structInterf.addInterface( "axe_interf", code_aster.InterfaceType.CraigBampton,
+                       ["AXE"] )
+structInterf.addInterface( "droite_interf", code_aster.InterfaceType.CraigBampton,
+                       ["Droite"] )
+structInterf.addInterface( "Gauche_interf", code_aster.InterfaceType.CraigBampton,
+                       ["Gauche"] )
+#structInterf.build()
+
