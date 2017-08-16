9014211235e1ab0ffaf0cba937681d9c14f42ee5
Mickael Abbas <mickael.abbas@edf.fr>
2017-08-16 08:41:15 +0200

    [#26570] Add *HH2 behaviour in RELATION
    Former-commit-id: 8ff35b84847295b43c0537b57ca62bd654ca0766

---
+++ bibpyt/Comportement/kit_hh2m.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+# --------------------------------------------------------------------
+# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# This file is part of code_aster.
+#
+# code_aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# code_aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------
+
+
+from cata_comportement import LoiComportement
+
+loi = LoiComportement(
+    nom            = 'KIT_HH2M',
+    doc            =   """KIT associé au comportement des milieux poreux (modélisations thermo-hydro-mécanique).""",
+    num_lc         = 0,
+    nb_vari        = 0,
+    nom_vari       = None,
+    mc_mater       = None,
+    modelisation   = ('D_PLAN_HH2M_SI','D_PLAN_HH2MD','D_PLAN_HH2MS','AXIS_HH2MD','AXIS_HH2MS','3D_HH2MD',
+        '3D_HH2MS','3D_HH2M_SI',),
+    deformation    = ('PETIT',),
+    algo_inte      = ('SANS_OBJET',),
+    type_matr_tang = None,
+    proprietes     = None,
+    syme_matr_tang = ('Yes',),
+    exte_vari      = None,
+)
---
+++ bibpyt/Comportement/kit_thh2m.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+# --------------------------------------------------------------------
+# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# This file is part of code_aster.
+#
+# code_aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# code_aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------
+
+
+from cata_comportement import LoiComportement
+
+loi = LoiComportement(
+    nom            = 'KIT_THH2M',
+    doc            =   """KIT associé au comportement des milieux poreux (modélisations thermo-hydro-mécanique).
+   Pour plus de détails sur les modélisations thermo-hydro-mécaniques et les modèles de comportement,
+   """          ,
+    num_lc         = 0,
+    nb_vari        = 0,
+    nom_vari       = None,
+    mc_mater       = None,
+    modelisation   = ('D_PLAN_THH2MD','AXIS_THH2MD','3D_THH2MD','D_PLAN_THH2MS','AXIS_THH2MS','3D_THH2MS',
+        ),
+    deformation    = ('PETIT',),
+    algo_inte      = ('SANS_OBJET',),
+    type_matr_tang = None,
+    proprietes     = None,
+    syme_matr_tang = ('Yes',),
+    exte_vari      = None,
+)
