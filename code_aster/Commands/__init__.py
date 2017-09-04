ea19d0a2b00f7b4825ef9109a7a72acb60cb4759
Mathieu Courtois <mathieu.courtois@edf.fr>
2017-09-04 17:44:46 +0200

    [#25434] Use new language catalog in code_aster executions.
    Former-commit-id: 262c8904ba220955731ffaaeca5b8b1480d3dba6

--- code_aster/Commands/__init__.py
+++ code_aster/Commands/__init__.py
@@ -1,16 +1,5 @@
 # code_aster.Commands compatibility package
 
-__all__ = ['_F',             'AFFE_CARA_ELEM', 'AFFE_CHAR_CINE', 'AFFE_CHAR_MECA', 'AFFE_MATERIAU',
-           'AFFE_MODELE',    'ASSE_MATRICE',   'ASSE_MATR_GENE', 'ASSE_VECT_GENE', 'CALC_MATR_ELEM',
-           'COMB_FOURIER',   'CREA_CHAMP',     'CREA_RESU',      'CREA_TABLE',     'DEFI_COMPOR',
-           'DEFI_FISS_XFEM', 'DEFI_FLUI_STRU', 'DEFI_FONC_FLUI', 'DEFI_FONCTION',  'DEFI_FOND_FISS',
-           'DEFI_GEOM_FIBRE','DEFI_GRILLE',    'DEFI_GROUP',     'DEFI_INTE_SPEC', 'DEFI_INTERF_DYNA',
-           'DEFI_LIST_REEL', 'DEFI_MATERIAU',  'DEFI_MODELE_GENE','DEFI_NAPPE',    'DEFI_SQUELETTE',
-           'DEFI_SPEC_TURB', 'LIRE_MAILLAGE',  'MACR_ELEM_DYNA', 'MACR_ELEM_STAT', 'MECA_STATIQUE',
-           'MODE_STATIQUE',  'MODI_MAILLAGE',  'NUME_DDL',       'NUME_DDL_GENE',  'PROJ_CHAMP',
-           'DEBUT', 'FIN', 'STAT_NON_LINE'
-          ]
-
 from code_aster.Cata.Language.Syntax import _F
 
 # please keep alphabetical order
@@ -37,6 +26,7 @@ from code_aster.Commands.defi_geom_fibre import DEFI_GEOM_FIBRE
 from code_aster.Commands.defi_grille import DEFI_GRILLE
 from code_aster.Commands.defi_group import DEFI_GROUP
 from code_aster.Commands.defi_inte_spec import DEFI_INTE_SPEC
+from code_aster.Commands.defi_interf_dyna import DEFI_INTERF_DYNA
 from code_aster.Commands.defi_list_reel import DEFI_LIST_REEL
 from code_aster.Commands.defi_materiau import DEFI_MATERIAU
 from code_aster.Commands.defi_modele_gene import DEFI_MODELE_GENE
@@ -48,13 +38,12 @@ from code_aster.Commands.lire_maillage import LIRE_MAILLAGE
 from code_aster.Commands.macr_elem_dyna import MACR_ELEM_DYNA
 from code_aster.Commands.macr_elem_stat import MACR_ELEM_STAT
 from code_aster.Commands.meca_statique import MECA_STATIQUE
+from code_aster.Commands.mode_iter_cycl import MODE_ITER_CYCL
 from code_aster.Commands.mode_statique import MODE_STATIQUE
 from code_aster.Commands.modi_maillage import MODI_MAILLAGE
 from code_aster.Commands.nume_ddl import NUME_DDL
 from code_aster.Commands.nume_ddl_gene import NUME_DDL_GENE
 from code_aster.Commands.proj_champ import PROJ_CHAMP
-from code_aster.Commands.mode_iter_cycl import MODE_ITER_CYCL
-from code_aster.Commands.defi_interf_dyna import DEFI_INTERF_DYNA
 from code_aster.Commands.stat_non_line import STAT_NON_LINE
 
 from code_aster.Commands.macro_commands import (
