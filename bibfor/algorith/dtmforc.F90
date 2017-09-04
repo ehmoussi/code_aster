9ddd69c594b2c5be7b4c0b0fbc3173fee25c4911
Ayaovi Dzifa Kudawoo <ayaovi-dzifa.kudawoo@edf.fr>
2017-09-04 11:42:51 +0200

    [#26716] Correction of DYNA_VIBRA performance in sequential
    Former-commit-id: 27dabcf151c7037edb665b92ed21852216d6f207

--- bibfor/algorith/dtmforc.F90
+++ bibfor/algorith/dtmforc.F90
@@ -38,6 +38,7 @@ subroutine dtmforc(sd_dtm_, sd_int_, index, buffdtm, buffint, nlaccnt)
 #include "asterfort/dtmforc_rotf.h"
 #include "asterfort/dtmforc_yacs.h"
 #include "asterfort/dtmforc_lub.h"
+#include "asterfort/dtmforc_calcnoli.h"
 #include "asterfort/dtmget.h"
 #include "asterfort/dtmsav.h"
 #include "asterfort/intget.h"
@@ -57,7 +58,6 @@ subroutine dtmforc(sd_dtm_, sd_int_, index, buffdtm, buffint, nlaccnt)
 #include "asterfort/vecini.h"
 #include "asterfort/sdmpic.h"
 #include "asterfort/wkvect.h"
-
 !
 #include "asterc/asmpi_comm.h"
 #include "asterfort/asmpi_info.h"
@@ -97,8 +97,8 @@ subroutine dtmforc(sd_dtm_, sd_int_, index, buffdtm, buffint, nlaccnt)
     real(kind=8)    , pointer :: fext(:)    => null()
     real(kind=8)    , pointer :: fext_tmp(:)    => null()
     real(kind=8)    , pointer :: phi(:)     => null()
-    real(kind=8)    , pointer :: fext_nl(:) => null()
-    real(kind=8)    , pointer :: fext_tgt(:)=> null()
+!    real(kind=8)    , pointer :: fext_nl(:) => null()
+!    real(kind=8)    , pointer :: fext_tgt(:)=> null()
     real(kind=8)    , pointer :: fadd_nl(:) => null()
     real(kind=8)    , pointer :: coefm(:)   => null()
     character(len=8), pointer :: nomfon(:)  => null()
@@ -192,117 +192,52 @@ subroutine dtmforc(sd_dtm_, sd_int_, index, buffdtm, buffint, nlaccnt)
         call dtmget(sd_dtm, _SD_NONL  , kscal=sd_nl, buffer=buffdtm)
         call dtmget(sd_dtm, _NL_BUFFER, vi=buffnl  , buffer=buffdtm)
 
-        call nlget (sd_nl,  _F_TOT_WK , vr=fext_nl , buffer=buffnl)
-        call nlget (sd_nl,  _F_TAN_WK , vr=fext_tgt, buffer=buffnl)
+!        call nlget (sd_nl,  _F_TOT_WK , vr=fext_nl , buffer=buffnl)
+!        call nlget (sd_nl,  _F_TAN_WK , vr=fext_tgt, buffer=buffnl)
+!        call nlget(sd_nl, _FEXT_MPI, vr=fext_tmp,savejv=sd_fextnl_tmp)
+!        call vecini(nbmode, 0.d0, fext_tmp)
+!        write (6,*) "OBJET JEVEUX MPI" , sd_fextnl_tmp
 
-!
-! ----- Mpi informations
-!
-    one_proc = .false.
-    call asmpi_comm('GET', mpicou)
-    call asmpi_info(mpicou,rank=i_proc , size=nb_proc)    
-    if(nb_proc .eq. 1 .or. nbnli .le. nb_proc) one_proc = .true.
-!
-! ----- MPI initialisation
-! ----- Temporary non linear force 
+        !
+        ! ----- Mpi informations
+        !
+            one_proc = .false.
+            call asmpi_comm('GET', mpicou)
+            call asmpi_info(mpicou,rank=i_proc , size=nb_proc)    
+            if(nb_proc .eq. 1 .or. nbnli .le. nb_proc) one_proc = .true.
 
-    call nlinivec(sd_nl,  _FEXT_MPI , nbmode, vr=fext_tmp)
-    call nlget(sd_nl, _FEXT_MPI, savejv=sd_fextnl_tmp)    
-    call jeexin(sd_fextnl_tmp(1:20)//'.MPI',iret_mpi)
-    
-    if (iret_mpi .eq. 0) then
-        call wkvect(sd_fextnl_tmp(1:20)//'.MPI','V V K16',1,vk16=valk)
-    else 
-        call jeveuo(sd_fextnl_tmp(1:20)//'.MPI', 'E',vk16=valk)
-    endif   
-    
-    if (one_proc) then 
-        valk(1)='MPI_COMPLET'
-        fext_tmp = fext
-        idx_start=1
-        idx_end  =nbnli
-    else
-        valk(1)='MPI_INCOMPLET'
-        nb_nbnli_mpi  = int(nbnli/nb_proc)
-        nbr_nbnli_mpi = nbnli-nb_nbnli_mpi*nb_proc
-        idx_start   = 1+(i_proc)*nb_nbnli_mpi
-        idx_end     = idx_start+nb_nbnli_mpi-1+(nbr_nbnli_mpi*int((i_proc+1)/nb_proc))
-    endif
-    
-        
-        
-        do inl  = idx_start, idx_end
-            call nlget(sd_nl, _NL_TYPE, iocc=inl, iscal=nl_type, buffer=buffnl)
-
-            select case (nl_type)
-!
-                case(NL_CHOC)
-                    call dtmforc_choc(inl, sd_dtm, sd_nl, buffdtm, buffnl,&
-                                      temps, depl, vite, fext_nl, fext_tgt)
-
-!                   --- Special case of choc nonlinearities that can be
-!                       implicitely treated
-                    if ((nlcase.eq.0).or.(nlacc.eq.1)) then
-                        do i=1, nbmode
-                            fext_tmp(i) = fext_tmp(i) + fext_nl(i)
-                        end do
-                    else
-                        do i=1, nbmode
-                            fext_tmp(i) = fext_tmp(i) + fext_tgt(i)
-                        end do
-                    end if
-    
-                case(NL_BUCKLING)
-                    call dtmforc_flam(inl, sd_dtm, sd_nl, buffdtm, buffnl,&
-                                      temps, depl, vite, fext_tmp)
-
-                case(NL_ANTI_SISMIC)
-                    call dtmforc_ants(inl, sd_dtm, sd_nl, buffdtm, buffnl,&
-                                      temps, depl, vite, fext_tmp)
-
-                case(NL_DIS_VISC)
-                    call dtmforc_dvis(inl, sd_dtm, sd_nl, buffdtm, buffnl,&
-                                      temps, dt, depl, vite, fext_tmp)
-
-                case(NL_DIS_ECRO_TRAC)
-                    call dtmforc_decr(inl, sd_dtm, sd_nl, buffdtm, buffnl,&
-                                      temps, dt, depl, vite, fext_tmp)
-
-                case(NL_CRACKED_ROTOR)
-                    call dtmforc_rotf(inl,sd_dtm, sd_nl, buffdtm, buffnl,&
-                                      temps, depl, fext_tmp)
-    ! !
-                ! TODO: change the name to NL_YACS
-                case(NL_LUBRICATION)
-                     ! we will deal with it later
-                     continue
-!
-                case(NL_YACS)
-                    ! we will deal with it later
-                    continue
-    ! 
-                case(NL_FX_RELATIONSHIP)
-                    call dtmforc_rede(inl, sd_dtm, sd_nl, buffdtm, buffnl,&
-                                      depl, fext_tmp)
-    ! ! 
-                case(NL_FV_RELATIONSHIP)
-                    call dtmforc_revi(inl, sd_dtm, sd_nl, buffdtm, buffnl,&
-                                      vite, fext_tmp)
-    !
-                case default
-                    ASSERT(.false.)
-    !
-            end select
-        end do
-!
-! - All-reduce _NL_FEXT_MPI
-!
-    call sdmpic('&&OP29NL',sd_fextnl_tmp)
-    if (one_proc) then
-        fext = fext_tmp
-    else
-        fext = fext + fext_tmp
-    endif
+            if (one_proc) then 
+                idx_start=1
+                idx_end  =nbnli
+                call dtmforc_calcnoli(sd_dtm, sd_nl, buffdtm, buffnl,&
+                                      temps,dt, depl, vite, fext,&
+                                      nbmode,nlacc,nlcase,idx_start,idx_end)
+            else 
+            ! ----- MPI initialisation
+            ! ----- Temporary non linear force 
+                call nlget(sd_nl, _FEXT_MPI, savejv=sd_fextnl_tmp,vr=fext_tmp)
+                call vecini(nbmode, 0.d0, fext_tmp)
+                call jeexin(sd_fextnl_tmp(1:20)//'.MPI',iret_mpi)
+                
+                if (iret_mpi .eq. 0) then
+                    call wkvect(sd_fextnl_tmp(1:20)//'.MPI','V V K16',1,vk16=valk)
+                else 
+                    call jeveuo(sd_fextnl_tmp(1:20)//'.MPI', 'E',vk16=valk)
+                endif   
+                valk(1)='MPI_INCOMPLET'
+                nb_nbnli_mpi  = int(nbnli/nb_proc)
+                nbr_nbnli_mpi = nbnli-nb_nbnli_mpi*nb_proc
+                idx_start   = 1+(i_proc)*nb_nbnli_mpi
+                idx_end     = idx_start+nb_nbnli_mpi-1+(nbr_nbnli_mpi*int((i_proc+1)/nb_proc))
+                call dtmforc_calcnoli(sd_dtm, sd_nl, buffdtm, buffnl,&
+                                      temps,dt, depl, vite, fext_tmp,&
+                                      nbmode,nlacc,nlcase,idx_start,idx_end)
+            !
+            ! - All-reduce _NL_FEXT_MPI
+            !
+                call sdmpic('&&OP29NL',sd_fextnl_tmp)
+                fext = fext + fext_tmp
+            endif
 
         ! we take care of everything
 !        print *, "calling dtmforc_lub"
