10e5c316f92ca2c8f56fd2291e15ee085ca41fb0
Mathieu Courtois <mathieu.courtois@edf.fr>
2017-09-14 11:39:57 +0200

    [#25434] Extend unittest for Table.
    Former-commit-id: 39bd9c3776af8218adbaa2b067c663f68b720e48

--- astest/table01a.py
+++ astest/table01a.py
@@ -10,8 +10,18 @@ tab = CREA_TABLE(LISTE=_F(PARA='X', LISTE_I=(1, 2, 3)),
 
 test.assertEqual(tab['X', 2], 2)
 
+with test.assertRaises(RuntimeError):
+    tab[8]
+
+with test.assertRaises(KeyError):
+    tab['U', 55]
+
+test.assertEqual(tab.get_nom_para(), ['X'])
+
 title = tab.TITRE()
-print "DEBUG:", repr(title)
 test.assertEqual(tab.TITRE().strip(), 'test title')
+
+pytab = tab.EXTR_TABLE()
+test.assertEqual(len(pytab), 3)
 
 test.printSummary()
