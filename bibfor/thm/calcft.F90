db73fa0576088a1a4d1b60b1d91a2d7f88bc3644
Micka?l Abbas <mickael.abbas@edf.fr>
2017-10-06 15:57:40 +0200

    [#26833] Refactoring thermic flux computation
    Former-commit-id: 22a14b87515dc00de77bc138141a4391f3bd47d9

--- bibfor/thm/calcft.F90
+++ bibfor/thm/calcft.F90
@@ -18,16 +18,17 @@
 ! aslint: disable=W1504,W1306
 ! person_in_charge: sylvie.granet at edf.fr
 !
-subroutine calcft(option, ndim, dimdef,&
-                  dimcon, addete,&
-                  addeme, addep1, addep2, adcote, congep,&
-                  dsde, t, grat, phi, pvp,&
-                  tbiot, sat, dsatp1, lambp,&
-                  dlambp, lambs, dlambs, tlambt, tdlamt,&
-                  tlamct, rho11, h11, h12,&
-                  angmas)
-
-
+subroutine calcft(option, angl_naut,&
+                  ndim  , dimdef   , dimcon,&
+                  adcote, &
+                  addeme, addete   , addep1, addep2,&
+                  temp  , grad_temp,&
+                  tbiot ,&
+                  phi   , rho11    , satur_, dsatur_,&
+                  pvp   , h11      , h12   ,&
+                  lambs , dlambs   , lambp , dlambp,&
+                  tlambt, tlamct   , tdlamt,&
+                  congep, dsde)
 !
 use THM_type
 use THM_module
@@ -35,172 +36,211 @@ use THM_module
 implicit none
 !
 #include "asterfort/dilata.h"
-#include "asterfort/matini.h"
 #include "asterfort/unsmfi.h"
 #include "asterfort/THM_type.h"
-! ======================================================================
-! ======================================================================
-
-! ROUTINE CALC_FLUX_THERM ----------------------------------------------
-! CALCULE LES CONTRAINTES GENERALISEES ET LA MATRICE TANGENTE DES FLUX -
-! ======================================================================
-    integer :: ndim, dimdef, dimcon
-    integer :: addete, addeme, addep1, addep2, adcote
-    real(kind=8) :: congep(1:dimcon)
-    real(kind=8) :: dsde(1:dimcon, 1:dimdef)
+!
+character(len=16), intent(in) :: option
+real(kind=8), intent(in) :: angl_naut(3)
+integer, intent(in) :: ndim, dimdef, dimcon
+integer, intent(in) :: adcote
+integer, intent(in) :: addeme, addete, addep1, addep2
+real(kind=8), intent(in) :: temp, grad_temp(3)
+real(kind=8), intent(in) :: tbiot(6)
+real(kind=8), intent(in) :: phi, rho11, satur_, dsatur_
+real(kind=8), intent(in) :: pvp, h11, h12
+real(kind=8), intent(in) :: lambs, dlambs
+real(kind=8), intent(in) :: lambp, dlambp
+real(kind=8), intent(in) :: tlambt(ndim, ndim)
+real(kind=8), intent(in) :: tlamct(ndim, ndim)
+real(kind=8), intent(in) :: tdlamt(ndim, ndim)
+real(kind=8), intent(inout) :: congep(1:dimcon), dsde(1:dimcon, 1:dimdef)
+!
+! --------------------------------------------------------------------------------------------------
+!
+! THM
+!
+! Compute flux and stress for thermic
+!
+! --------------------------------------------------------------------------------------------------
+!
+! In  option           : name of option- to compute
+! In  angl_naut        : nautical angles
+! In  ndim             : dimension of space (2 or 3)
+! In  dimdef           : dimension of generalized strains vector
+! In  dimcon           : dimension of generalized stresses vector
+! In  adcote           : adress of thermic components in generalized stresses vector
+! In  addeme           : adress of mechanic components in generalized strains vector
+! In  addete           : adress of thermic components in generalized strains vector
+! In  addep1           : adress of capillary pressure in generalized strains vector
+! In  addep2           : adress of gaz pressure in generalized strains vector
+! In  temp             : temperature - At end of current step
+! In  grad_temp        : gradient of temperature
+! In  tbiot            : Biot tensor
+! In  phi              : porosity
+! In  rho11            : volumic mass for liquid
+! In  satur            : saturation
+! In  dsatur           : derivative of saturation (/pc)
+! In  pvp              : steam pressure
+! In  h11              : enthalpy of liquid
+! In  h12              : enthalpy of steam
+! In  lambs            : thermal conductivity depending on saturation
+! In  dlambs           : derivative of thermal conductivity depending on saturation
+! In  lambp            : thermal conductivity depending on porosity
+! In  dlambp           : derivative of thermal conductivity depending on porosity
+! In  tlambt           : tensor of thermal conductivity
+! In  tlamct           : tensor of thermal conductivity (constant part)
+! In  tdlamt           : tensor of derivatives for thermal conductivity
+! IO  congep           : generalized stresses - At end of current step
+! IO  dsde             : derivative matrix
+!
+! --------------------------------------------------------------------------------------------------
+!
+    integer :: nume_thmc
     integer :: i, j, k
-    real(kind=8) :: t, grat(3), phi, sat, dsatp1, pvp
-    real(kind=8) :: rgaz, tbiot(6), biot(3, 3)
-    real(kind=8) :: lambp, dlambp
-    real(kind=8) :: lambs, dlambs
-    real(kind=8) :: rho11, h11, h12, rho12
-    real(kind=8) :: angmas(3)
-    real(kind=8) :: tlambt(ndim, ndim), tlamct(ndim, ndim)
-    real(kind=8) :: tdlamt(ndim, ndim)
+    real(kind=8) :: biot(3, 3)
+    real(kind=8) :: rgaz, rho12, mamolv
     real(kind=8) :: alphfi, cs, kron(ndim, ndim)
     real(kind=8) :: lamdt1(ndim, ndim), lamdt2(ndim, ndim)
     real(kind=8) :: lamdt3(ndim, ndim)
-    real(kind=8) :: lamdt4(ndim, ndim), lamdt5(ndim, ndim), mamolv
-    character(len=16) :: option
-    integer :: nume_thmc
-! =====================================================================
-! --- DEFINITION DU SYMBOLE DE KRONECKER ------------------------------
-! =====================================================================
-    do i = 1, ndim
-        do j = 1, ndim
-            kron(i,j) = 0.d0
-        end do
-    end do
+    real(kind=8) :: lamdt4(ndim, ndim), lamdt5(ndim, ndim)
+    real(kind=8) :: satur, dsatur
+!
+! --------------------------------------------------------------------------------------------------
+!
+    kron(1:ndim,1:ndim) = 0.d0
     do i = 1, ndim
         kron(i,i) = 1.d0
     end do
+    lamdt1(1:ndim, 1:ndim) = 0.d0
+    lamdt2(1:ndim, 1:ndim) = 0.d0
+    lamdt3(1:ndim, 1:ndim) = 0.d0
+    lamdt4(1:ndim, 1:ndim) = 0.d0
+    lamdt5(1:ndim, 1:ndim) = 0.d0
 !
 ! - Get storage parameters for behaviours
 !
     nume_thmc = ds_thm%ds_behaviour%nume_thmc
 !
+! - Get parameters
+!
     rgaz   = ds_thm%ds_material%solid%r_gaz
+    mamolv = ds_thm%ds_material%steam%mass_mol
 !
-    call matini(ndim, ndim, 0.d0, lamdt1)
-    call matini(ndim, ndim, 0.d0, lamdt2)
-    call matini(ndim, ndim, 0.d0, lamdt3)
-    call matini(ndim, ndim, 0.d0, lamdt4)
-    call matini(ndim, ndim, 0.d0, lamdt5)
+! - Change biot tensor with thermic
 !
-! =====================================================================
-! --- REDEFINITION DU TENSEUR DE BIOT ---------------------------------
-! =====================================================================
-    biot(1,1)=tbiot(1)
-    biot(2,2)=tbiot(2)
-    biot(3,3)=tbiot(3)
-    biot(1,2)=tbiot(4)
-    biot(1,3)=tbiot(5)
-    biot(2,3)=tbiot(6)
-    biot(2,1)=biot(1,2)
-    biot(3,1)=biot(1,3)
-    biot(3,2)=biot(2,3)
-! =====================================================================
-! --- RECUPERATION DES COEFFICIENTS MECANIQUES ALPHAFI ET CS-----------
-! =====================================================================
+    biot(1,1) = tbiot(1)
+    biot(2,2) = tbiot(2)
+    biot(3,3) = tbiot(3)
+    biot(1,2) = tbiot(4)
+    biot(1,3) = tbiot(5)
+    biot(2,3) = tbiot(6)
+    biot(2,1) = biot(1,2)
+    biot(3,1) = biot(1,3)
+    biot(3,2) = biot(2,3)
+!
     if (ds_thm%ds_elem%l_dof_meca) then
-        call dilata(angmas, phi, tbiot, alphfi)
+        call dilata(angl_naut, phi, tbiot, alphfi)
         call unsmfi(phi, tbiot, cs)
     else
-! =====================================================================
-! --- EN ABSENCE DE MECA ALPHA0 = 0 et 1/KS = 0 -----------------------
-! =====================================================================
         alphfi = 0.d0
         cs = 0.d0
     endif
+!
+! - Set saturation
+!
     if (nume_thmc .eq. GAZ) then
-        sat = 0.d0
-        dsatp1 = 0.d0
+        satur  = 0.d0
+        dsatur = 0.d0
     else if (nume_thmc .eq. LIQU_SATU) then
-        sat = 1.d0
-        dsatp1 = 0.d0
+        satur  = 1.d0
+        dsatur = 0.d0
+    else
+        satur  = satur_
+        dsatur = dsatur_
     endif
-! =====================================================================
-!           LAMDT1 : LAMBDA
-!           LAMDT2 : DLAMB / DEPSV
-!           LAMDT3 : DLAMB / DP1
-!           LAMDT4 : DLAMB / DP2
-!           LAMDT5 : DLAMB / DT
-! =====================================================================
+!
+! - Compute tensor of thermic conductivity
+!
     if (nume_thmc .eq. LIQU_VAPE) then
-        mamolv = ds_thm%ds_material%steam%mass_mol
-        rho12=mamolv*pvp/rgaz/t
+        rho12  = mamolv*pvp/rgaz/temp
         do i = 1, ndim
             do j = 1, ndim
-                lamdt1(i,j) =lamdt1(i,j)+lambs*lambp*tlambt(i,j)&
-                + tlamct(i,j)
-                lamdt2(i,j) =lamdt2(i,j)+(biot(i,j)-phi*kron(i,j))&
-                *tlambt(j,i)*dlambp*lambs
-                lamdt3(i,j) =lamdt3(i,j)+(rho12/rho11-1.d0)*lambp&
-                *dlambs*tlambt(i,j)*dsatp1 +cs*(sat+(1.d0-sat)*rho12/&
-                rho11)* dlambp*lambs*tlambt(i,j)
+                lamdt1(i,j) = lamdt1(i,j)+&
+                              lambs*lambp*tlambt(i,j)+&
+                              tlamct(i,j)
+                lamdt2(i,j) = lamdt2(i,j)+&
+                              (biot(i,j)-phi*kron(i,j))*tlambt(j,i)*dlambp*lambs
+                lamdt3(i,j) = lamdt3(i,j)+&
+                              (rho12/rho11-1.d0)*lambp*dlambs*tlambt(i,j)*dsatur +&
+                              cs*(satur+(1.d0-satur)*rho12/rho11)*dlambp*lambs*tlambt(i,j)
                 lamdt4(i,j) = 0.d0
-                lamdt5(i,j) =lamdt5(i,j)+lambs*lambp*tdlamt(i,j)&
-                +(-3.d0*alphfi+cs*(1.d0-sat)* rho12*(h12-h11)/t)*&
-                dlambp*lambs* tlambt(i,j) +lambp*dlambs*tlambt(i,j)*&
-                dsatp1*rho12* (h12-h11)/t
+                lamdt5(i,j) = lamdt5(i,j)+&
+                              lambs*lambp*tdlamt(i,j)+&
+                              (-3.d0*alphfi+cs*(1.d0-satur)*rho12*(h12-h11)/temp)*&
+                               dlambp*lambs*tlambt(i,j) +&
+                              lambp*dlambs*tlambt(i,j)*dsatur*rho12*(h12-h11)/temp
             end do
         end do
     else
         do i = 1, ndim
             do j = 1, ndim
-                lamdt1(i,j) =lamdt1(i,j)+lambs*lambp*tlambt(i,j)&
-                + tlamct(i,j)
-                lamdt2(i,j) =lamdt2(i,j)+(biot(i,j)-phi*kron(i,j))&
-                *tlambt(j,i)*dlambp*lambs
-                lamdt3(i,j) =lamdt3(i,j)+lambp*dlambs*tlambt(i,j)*&
-                dsatp1 -sat*cs*dlambp*lambs*tlambt(i,j)
-                lamdt4(i,j) =lamdt4(i,j)+ cs*dlambp*lambs*tlambt(i,j)
-                lamdt5(i,j) =lamdt5(i,j)+ lambs*lambp*tdlamt(i,j)&
-                -3.d0*alphfi*dlambp*lambs*tlambt(i,j)
+                lamdt1(i,j) = lamdt1(i,j)+&
+                              lambs*lambp*tlambt(i,j)+&
+                              tlamct(i,j)
+                lamdt2(i,j) = lamdt2(i,j)+&
+                              (biot(i,j)-phi*kron(i,j))*tlambt(j,i)*dlambp*lambs
+                lamdt3(i,j) = lamdt3(i,j)+&
+                              lambp*dlambs*tlambt(i,j)*dsatur-&
+                              satur*cs*dlambp*lambs*tlambt(i,j)
+                lamdt4(i,j) = lamdt4(i,j)+&
+                              cs*dlambp*lambs*tlambt(i,j)
+                lamdt5(i,j) = lamdt5(i,j)+&
+                              lambs*lambp*tdlamt(i,j)-&
+                              3.d0*alphfi*dlambp*lambs*tlambt(i,j)
             end do
         end do
     endif
 !
-! =====================================================================
-! --- CALCUL DU FLUX THERMIQUE ----------------------------------------
-! =====================================================================
+! - Compute matrix
+!
     if ((option(1:9).eq.'RIGI_MECA') .or. (option(1:9).eq.'FULL_MECA')) then
         do i = 1, ndim
             do j = 1, ndim
-                dsde(adcote+i,addete+j)=dsde(adcote+i,addete+j)-&
-                lamdt1(i,j)
-                dsde(adcote+i,addete)=dsde(adcote+i,addete)- lamdt5(i,&
-                j)*grat(j)
+                dsde(adcote+i,addete+j) = dsde(adcote+i,addete+j)-lamdt1(i,j)
+                dsde(adcote+i,addete)   = dsde(adcote+i,addete)- lamdt5(i,j)*grad_temp(j)
             end do
             if (ds_thm%ds_elem%l_dof_meca) then
                 do j = 1, 6
                     do k = 1, ndim
-                        dsde(adcote+i,addeme+ndim-1+j)= dsde(adcote+i,&
-                        addeme+ndim-1+j)-lamdt2(i,k)*grat(k)
+                        dsde(adcote+i,addeme+ndim-1+j) = dsde(adcote+i,addeme+ndim-1+j)-&
+                                                         lamdt2(i,k)*grad_temp(k)
                     end do
                 end do
             endif
             if (ds_thm%ds_elem%l_dof_pre1) then
                 do j = 1, ndim
-                    dsde(adcote+i,addep1)=dsde(adcote+i,addep1)&
-                    - lamdt3(i,j)*grat(j)
+                    dsde(adcote+i,addep1) = dsde(adcote+i,addep1)-&
+                                            lamdt3(i,j)*grad_temp(j)
                 end do
                 if (ds_thm%ds_elem%l_dof_pre2) then
                     do j = 1, ndim
-                        dsde(adcote+i,addep2)=dsde(adcote+i,addep2)&
-                        -lamdt4(i,j)*grat(j)
+                        dsde(adcote+i,addep2) = dsde(adcote+i,addep2)-&
+                                                lamdt4(i,j)*grad_temp(j)
                     end do
                 endif
             endif
         end do
     endif
+!
+! - Compute generalized stress
+!
     if ((option(1:9).eq.'RAPH_MECA') .or. (option(1:9).eq.'FULL_MECA')) then
         do i = 1, ndim
-            congep(adcote+i)=0.d0
+            congep(adcote+i) = 0.d0
             do j = 1, ndim
-                congep(adcote+i)=congep(adcote+i)-lamdt1(i,j)*grat(j)
+                congep(adcote+i) = congep(adcote+i)-lamdt1(i,j)*grad_temp(j)
             end do
         end do
     endif
-! =====================================================================
+!
 end subroutine
