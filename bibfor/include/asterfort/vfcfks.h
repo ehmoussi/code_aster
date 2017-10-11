5ce775a4fe03a714616ebfd4a29ea18e7bd66436
Micka?l Abbas <mickael.abbas@edf.fr>
2017-10-11 14:12:49 +0200

    [#26979] Compute volumic flux
    Former-commit-id: db5bbaf967264d759c106add3c6a1b26dc3aa7a7

--- bibfor/include/asterfort/vfcfks.h
+++ bibfor/include/asterfort/vfcfks.h
@@ -15,38 +15,24 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
-!
 !
 #include "asterf_types.h"
 !
 interface
-    subroutine vfcfks(cont, tange, maxfa, nface, uk,&
-                      dukp1, dukp2, ufa, dufa1, dufa2,&
-                      c, pesa, rho, drho1, drho2,&
-                      xk, xfa, maxdim, ndim, fks,&
-                      dfks1, dfks2)
-        integer :: ndim
-        integer :: maxdim
-        integer :: nface
-        integer :: maxfa
-        aster_logical :: cont
-        aster_logical :: tange
-        real(kind=8) :: uk
-        real(kind=8) :: dukp1
-        real(kind=8) :: dukp2
-        real(kind=8) :: ufa(1:nface)
-        real(kind=8) :: dufa1(1:nface)
-        real(kind=8) :: dufa2(1:nface)
-        real(kind=8) :: c(1:maxfa, 1:nface)
-        real(kind=8) :: pesa(ndim)
-        real(kind=8) :: rho
-        real(kind=8) :: drho1
-        real(kind=8) :: drho2
-        real(kind=8) :: xk(ndim)
-        real(kind=8) :: xfa(1:maxdim, 1:nface)
-        real(kind=8) :: fks(nface)
-        real(kind=8) :: dfks1(1+maxfa, nface)
-        real(kind=8) :: dfks2(1+maxfa, nface)
+    subroutine vfcfks(l_matr, maxfa  , ndim , nface,&
+                      uk    , dukp1  , dukp2,&
+                      ufa   , dufa1  , dufa2,&
+                      c     , gravity,&
+                      rho   , drho1  , drho2,&
+                      xk    , xface, &
+                      fks   , dfks1, dfks2)
+        aster_logical, intent(in) :: l_matr
+        integer, intent(in) :: maxfa, nface, ndim
+        real(kind=8), intent(in) :: uk, dukp1, dukp2
+        real(kind=8), intent(in) :: ufa(1:nface), dufa1(1:nface), dufa2(1:nface)
+        real(kind=8), intent(in) :: c(1:maxfa, 1:nface), gravity(ndim)
+        real(kind=8), intent(in) :: rho, drho1, drho2
+        real(kind=8), intent(in) :: xk(ndim), xface(1:3, 1:nface)
+        real(kind=8), intent(out) :: fks(nface), dfks1(1+maxfa, nface), dfks2(1+maxfa, nface)
     end subroutine vfcfks
 end interface
