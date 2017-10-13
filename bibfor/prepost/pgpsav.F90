cf6814dbac21106c36ce1102195df81ecd9bf83d
Maximilien Siavelis <maximilien.siavelis@necs.fr>
2017-10-13 17:33:14 +0200

    [#22281] multiple absolute accelero in POST_GENE_PHY
    Former-commit-id: 30da68ab830a1b96f7ec5310a44ef66c7413d8f9

--- bibfor/prepost/pgpsav.F90
+++ bibfor/prepost/pgpsav.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -110,7 +110,7 @@ subroutine pgpsav(sd_pgp, param, lonvec, iobs, kscal,&
     data  parind  / -1, -1, -1, -1, -1,&
                     -1, -1,  1,  1,  2,&
                      1,  2,  2,  2,  2,&
-                     1,  1,  2,  2,&
+                     1,  2,  2,  2,&
                      2,  2,  2,  2,  2/
 !
     call jemarq()
--- code_aster/Cata/Commands/post_gene_phys.py
+++ code_aster/Cata/Commands/post_gene_phys.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -64,8 +64,8 @@ POST_GENE_PHYS  = OPER( nom="POST_GENE_PHYS",op=  58,sd_prod=table_sdaster,
 
                       b_acce_abs      = BLOC(condition = """(equal_to("NOM_CHAM", 'ACCE_ABSOLU'))""",
                                              regles    = (PRESENT_PRESENT('ACCE_MONO_APPUI','DIRECTION'),),
-                      ACCE_MONO_APPUI = SIMP(statut = 'f', typ=(fonction_sdaster,nappe_sdaster,formule)),
-                      DIRECTION       = SIMP(statut = 'f', typ='R', min=3, max = 3 ),),),
+                      ACCE_MONO_APPUI = SIMP(statut = 'f', typ=(fonction_sdaster,nappe_sdaster,formule), max = 3),
+                      DIRECTION       = SIMP(statut = 'f', typ='R', min=3, max = 9 ),),),
 
                   TITRE       = SIMP(statut = 'f', typ='TXM',),
 )  ;
