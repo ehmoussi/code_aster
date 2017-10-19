b811c54aae49ef97260f656ba8d4a115d6c9f436
Mathieu Courtois <mathieu.courtois@edf.fr>
2017-10-19 18:15:17 +0200

    [#25434] Rewritting of MODE_STATIQUE Command based on ExecuteCommand.
    Former-commit-id: 023ecc579d5516851588ac3e7ca517169c992bb2

--- code_aster/Commands/__init__.py
+++ code_aster/Commands/__init__.py
@@ -72,7 +72,7 @@ from .lire_maillage import LIRE_MAILLAGE
 # from .macr_elem_stat import MACR_ELEM_STAT
 from .meca_statique import MECA_STATIQUE
 # from .mode_iter_cycl import MODE_ITER_CYCL
-# from .mode_statique import MODE_STATIQUE
+from .mode_statique import MODE_STATIQUE
 # from .modi_maillage import MODI_MAILLAGE
 # from .nume_ddl import NUME_DDL
 # from .nume_ddl_gene import NUME_DDL_GENE
