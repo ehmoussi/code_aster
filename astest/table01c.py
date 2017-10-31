e2df660cc882e65b07558c71b907a83b76a34cb8
Mathieu Courtois <mathieu.courtois@edf.fr>
2017-10-31 18:40:04 +0100

    [#25434] Make LIRE_TABLE macro-command work.
    Former-commit-id: 2183fc64d032bd8d0948017ea8415044ced1562a

---
+++ astest/table01c.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+import code_aster
+from code_aster.Commands import *
+
+code_aster.init()
+
+test = code_aster.TestCase()
+
+table = LIRE_TABLE(UNITE=31, FORMAT='TABLEAU')
+
+pytab = table.EXTR_TABLE()
+test.assertSequenceEqual(pytab.GAMMA.values(), [None, 1.5, 2.5, None, 3.5])
+
+test.printSummary()
