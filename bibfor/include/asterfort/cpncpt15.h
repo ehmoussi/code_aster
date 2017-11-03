7990d97f68b7f296cac0b0bca8ad2102965354d7
Guillaume Drouet <guillaume.drouet@edf.fr>
2017-11-03 11:40:35 +0100

    [#25432] Add management of PENTA15 for LAC method
    Former-commit-id: 9247de4148d70140b237d712c1b1480f0ac111d6

---
+++ bibfor/include/asterfort/cpncpt15.h
@@ -0,0 +1,30 @@
+! --------------------------------------------------------------------
+! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! This file is part of code_aster.
+!
+! code_aster is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! code_aster is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+! --------------------------------------------------------------------
+
+!
+!
+interface
+    subroutine cpncpt15(main,numa,coor,ind,nomnoe, conneo)
+        integer, intent(in) :: ind
+        integer, intent(in) :: numa
+        real(kind=8),intent(out) :: coor(3, *)
+        character(len=8), intent(in) :: main
+        character(len=24), intent(in) :: nomnoe
+        integer, intent(in) :: conneo(*)
+    end subroutine cpncpt15
+end interface
