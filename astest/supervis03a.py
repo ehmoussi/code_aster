edef9fcdfe6f54d6b4885f48179559ec2354f0a4
Mathieu Courtois <mathieu.courtois@edf.fr>
2017-11-06 18:32:47 +0100

    [#25434] Add 'supervis03a' testcase for additional results.
    Former-commit-id: 8deb19990116b9db0c79a98dd14656a9d703d7a5

---
+++ astest/supervis03a.py
@@ -0,0 +1,109 @@
+# coding=utf-8
+# --------------------------------------------------------------------
+# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# This file is part of code_aster.
+#
+# code_aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# code_aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------
+
+import code_aster
+from code_aster.Commands import *
+from code_aster.Supervis.ExecutionParameter import ExecutionParameter
+
+test = code_aster.TestCase()
+
+# extract from zzzz351a
+DEBUT(CODE=_F(NIV_PUB_WEB='INTERNET'), DEBUG=_F(SDVERI='OUI'),
+      )
+
+params = ExecutionParameter()
+
+test.assertEqual(params.get_option("use_legacy_mode"), 1)
+params.set_option("use_legacy_mode", 0)
+test.assertEqual(params.get_option("use_legacy_mode"), 0)
+
+MAIL=LIRE_MAILLAGE(UNITE=20, FORMAT='MED',);
+
+MODELE=AFFE_MODELE(MAILLAGE=MAIL,
+                   AFFE=_F(TOUT='OUI', PHENOMENE='MECANIQUE', MODELISATION='3D',),);
+
+MAT=DEFI_MATERIAU(ELAS=_F(E=200.e9, NU=0.3, RHO=8000.0,),);
+
+CHMAT=AFFE_MATERIAU(MAILLAGE=MAIL,
+                    AFFE=_F(TOUT='OUI', MATER=MAT,),);
+
+BLOCAGE=AFFE_CHAR_MECA(MODELE=MODELE,
+                       DDL_IMPO=_F(GROUP_MA='BLOK',
+                                   DX=0.0,
+                                   DY=0.0,
+                                   DZ=0.0,),);
+
+asse = ASSEMBLAGE(MODELE=MODELE,
+                  CHAM_MATER=CHMAT,
+                  CHARGE=BLOCAGE,
+                  NUME_DDL=CO('NUMEDDL'),
+                  MATR_ASSE=(_F(MATRICE=CO('K1'),
+                                OPTION='RIGI_MECA',),
+                             _F(MATRICE=CO('M1'),
+                                OPTION='MASS_MECA',),),);
+test.assertEqual(len(asse), 4)
+test.assertIsNone(asse.main)
+
+NUMEDDL = asse.NUMEDDL
+K1 = asse.K1
+M1 = asse.M1
+
+# 1. Calcul de reference avec les matrices "completes" :
+#--------------------------------------------------------
+if False :
+    MODE1=CALC_MODES( OPTION='BANDE',
+                      MATR_RIGI=K1,
+                      MATR_MASS=M1,
+                      CALC_FREQ=_F( FREQ=(10., 250.) ),
+                      VERI_MODE=_F( SEUIL=1e-03 )
+                     )
+
+    TEST_RESU(RESU=_F(RESULTAT=MODE1, NUME_MODE=2, PARA='FREQ', VALE_CALC= 85.631015163879, ))
+
+
+# 2. Calcul avec les matrices reduites :
+#--------------------------------------------------------
+if False :
+    K2=ELIM_LAGR(MATR_RIGI=K1)
+    M2=ELIM_LAGR(MATR_ASSE=M1, MATR_RIGI=K1)
+
+#   2.1 avec MULT_FRONT :
+    MODE2=CALC_MODES( OPTION='BANDE',
+                      MATR_RIGI=K2,
+                      MATR_MASS=M2,
+                      CALC_FREQ=_F( FREQ=(10., 250.) ),
+                      VERI_MODE=_F( SEUIL=1e-03 ),
+                      SOLVEUR=_F(METHODE='MULT_FRONT'),
+                     )
+
+    TEST_RESU(RESU=_F(RESULTAT=MODE2, NUME_MODE=2, PARA='FREQ', VALE_CALC= 85.631015163879, ))
+
+#   2.2 avec MUMPS :
+    MODE3=CALC_MODES( OPTION='BANDE',
+                      MATR_RIGI=K2,
+                      MATR_MASS=M2,
+                      CALC_FREQ=_F( FREQ=(10., 250.) ),
+                      VERI_MODE=_F( SEUIL=1e-03 ),
+                      SOLVEUR=_F(METHODE='MUMPS'),
+                     )
+
+    TEST_RESU(RESU=_F(RESULTAT=MODE3, NUME_MODE=2, PARA='FREQ', VALE_CALC= 85.631015163879, ))
+
+
+FIN();
