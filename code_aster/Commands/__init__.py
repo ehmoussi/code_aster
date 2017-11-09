8c403e4f2e455cd8028a4378ef19aae846dec516
Mathieu Courtois <mathieu.courtois@edf.fr>
2017-11-09 23:21:40 +0100

    [#27123] Better writing of POURSUITE / init("--continue").
    Former-commit-id: 3d8fe2c53a0442f6ce6c87c0cd2c14ce21dce197

--- code_aster/Commands/__init__.py
+++ code_aster/Commands/__init__.py
@@ -47,7 +47,7 @@ from .crea_champ import CREA_CHAMP
 # from .crea_maillage import CREA_MAILLAGE
 from .crea_resu import CREA_RESU
 from .crea_table import CREA_TABLE
-from .debut import DEBUT
+from .debut import DEBUT, POURSUITE
 from .defi_compor import DEFI_COMPOR
 from .defi_constante import DEFI_CONSTANTE
 from .defi_fichier import DEFI_FICHIER
