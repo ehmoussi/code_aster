f4f9cdbad97c2d501c35cb2680578a22c23ba7aa
Nicolas Sellenet <nicolas.sellenet at edf.fr>
2017-11-17 14:59:12 +0100

    [#27081] modifications of xxParallelLinearMechanics001 b and c
    Former-commit-id: aa736a6f8c725586dadfc4de2cc1e1d3481a8383

--- astest/xxParallelLinearMechanics001b.py
+++ astest/xxParallelLinearMechanics001b.py
@@ -9,10 +9,10 @@ code_aster.init()
 test = code_aster.TestCase()
 
 rank = code_aster.getMPIRank()
-pMesh = code_aster.ParallelMesh.create()
+pMesh = code_aster.ParallelMesh()
 pMesh.readMedFile("xxParallelMesh001a")
 
-monModel = code_aster.Model.create()
+monModel = code_aster.Model()
 monModel.setSupportMesh(pMesh)
 monModel.addModelingOnAllMesh(code_aster.Physics.Mechanics,
                               code_aster.Modelings.Tridimensional)
@@ -21,36 +21,36 @@ monModel.build()
 testMesh = monModel.getSupportMesh()
 test.assertEqual(testMesh.getType(), "MAILLAGE_P")
 
-elas = code_aster.ElasMaterialBehaviour.create()
+elas = code_aster.ElasMaterialBehaviour()
 elas.setDoubleValue("E", 2.e11)
 elas.setDoubleValue("Nu", 0.3)
 
-acier = code_aster.Material.create()
+acier = code_aster.Material()
 acier.addMaterialBehaviour(elas)
 acier.build()
 
-affectMat = code_aster.MaterialOnMesh.create(pMesh)
+affectMat = code_aster.MaterialOnMesh(pMesh)
 affectMat.addMaterialOnAllMesh( acier )
 affectMat.build()
 
 testMesh2 = affectMat.getSupportMesh()
 test.assertEqual(testMesh2.getType(), "MAILLAGE_P")
 
-charCine = code_aster.KinematicsLoad.create()
+charCine = code_aster.KinematicsLoad()
 charCine.setSupportModel(monModel)
 charCine.addImposedMechanicalDOFOnNodes(code_aster.PhysicalQuantityComponent.Dx, 0., "COTE_B")
 charCine.addImposedMechanicalDOFOnNodes(code_aster.PhysicalQuantityComponent.Dy, 0., "COTE_B")
 charCine.addImposedMechanicalDOFOnNodes(code_aster.PhysicalQuantityComponent.Dz, 0., "COTE_B")
 charCine.build()
 
-charCine2 = code_aster.KinematicsLoad.create()
+charCine2 = code_aster.KinematicsLoad()
 charCine2.setSupportModel(monModel)
 charCine2.addImposedMechanicalDOFOnNodes(code_aster.PhysicalQuantityComponent.Dz, 1., "COTE_H")
 charCine2.build()
 
-monSolver = code_aster.MumpsSolver.create(code_aster.Renumbering.Metis)
+monSolver = code_aster.MumpsSolver(code_aster.Renumbering.Metis)
 
-mecaStatique = code_aster.StaticMechanicalSolver.create()
+mecaStatique = code_aster.StaticMechanicalSolver()
 mecaStatique.addKinematicsLoad(charCine)
 mecaStatique.addKinematicsLoad(charCine2)
 mecaStatique.setSupportModel(monModel)
