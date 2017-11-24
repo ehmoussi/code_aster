3f62d90d2ea0622fb79cb8e164073cc8cb53f177
Code_Aster <code-aster@edf.fr>
2017-11-24 13:30:15 +0100

    [repo] EOLE use only Intel mkl/2017.0.098 with impi/2017.0.098
    Former-commit-id: b4947ac9bf11827beb60d3a0b03652f5c5094700

--- wafcfg/eole_mpi.py
+++ wafcfg/eole_mpi.py
@@ -39,7 +39,7 @@ def configure(self):
     self.env['ADDMEM'] = 900
 
     self.env.append_value('OPT_ENV_FOOTER', [
-        'module load impi/2017.0.098'])
+        'module load mkl/2017.0.098 impi/2017.0.098'])
 
     self.env.prepend_value('LIBPATH', [
         YAMMROOT + '/prerequisites/Parmetis_aster-403_aster/lib',
