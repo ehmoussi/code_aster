4c5193a45ff9ea6e8c2085cfaa09d42d7a3ade95
Jean-Pierre Lefebvre <j-pierre.lefebvre@edf.fr>
2017-11-28 10:45:20 +0100

    [#27145] Suppress too aggressive optimization with Intel impi/2017
    Former-commit-id: 38171537b300665a7296b30fc9a7760100d01aa9

--- wafcfg/eole_mpi.py
+++ wafcfg/eole_mpi.py
@@ -38,8 +38,11 @@ def configure(self):
     eole.configure(self)
     self.env['ADDMEM'] = 900
 
+    # suppress too aggressive optimization with Intel impi/2017.0.98 : I_MPI_DAPL_TRANSLATION_CACHE=0 
     self.env.append_value('OPT_ENV_FOOTER', [
-        'module load mkl/2017.0.098 impi/2017.0.098'])
+        'module load mkl/2017.0.098 impi/2017.0.098',
+        'export I_MPI_DAPL_TRANSLATION_CACHE=0'
+    ])
 
     self.env.prepend_value('LIBPATH', [
         YAMMROOT + '/prerequisites/Parmetis_aster-403_aster/lib',
