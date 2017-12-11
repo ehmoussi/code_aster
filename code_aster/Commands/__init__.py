ac2a7e96102a70bbcede88f5781f12a4652e6c6e
Karina Macocco <macocco@phimeca.com>
2017-12-11 19:56:36 +0100

    [#23037] add of AFFE_CHAR_MECA_C
    Former-commit-id: 880379fd4ae79ce56376df3ec036d0889968ae70

--- code_aster/Commands/__init__.py
+++ code_aster/Commands/__init__.py
@@ -37,6 +37,7 @@ from .affe_char_acou import AFFE_CHAR_ACOU
 from .affe_char_cine import AFFE_CHAR_CINE
 from .affe_char_cine_f import AFFE_CHAR_CINE_F
 from .affe_char_meca import AFFE_CHAR_MECA
+from .affe_char_meca_c import AFFE_CHAR_MECA_C
 from .affe_char_meca_f import AFFE_CHAR_MECA_F
 from .affe_char_ther import AFFE_CHAR_THER
 from .affe_char_ther_f import AFFE_CHAR_THER_F
