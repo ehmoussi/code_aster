16c72014c10eb0a207d032c24bf027372c3f8dfb
Thomas DE SOZA <thomas.de-soza@edf.fr>
2017-12-12 18:10:29 +0100

    [#26823] remove unused option DEPL_DAMAC
    Former-commit-id: c18e36626325e4d405cc68ea85cf1ad83d7b465a

--- code_aster/Cata/Commands/calc_mac3coeur.py
+++ code_aster/Cata/Commands/calc_mac3coeur.py
@@ -17,17 +17,16 @@
 # along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 # --------------------------------------------------------------------
 
-# person_in_charge: romeo.fernandes at edf.fr
+# person_in_charge: pierre.badel at edf.fr
 
 from code_aster.Cata.Syntax import *
 from code_aster.Cata.DataStructure import *
 from code_aster.Cata.Commons import *
 
-def calc_mac3coeur_prod(self,RESU_DEF,DEPL_DAMAC,**args):
+
+def calc_mac3coeur_prod(self,RESU_DEF,**args):
     if RESU_DEF:
         self.type_sdprod(RESU_DEF,evol_noli)
-    if DEPL_DAMAC:
-        self.type_sdprod(DEPL_DAMAC,evol_noli)
     return evol_noli
 
 CALC_MAC3COEUR = MACRO(nom="CALC_MAC3COEUR",
@@ -39,7 +38,6 @@ CALC_MAC3COEUR = MACRO(nom="CALC_MAC3COEUR",
          TABLE_N      = SIMP(statut='o',typ=table_sdaster),         # TABLE INITIALE DES DAMAC A L INSTANT N
          MAILLAGE_N   = SIMP(statut='f',typ=maillage_sdaster),      # MAILLAGE EN ATTENDANT MIEUX ???
          RESU_DEF     = SIMP(statut='f',typ=CO,defaut=None),
-         DEPL_DAMAC     = SIMP(statut='f',typ=CO,defaut=None),
          FLUENCE_CYCLE = SIMP(statut='o',typ='R',max=1,defaut=0.),
 
          ETAT_INITIAL = FACT(statut='f',max=1,
@@ -53,12 +51,12 @@ CALC_MAC3COEUR = MACRO(nom="CALC_MAC3COEUR",
          LAME = FACT(statut='f',max=1, fr=tr("Estimation des lames d'eau entre AC"),
                      UNITE_THYC   = SIMP(statut='o',typ=UnitType(), max=1, inout='in'),            # Unite Logique du fichier THYC
                      ),
-         
+
          # choix du maintien dans le cas mono-assemblage
          b_maintien_mono = BLOC(condition = """exists("TYPE_COEUR") and TYPE_COEUR.startswith('MONO')""",
 
          DEFORMATION  = FACT(statut='f',max=1, fr=tr("Estimation des deformations des AC"),
-              
+
                RESU_INIT    = SIMP(statut='f',typ=resultat_sdaster),
                NIVE_FLUENCE = SIMP(statut='o',typ='R',max=1), # FLUENCE MAXIMALE DANS LE COEUR
                UNITE_THYC      = SIMP(statut='o',typ=UnitType(), max=1, inout='in'),                   # Unite Logique du fichier THYC
@@ -70,7 +68,7 @@ CALC_MAC3COEUR = MACRO(nom="CALC_MAC3COEUR",
                                  FORCE_MAINTIEN           =SIMP(statut='o',typ='R', max=1),),
                ARCHIMEDE = SIMP(statut='o',typ='TXM',into=("OUI","NON"), ),
           ),),
-          
+
           # choix du maintien dans le cas d'un coeur à plusieurs assemblages
           b_maintien_coeur = BLOC(condition = """exists("TYPE_COEUR") and not TYPE_COEUR.startswith('MONO')""",
           DEFORMATION  = FACT(statut='f',max=1,
