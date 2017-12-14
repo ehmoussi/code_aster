d28f9d1d1fa9ae8ab3e013350bfa3adae219ed06
Karina Macocco <macocco@phimeca.com>
2017-12-14 19:32:46 +0100

    [#23037] add of CALC_ERREUR
    Former-commit-id: 74a37b4382bc4ea750cd8ea58b3f83bc0c3bb024

---
+++ code_aster/Commands/calc_erreur.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+
+# Copyright (C) 1991 - 2017  EDF R&D                www.code-aster.org
+#
+# This file is part of Code_Aster.
+#
+# Code_Aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Code_Aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Code_Aster.  If not, see <http://www.gnu.org/licenses/>.
+
+# person_in_charge: nicolas.sellenet@edf.fr
+
+from ..Utilities import required
+from .ExecuteCommand import ExecuteCommand
+
+
+class ComputeError(ExecuteCommand):
+    """Command that creates the :class:`~code_aster.Objects.ResultsContainer`"""
+    command_name = "CALC_ERREUR"
+
+    def adapt_syntax(self, keywords):
+        """Hook to adapt syntax from a old version or for compatibility reasons.
+
+        Arguments:
+            keywords (dict): Keywords arguments of user's keywords, changed
+                in place.
+        """
+        required(keywords, "", "reuse")
+
+    def create_result(self, keywords):
+        """Initialize the result.
+
+        Arguments:
+            keywords (dict): Keywords arguments of user's keywords.
+        """
+        self._result = keywords["RESULTAT"]
+
+    def post_exec(self, keywords):
+        """Execute the command.
+
+        Arguments:
+            keywords (dict): User's keywords.
+        """
+        self._result.update()
+
+CALC_ERREUR = ComputeError.run
