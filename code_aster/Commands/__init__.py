ef1911fba60dd8651b35e8c5eaa4acef0faa7bd0
Karina Macocco <macocco@phimeca.com>
2018-01-05 11:39:00 +0100

    [#23037] add of COMB_SISM_MODAL
    Former-commit-id: 605748c721c343a8aba910cf834349d57b2a6be3

--- code_aster/Commands/__init__.py
+++ code_aster/Commands/__init__.py
@@ -68,6 +68,7 @@ from .calc_point_mat import CALC_POINT_MAT
 from .calc_vect_elem import CALC_VECT_ELEM
 from .comb_fourier import COMB_FOURIER
 from .comb_matr_asse import COMB_MATR_ASSE
+from .comb_sism_modal import COMB_SISM_MODAL
 from .copier import COPIER
 from .crea_champ import CREA_CHAMP
 from .crea_maillage import CREA_MAILLAGE
