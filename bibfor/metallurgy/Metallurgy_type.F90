14384f1a5aa30e733e57790373367287206424ac
Micka?l Abbas <mickael.abbas@edf.fr>
2018-01-08 09:44:04 +0100

    [#19930] Define new module for metallurgy
    Former-commit-id: 007c8122bb88676c0cc341e2db63b121ce7e5477

---
+++ bibfor/metallurgy/Metallurgy_type.F90
@@ -0,0 +1,55 @@
+! --------------------------------------------------------------------
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+! This file is part of code_aster.
+!
+! code_aster is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! code_aster is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+! --------------------------------------------------------------------
+! person_in_charge: mickael.abbas at edf.fr
+!
+module Metallurgy_type
+!
+implicit none
+!
+#include "asterf_types.h"
+!
+! --------------------------------------------------------------------------------------------------
+!
+! Metallurgy
+!
+! Define types for datastructures
+!
+! --------------------------------------------------------------------------------------------------
+!
+!
+    type META_AusteniteParameters
+        real(kind=8) :: lambda0
+        real(kind=8) :: qsr_k
+        real(kind=8) :: d10
+        real(kind=8) :: wsr_k
+    end type META_AusteniteParameters
+!
+    type META_SteelParameters
+        real(kind=8) :: ar3
+        real(kind=8) :: alpha
+        real(kind=8) :: ms0
+        real(kind=8) :: ac1
+        real(kind=8) :: ac3
+        real(kind=8) :: taux_1
+        real(kind=8) :: taux_3
+        aster_logical :: l_grain_size
+        type(META_AusteniteParameters) :: austenite
+    end type META_SteelParameters
+
+!
+end module
