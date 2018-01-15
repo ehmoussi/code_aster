28b9eb22e604013df4545e52e047992e88f9f3df
Micka?l Abbas <mickael.abbas@edf.fr>
2018-01-15 07:19:45 +0100

    [#27245] Remove PARM_ALPHA from c_comportement
    Former-commit-id: a1710ad8b099ffe46d7091e051ac7d2da8e323d7

--- code_aster/Cata/Commons/c_comportement.py
+++ code_aster/Cata/Commons/c_comportement.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -94,7 +94,6 @@ def C_COMPORTEMENT(COMMAND=None) :  #COMMUN#
                                   ),
 
                 PARM_THETA      =SIMP(statut='f',typ='R',val_min=0.,val_max=1., defaut= 1.),
-                PARM_ALPHA      =SIMP(statut='f',typ='R',defaut= 1. ),
 
                 b_radi          =BLOC(condition = """not exists("TYPE_MATR_TANG")""", RESI_RADI_RELA  =SIMP(statut='f',typ='R', ),),
 
@@ -417,7 +416,6 @@ def C_COMPORTEMENT(COMMAND=None) :  #COMMUN#
 
 
             PARM_THETA      =SIMP(statut='f',typ='R',val_min=0.,val_max=1., defaut= 1.),
-            PARM_ALPHA      =SIMP(statut='f',typ='R',defaut= 1. ),
 
             b_radi          =BLOC(condition = """not exists("TYPE_MATR_TANG")""", RESI_RADI_RELA  =SIMP(statut='f',typ='R', ),),
 
