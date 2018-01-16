754744b581821f5b93f2c881248bc564424210f3
Karina Macocco <macocco@phimeca.com>
2018-01-16 16:07:28 +0100

    [#23037] add of INFO_MODE
    Former-commit-id: 61678b37f25d5c865ef75ed88b3a8a5ef166c6ab

--- code_aster/Commands/__init__.py
+++ code_aster/Commands/__init__.py
@@ -128,6 +128,7 @@ from .impr_co import IMPR_CO
 from .info_exec_aster import INFO_EXEC_ASTER
 from .impr_jeveux import IMPR_JEVEUX
 from .impr_resu import IMPR_RESU
+from .info_mode import INFO_MODE
 from .info_resu import INFO_RESU
 from .impr_macr_elem import IMPR_MACR_ELEM
 from .lire_champ import LIRE_CHAMP
