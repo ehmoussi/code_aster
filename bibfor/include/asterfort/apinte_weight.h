f488bff78ae481c6db5cb45a48a8c92dd5311dd6
Micka?l Abbas <mickael.abbas@edf.fr>
2018-01-19 13:24:29 +0100

    [#27303] New subroutine for compute weight of intersection
    Former-commit-id: bc9fe695706cf9358939b6868e877fb152d2b62a

---
+++ bibfor/include/asterfort/apinte_weight.h
@@ -0,0 +1,27 @@
+! --------------------------------------------------------------------
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+! This file is part of code_aster.
+!
+! code_aster is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! code_aster is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+! --------------------------------------------------------------------
+!
+interface
+    subroutine apinte_weight(elem_dime  , nb_poin_inte, poin_inte,&
+                             inte_weight)
+        integer, intent(in) :: elem_dime
+        integer, intent(in) :: nb_poin_inte
+        real(kind=8), intent(in) :: poin_inte(elem_dime-1,16)
+        real(kind=8), intent(out) :: inte_weight
+    end subroutine apinte_weight
+end interface
