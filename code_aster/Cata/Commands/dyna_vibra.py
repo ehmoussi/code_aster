a8b9f7be3570b34b03c40cb991624f983a1f37fe
Jean-Pierre Lefebvre <j-pierre.lefebvre@edf.fr>
2018-01-22 15:27:44 +0100

    [#27129] Change VITE_ROTA from mandatory to optional          Change forma12d and forma12e          Change copyright
    Former-commit-id: 44740692f0e57a097ee2ca6c19242a0dba843270

--- code_aster/Cata/Commands/dyna_vibra.py
+++ code_aster/Cata/Commands/dyna_vibra.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -73,7 +73,7 @@ DYNA_VIBRA = OPER (nom      = "DYNA_VIBRA",
         ACCE_ROTA       =     SIMP(statut='f',typ=(fonction_sdaster,formule),),),
 
         b_constante     = BLOC(condition="""equal_to("VITESSE_VARIABLE", 'NON')""",
-        VITE_ROTA       =     SIMP(statut='o',typ='R',defaut=0.E0),),
+        VITE_ROTA       =     SIMP(statut='f',typ='R',defaut=0.E0),),
         COUPLAGE_EDYOS  =     FACT(statut='f',max=1,
             PAS_TPS_EDYOS=        SIMP(statut='o',typ='R' ),),),
 
