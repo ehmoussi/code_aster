7dcd9f4e3b52399ff6a2968d54e25e6fba048001
Karina Macocco <macocco@phimeca.com>
2018-01-24 14:01:06 +0100

    [#23037] add of PRE_GMSH
    Former-commit-id: 23a290d55642f981769fd4c5ce659005d8015982

---
+++ astest/xxPreGmsh.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import code_aster
+from code_aster.Commands import *
+
+code_aster.init()
+
+test = code_aster.TestCase()
+
+PRE_GMSH(UNITE_GMSH=60,UNITE_MAILLAGE=22)
+
+test.assertTrue( True )
+test.printSummary()
