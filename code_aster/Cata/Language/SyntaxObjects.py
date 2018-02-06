f173374e69f0e6cb0c9c9ce861da3a2cca8d7161
Mathieu Courtois <mathieu.courtois@edf.fr>
2018-02-06 18:59:24 +0100

    [#27254] Ignore None objects when calling 'type_sdprod'.
    Former-commit-id: baf242ec383bdbdc9fa28c23b03cc9d8e7c7d11a

--- code_aster/Cata/Language/SyntaxObjects.py
+++ code_aster/Cata/Language/SyntaxObjects.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -701,6 +701,8 @@ class Macro(Command):
 
     def type_sdprod(self, result, astype):
         """Define the type of the result."""
+        if result is None:
+            return
         result.settype(astype)
 
 
