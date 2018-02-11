61c4fa3bfec60e592169c384407692e0ea9a0421
Mathieu Courtois <mathieu.courtois@edf.fr>
2018-02-11 12:16:08 +0100

    [#27390] Fix usages of CREA_RESU.
    Former-commit-id: 1e7b7bb65a8b5502a8d69aa8c7b4042f7a4234a9

--- bibpyt/Calc_epx/trans_var_int.py
+++ bibpyt/Calc_epx/trans_var_int.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -110,6 +110,7 @@ def var_int_a2e(compor_gr, resu, mod, nume_ordre):
             )
 
     __res = CREA_RESU(reuse=__res,
+                  RESULTAT=__res,
                   OPERATION = 'AFFE',
                   TYPE_RESU = 'EVOL_NOLI',
                   NOM_CHAM  = 'SIEF_ELGA',
@@ -120,6 +121,7 @@ def var_int_a2e(compor_gr, resu, mod, nume_ordre):
             )
 
     __res = CREA_RESU(reuse=__res,
+                  RESULTAT=__res,
                   OPERATION = 'AFFE',
                   TYPE_RESU = 'EVOL_NOLI',
                   NOM_CHAM  = 'VARI_ELGA',
--- bibpyt/Mac3coeur/mac3coeur_calcul.py
+++ bibpyt/Mac3coeur/mac3coeur_calcul.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -927,6 +927,7 @@ class Mac3CoeurLame(Mac3CoeurCalcul):
                    }
         if reuse :
             keywords['reuse'] = reuse
+            keywords['RESULTAT'] = reuse
         return keywords
 
 
