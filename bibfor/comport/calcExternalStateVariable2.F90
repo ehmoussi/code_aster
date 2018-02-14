7a8374b23dec6010c9f3314e8d663499f1d7d99e
Micka?l Abbas <mickael.abbas@edf.fr>
2018-02-14 13:14:44 +0100

    [#27408] Correct COORGA parameter for META_LEMA_ANI
    Former-commit-id: 5c787e574947606333ffc3f9ae2be472eaace726

--- bibfor/comport/calcExternalStateVariable2.F90
+++ bibfor/comport/calcExternalStateVariable2.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -31,7 +31,7 @@ implicit none
 integer, intent(in) :: nno, npg, ndim
 integer, intent(in) :: jv_func
 character(len=8), intent(in) :: typmod(2)
-real(kind=8), intent(in) :: geom(3, nno)
+real(kind=8), intent(in) :: geom(ndim, nno)
 !
 ! --------------------------------------------------------------------------------------------------
 !
