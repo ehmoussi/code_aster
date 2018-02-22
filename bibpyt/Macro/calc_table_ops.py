88d1329ea785e88e6ba185a32d99bba0ecf5536f
Nicolas Sellenet <nicolas.sellenet at edf.fr>
2018-02-22 09:31:56 +0100

    [#27379] fix xxErrorPostProcessing
    Former-commit-id: 70a805d12bb41f03d31de578fe7e568673075fe3

--- bibpyt/Macro/calc_table_ops.py
+++ bibpyt/Macro/calc_table_ops.py
@@ -210,4 +210,5 @@ def calc_table_ops(self, TABLE, ACTION, INFO, **args):
     # type de la table de sortie à passer à CREA_TABLE
     tabout = CREA_TABLE(TYPE_TABLE=typ_tabout,
                         **dprod)
+    print "tabout", tabout
     return tabout
