be83a0d78dd4350da76309e7606407396625fb47
Etienne Cheignon <etienne.cheignon@necs.fr>
2018-03-27 17:23:29 +0200

    [#27428] Enable AFFE_CHAR_MECA/FLUX_REP/FLUX_{X,Y,Z} in 3D          adjust ttnp200a
    Former-commit-id: 7cdbdb3f7753b306a032005fe4e97b112159d62c

--- bibfor/te/te0095.F90
+++ bibfor/te/te0095.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -16,10 +16,126 @@
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
 
-subroutine te0095(nomopt, nomte)
-! aslint: disable=W0104
+subroutine te0095(option, nomte)
     implicit none
-#include "asterfort/utmess.h"
-    character(len=16) :: nomte, nomopt
-    call utmess('F', 'FERMETUR_8')
+#include "jeveux.h"
+#include "asterfort/elrefe_info.h"
+#include "asterfort/fointe.h"
+#include "asterfort/jevech.h"
+!
+    character(len=16) :: option, nomte
+!.......................................................................
+!
+!     BUT: CALCUL DES VECTEURS ELEMENTAIRES EN THERMIQUE
+!          CORRESPONDANT AU FLUX IMPOSE (FONCTION)
+!          SUR DES FACES D'ELEMENTS ISOPARAMETRIQUES 3D
+!
+!          OPTION : 'CHAR_THER_FLUX_F'
+!
+!     ENTREES  ---> OPTION : OPTION DE CALCUL
+!          ---> NOMTE  : NOM DU TYPE ELEMENT
+!.......................................................................
+!
+    character(len=8) :: nompar(4)
+    real(kind=8) :: nx, ny, nz, sx(9, 9), sy(9, 9), sz(9, 9), jac
+    real(kind=8) :: valpar(4), xx, yy, zz, fluxx, fluxy, fluxz, flun
+    integer :: ipoids, ivf, idfdx, idfdy, igeom, itemps
+    integer :: ndim, nno, ipg, npg1, ivectt, iflux, idec, jdec, kdec
+    integer :: ldec, nnos, jgano
+!
+!-----------------------------------------------------------------------
+    integer :: i, ier, ino, j, jno
+!-----------------------------------------------------------------------
+    call elrefe_info(fami='RIGI', ndim=ndim, nno=nno, nnos=nnos, npg=npg1,&
+                     jpoids=ipoids, jvf=ivf, jdfde=idfdx, jgano=jgano)
+    idfdy = idfdx + 1
+!
+    call jevech('PGEOMER', 'L', igeom)
+    call jevech('PFLUXVF', 'L', iflux)
+    call jevech('PTEMPSR', 'L', itemps)
+    call jevech('PVECTTR', 'E', ivectt)
+
+    nompar(1) = 'X'
+    nompar(2) = 'Y'
+    nompar(3) = 'Z'
+    nompar(4) = 'INST'
+!
+    do i = 1, nno
+        zr(ivectt+i-1) = 0.0d0
+    end do
+!
+!    CALCUL DES PRODUITS VECTORIELS OMI X OMJ
+!
+    do ino = 1, nno
+        i = igeom + 3*(ino-1) -1
+        do jno = 1, nno
+            j = igeom + 3*(jno-1) -1
+            sx(ino,jno) = zr(i+2) * zr(j+3) - zr(i+3) * zr(j+2)
+            sy(ino,jno) = zr(i+3) * zr(j+1) - zr(i+1) * zr(j+3)
+            sz(ino,jno) = zr(i+1) * zr(j+2) - zr(i+2) * zr(j+1)
+        end do
+    end do
+!
+!    BOUCLE SUR LES POINTS DE GAUSS
+!
+    do ipg = 1, npg1
+        kdec = (ipg-1)*nno*ndim
+        ldec = (ipg-1)*nno
+!
+!    CALCUL DE FLUX
+!
+        xx = 0.d0
+        yy = 0.d0
+        zz = 0.d0
+        do i = 1, nno
+            xx = xx + zr(igeom+3*i-3) * zr(ivf+ldec+i-1)
+            yy = yy + zr(igeom+3*i-2) * zr(ivf+ldec+i-1)
+            zz = zz + zr(igeom+3*i-1) * zr(ivf+ldec+i-1)
+        end do
+        valpar(1) = xx
+        valpar(2) = yy
+        valpar(3) = zz
+        valpar(4) = zr(itemps)
+        call fointe('FM', zk8(iflux), 4, nompar, valpar,&
+                    fluxx, ier)
+        call fointe('FM', zk8(iflux+1), 4, nompar, valpar,&
+                    fluxy, ier)
+        call fointe('FM', zk8(iflux+2), 4, nompar, valpar,&
+                    fluxz, ier)
+!
+        nx = 0.0d0
+        ny = 0.0d0
+        nz = 0.0d0
+!
+!   CALCUL DE LA NORMALE AU POINT DE GAUSS IPG
+!
+        do i = 1, nno
+            idec = (i-1)*ndim
+            do j = 1, nno
+                jdec = (j-1)*ndim
+                nx = nx + zr(idfdx+kdec+idec) *zr(idfdy+kdec+jdec) * sx(i,j)
+                ny = ny + zr(idfdx+kdec+idec) *zr(idfdy+kdec+jdec) * sy(i,j)
+                nz = nz + zr(idfdx+kdec+idec) *zr(idfdy+kdec+jdec) * sz(i,j)
+            end do
+        end do
+    
+!
+!   CALCUL DU JACOBIEN AU POINT DE GAUSS IPG
+!
+        
+        jac = sqrt(nx*nx + ny*ny + nz*nz)
+        
+!
+!       PROJECTION SUR LA NORMALE
+!
+        nx=nx/jac
+        ny=ny/jac
+        nz=nz/jac
+        flun = nx*fluxx + ny*fluxy + nz*fluxz
+        
+        do i = 1, nno
+            zr(ivectt+i-1) = zr(ivectt+i-1) + jac * zr(ipoids+ipg-1) * flun * zr(ivf+ldec+i-1)
+        end do
+!
+    end do
 end subroutine
