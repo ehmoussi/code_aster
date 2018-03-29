b495985d3a93870fb83cca67d05d85760f2d62c6
Micka?l Abbas <mickael.abbas@edf.fr>
2018-03-29 08:19:39 +0200

    [#27475] Suppress some deprecation warnings
    Former-commit-id: bdf3a6297fc62300f721be05e18fa924d89c9e1c

--- bibfor/deprecated/deprecated_model.F90
+++ bibfor/deprecated/deprecated_model.F90
@@ -15,16 +15,15 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
+!
 subroutine deprecated_model(model)
 !
 implicit none
+!
 #include "asterfort/assert.h"
 #include "asterfort/utmess.h"
 !
-! person_in_charge: josselin.delmas at edf.fr
-!
-    character(len=*), intent(in) :: model
+character(len=*), intent(in) :: model
 !
 ! --------------------------------------------------------------------------------------------------
 !
@@ -43,25 +42,17 @@ implicit none
 !
 ! --------------------------------------------------------------------------------------------------
 !
-    if (model .eq. 'POU_C_T') then
-        vali = 13
-        valk    = "MODELISATION='POU_C_T'"
-!
-    else if (model .eq. '3D_INCO_UPGB') then
+    if (model .eq. '3D_INCO_UPGB') then
         vali = 14
         valk    = "MODELISATION='3D_INCO_UPGB'"
-!
     else if (model .eq. 'AXIS_INCO_UPGB') then
         vali = 14
         valk    = "MODELISATION='AXIS_INCO_UPGB'"
-!
     else if (model .eq. 'D_PLAN_INCO_UPGB') then
         vali = 14
         valk    = "MODELISATION='D_PLAN_INCO_UPGB'"
-!
     else
         goto 999
-!
     endif
 !
     call utmess('A', 'SUPERVIS_9', sk = valk, si = vali)
--- bibfor/op/op0039.F90
+++ bibfor/op/op0039.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -30,7 +30,6 @@ subroutine op0039()
 #include "asterc/getfac.h"
 #include "asterfort/assert.h"
 #include "asterfort/codent.h"
-#include "asterfort/deprecated_algom.h"
 #include "asterfort/dismoi.h"
 #include "asterfort/getvid.h"
 #include "asterfort/getvis.h"
