12ada0cc686486b2bc746bfc37bb6c32a8a4644d
Jean-Pierre Lefebvre <j-pierre.lefebvre@edf.fr>
2018-04-30 13:47:41 +0200

    [#27536] suppress logical unit 8 (RESULTAT)          suppress logical unit 8 (RESULTAT) impr_table          use iunifi in w039c1          replace unit 8 in defi_sol_equi_ops          change dglrdm (ifr)          modify ulopen
    Former-commit-id: e7d727afce81f92649a528210efb31a80a1b7e04

--- code_aster/Cata/Commands/maj_cata.py
+++ code_aster/Cata/Commands/maj_cata.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -30,6 +30,6 @@ MAJ_CATA=PROC(nom="MAJ_CATA",op=20,
 
               ELEMENT     =FACT(statut='f',),
 
-              UNITE       =SIMP(statut='f',typ=UnitType(),defaut=8, inout='out'),       
+              UNITE       =SIMP(statut='f',typ=UnitType(),defaut=6, inout='out'),       
               TYPE_ELEM   =FACT(statut='f',),
 );
