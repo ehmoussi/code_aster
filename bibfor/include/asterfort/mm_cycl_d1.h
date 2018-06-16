9ae0ec2e6a0161b54c2709a86f0df580c2282e81
ayaovi-dzifa Kudawoo <ayaovi-dzifa.kudawoo@edf.fr>
2018-06-16 21:23:47 +0200

    [#25963] contact criteria revisiting : FLIP-FLOP
    Former-commit-id: 01258a139f656f1794689df8f69d613d71db33c6

--- bibfor/include/asterfort/mm_cycl_d1.h
+++ bibfor/include/asterfort/mm_cycl_d1.h
@@ -21,7 +21,7 @@
 interface
     subroutine mm_cycl_d1(ds_contact    , i_cont_poin   , &
                           coef_cont     , pres_cont_prev, dist_cont_prev, &
-                          indi_cont_eval, indi_cont_prev, dist_cont     , pres_cont,&
+                          indi_cont_eval, dist_cont     , pres_cont,&
                           alpha_cont_matr,alpha_cont_vect)
         use NonLin_Datastructure_type
         type(NL_DS_Contact), intent(in) :: ds_contact
@@ -30,7 +30,6 @@ interface
         real(kind=8), intent(in) :: pres_cont_prev
         real(kind=8), intent(in) :: dist_cont_prev
         integer, intent(in) :: indi_cont_eval
-        integer, intent(in) :: indi_cont_prev
         real(kind=8), intent(in) :: dist_cont
         real(kind=8), intent(in) :: pres_cont
         real(kind=8), intent(out) :: alpha_cont_matr
