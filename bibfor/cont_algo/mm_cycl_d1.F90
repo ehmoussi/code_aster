f60e36e9b85b921c5dc50941135d8ffba5a46c0d
ayaovi-dzifa Kudawoo <ayaovi-dzifa.kudawoo@edf.fr>
2018-06-16 21:23:47 +0200

    [#25963] contact criteria revisiting : FLIP-FLOP
    Former-commit-id: 01258a139f656f1794689df8f69d613d71db33c6

--- bibfor/cont_algo/mm_cycl_d1.F90
+++ bibfor/cont_algo/mm_cycl_d1.F90
@@ -18,22 +18,20 @@
 
 subroutine mm_cycl_d1(ds_contact    , i_cont_poin   ,&
                       coef_cont     , pres_cont_prev, dist_cont_prev,&
-                      indi_cont_eval,indi_cont_prev,&
-                      dist_cont     , pres_cont,alpha_cont_matr,alpha_cont_vect)
+                      indi_cont_eval, dist_cont     , pres_cont,alpha_cont_matr,alpha_cont_vect)
 !
 use NonLin_Datastructure_type
 !
 implicit none
 !
 #include "asterf_types.h"
-#include "asterfort/assert.h"
 #include "asterfort/iscode.h"
 #include "asterfort/iscycl.h"
 #include "asterfort/isdeco.h"
 #include "asterfort/jedema.h"
 #include "asterfort/jemarq.h"
 #include "asterfort/jeveuo.h"
-#include "asterfort/mm_cycl_erase.h"
+#include "asterfort/mm_cycl_shift.h"
 #include "asterfort/mm_cycl_d1_ss.h"
 !
 ! person_in_charge: ayaovi-dzifa.kudawoo at edf.fr
@@ -43,7 +41,7 @@ implicit none
     real(kind=8), intent(in) :: coef_cont
     real(kind=8), intent(in) :: pres_cont_prev
     real(kind=8), intent(in) :: dist_cont_prev
-    integer, intent(in) :: indi_cont_eval,indi_cont_prev
+    integer, intent(in) :: indi_cont_eval
     real(kind=8), intent(in) :: dist_cont
     real(kind=8), intent(in) :: pres_cont
     real(kind=8), intent(out) :: alpha_cont_matr, alpha_cont_vect 
@@ -75,10 +73,10 @@ implicit none
     integer, pointer :: p_sdcont_cyceta(:) => null()
     integer :: statut(30)
     integer :: cycl_type, cycl_long_acti
-    integer :: cycl_ecod, cycl_long, cycl_sub_type, cycl_stat
+    integer :: cycl_ecod(1), cycl_long, cycl_sub_type, cycl_stat
     aster_logical :: detect
     real(kind=8) :: laug_cont_prev, laug_cont_curr
-    real(kind=8) :: pres_near_zero
+    real(kind=8) :: pres_near
     integer :: zone_cont_prev, zone_cont_curr
 !
 ! --------------------------------------------------------------------------------------------------
@@ -87,12 +85,9 @@ implicit none
 !
 ! - Initializations
 !
-    cycl_long_acti = ds_contact%cycl_long_acti
+    cycl_long_acti = 3
     cycl_type = 1
-    cycl_ecod = 0
-    detect    = .false.
-    !on definit une pression rasante proche du zero
-    pres_near_zero = 1.d-2 * ds_contact%arete_min
+    pres_near = 1.d2 * ds_contact%arete_min
     alpha_cont_matr = 1.0d0
     alpha_cont_vect = 1.0d0
 !
@@ -115,49 +110,41 @@ implicit none
 !
 ! - Cycle state
 !
-    cycl_long    = p_sdcont_cycnbr(4*(i_cont_poin-1)+cycl_type)
-    cycl_ecod    = p_sdcont_cyclis(4*(i_cont_poin-1)+cycl_type)
-    cycl_ecod    = cycl_ecod + (2**cycl_long)*indi_cont_eval
-    
+    cycl_ecod(1) = p_sdcont_cyclis(4*(i_cont_poin-1)+cycl_type)
+    cycl_long = p_sdcont_cycnbr(4*(i_cont_poin-1)+cycl_type)
+    call isdeco(cycl_ecod(1), statut, 30)
+!
+! - New iteration in cycle
+!
+    cycl_long = cycl_long + 1
+    statut(cycl_long) = indi_cont_eval
+    call iscode(statut, cycl_ecod(1), 30)
 !
 ! - Cycling detection
 !
     cycl_stat = 0
-!     write (6,*) "(2**cycl_long)*indi_cont_eval",(2**cycl_long)*indi_cont_eval,cycl_long,indi_cont_eval
-!     write (6,*) "p_sdcont_cyclis(4*(i_cont_poin-1)+cycl_type)",p_sdcont_cyclis(4*(i_cont_poin-1)+cycl_type)
-!     write (6,*) "cycl_long",cycl_long
-    if (cycl_long+1  .eq. cycl_long_acti) then
-        detect = iscycl(cycl_ecod, cycl_long_acti)
-        if (p_sdcont_cyceta(4*(i_cont_poin-1)+cycl_type) .ge. 10) then
+    if (cycl_long .eq. cycl_long_acti) then
+        detect = iscycl(cycl_ecod(1), cycl_long_acti) .and. &
+                 laug_cont_curr *laug_cont_prev .lt. 0.0d0
+        if (detect) then
             cycl_stat = 10
-            if (indi_cont_eval .eq. indi_cont_prev) cycl_stat = 0
-        elseif (detect) then
-            cycl_stat = 10
-            call mm_cycl_d1_ss(pres_near_zero, laug_cont_prev, laug_cont_curr, zone_cont_prev,&
+            call mm_cycl_d1_ss(pres_near, laug_cont_prev, laug_cont_curr, zone_cont_prev,&
                                zone_cont_curr, cycl_sub_type,alpha_cont_matr,alpha_cont_vect)
 
             cycl_stat = cycl_stat + cycl_sub_type
-            alpha_cont_matr = 0.99d0
-            alpha_cont_vect = 0.99d0
         endif
-        if (cycl_stat .ge. 10) &
-                write (6,*) "Point de contact en CYCLAGE CONTACT : i_cont_poin",i_cont_poin
     endif
-
-
 !
-! - Cycling save : incrementation of cycle objects
+! - End of cycling detection zone: shifting
 !
-    cycl_long = cycl_long + 1
+    if (cycl_long .eq. cycl_long_acti) call mm_cycl_shift(cycl_long_acti, cycl_ecod(1),&
+                                                          cycl_long)
+!
+! - Cycling save
+!
     p_sdcont_cyceta(4*(i_cont_poin-1)+cycl_type) = cycl_stat
-    p_sdcont_cyclis(4*(i_cont_poin-1)+cycl_type) = cycl_ecod
-    if (cycl_long .eq. cycl_long_acti)  then 
-        cycl_long = 0
-        cycl_ecod = 0
-    endif
+    p_sdcont_cyclis(4*(i_cont_poin-1)+cycl_type) = cycl_ecod(1)
     p_sdcont_cycnbr(4*(i_cont_poin-1)+cycl_type) = cycl_long
-    p_sdcont_cyclis(4*(i_cont_poin-1)+cycl_type) = cycl_ecod
-    ASSERT(cycl_long .le. cycl_long_acti)
 !
     call jedema()
 end subroutine
--- bibfor/cont_algo/mm_cycl_detect.F90
+++ bibfor/cont_algo/mm_cycl_detect.F90
@@ -90,7 +90,7 @@ implicit none
 ! - Detection of cycling: contact/no contact
 !
     call mm_cycl_d1(ds_contact, i_cont_poin, pres_cont_prev, dist_cont_prev, coef_cont,&
-                    indi_cont_eval,indi_cont_prev, dist_cont_curr, pres_cont_curr,alpha_cont_matr,&
+                    indi_cont_eval, dist_cont_curr, pres_cont_curr,alpha_cont_matr,&
                     alpha_cont_vect)
 !
 ! - Detection of cycling: sliding/sticking
