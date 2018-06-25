f34c1fcb83b4e1b76fa7d794c4b65edc95479d17
Jean-Pierre Lefebvre <j-pierre.lefebvre@edf.fr>
2018-06-25 14:58:11 +0200

    [#26899] Add environment variable LD_PRELOAD with mkl librairies
    Former-commit-id: 6d67c70052ba282e672108f172937f9fba4131a2

--- wafcfg/aster5.py
+++ wafcfg/aster5.py
@@ -49,7 +49,10 @@ def configure(self):
     self.env['ADDMEM'] = 600
     self.env.append_value('OPT_ENV', [
         '. /etc/profile.d/lmod.sh',
-        'module load ifort/2016.0.047 icc/2016.0.047 mkl/2016.0.047'])
+        'module load ifort/2016.0.047 icc/2016.0.047 mkl/2016.0.047',
+        'module load ifort/2016.0.047 icc/2016.0.047 mkl/2016.0.047',
+        'export LD_PRELOAD=/opt/intel/2016.0.047/compilers_and_libraries_2016.0.109/linux/mkl/lib/intel64_lin/libmkl_scalapack_ilp64.so:/opt/intel/2016.0.047/compilers_and_libraries_2016.0.109/linux/mkl/lib/intel64_lin/libmkl_intel_ilp64.so:/opt/intel/2016.0.047/compilers_and_libraries_2016.0.109/linux/mkl/lib/intel64_lin/libmkl_intel_thread.so:/opt/intel/2016.0.047/compilers_and_libraries_2016.0.109/linux/mkl/lib/intel64_lin/libmkl_core.so:/opt/intel/2016.0.047/compilers_and_libraries_2016.0.109/linux/mkl/lib/intel64_lin/libmkl_blacs_intelmpi_ilp64.so:/opt/intel/2016.0.047/compilers_and_libraries_2016.0.109/linux/compiler/lib/intel64_lin/libiomp5.so',
+    ])
 
     TFELHOME = YAMMROOT + '/prerequisites/Mfront-TFEL311_aster'
     TFELVERS = '3.1.1'
