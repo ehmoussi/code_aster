fd66b89e29fc289c3609bdc274365a4d540debe0
Jean-Pierre Lefebvre <j-pierre.lefebvre@edf.fr>
2018-07-09 13:52:00 +0200

    [#27842] Suppress summarized cpu times in RESULTAT
    Former-commit-id: bb3d42df1ba1b8d35df5b7ce8d42f07d39d7f0ba

--- bibpyt/Execution/E_JDC.py
+++ bibpyt/Execution/E_JDC.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -180,9 +180,6 @@ class JDC:
         # affiche le récapitulatif du timer
         cpu_total_user, cpu_total_syst, elapsed_total = self.timer.StopAndGetTotal(
         )
-
-        aster.affiche('RESULTAT', convert(repr(self.timer)))
-        aster.fclose(8)
 
         tpmax = aster_core.get_option("tpmax")
         if tpmax != None:
