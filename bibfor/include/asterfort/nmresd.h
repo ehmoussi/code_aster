cfda091a29dc5a2dcca17a10b345b15d73d4f912
Micka?l Abbas <mickael.abbas@edf.fr>
2018-07-24 17:23:51 +0200

    [#27906] Allow PREDICTION=EXTRAPOLE for ROM
    Former-commit-id: e47132f104eda9e61048f80ef6d19c28f863a8df

--- bibfor/include/asterfort/nmresd.h
+++ bibfor/include/asterfort/nmresd.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,13 +15,13 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
-!
+#include "asterf_types.h"
 !
 interface
-    subroutine nmresd(fonact, sddyna, ds_measure, solveu,&
-                      numedd, instan, maprec    , matass    , cndonn,&
-                      cnpilo, cncine, solalg    , rescvg, ds_algorom_)
+    subroutine nmresd(fonact     , sddyna        , ds_measure, solveu,&
+                      numedd     , instan        , maprec    , matass, cndonn,&
+                      cnpilo     , cncine        , solalg    , rescvg,&
+                      ds_algorom_)
         use NonLin_Datastructure_type
         use ROM_Datastructure_type        
         integer :: fonact(*)
