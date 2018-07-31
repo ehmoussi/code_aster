b37c381608541451ce9252a9845a6bda54073ab1
Nicolas Pignet <nicolas.pignet@edf.fr>
2018-07-31 15:11:59 +0200

    [#27924] Supress a nonphysical check and correct the consistency of criteria between GDEF_LOG routines
    Former-commit-id: 274c5ed5808d3f1e80c97169b2c6fa70788d029e

--- bibfor/algorith/deflog.F90
+++ bibfor/algorith/deflog.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -16,13 +16,31 @@
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
 
-subroutine deflog(ndim, f, epsl, gn, lamb,&
-                  logl, iret)
+subroutine deflog(ndim, f, epsl, gn, lamb, logl, iret)
+!
     implicit none
+!
+#include "asterc/r8prem.h"
+#include "asterfort/diago2.h"
+#include "asterfort/diagp3.h"
+#include "asterfort/lctr2m.h"
+#include "asterfort/pmat.h"
+#include "asterfort/r8inir.h"
+#include "asterfort/tnsvec.h"
+!
+    integer, intent(in) :: ndim
+    real(kind=8), intent(in) :: f(3,3)
+    real(kind=8), intent(out) :: epsl(6)
+    real(kind=8), intent(out) :: gn(3, 3)
+    real(kind=8), intent(out) :: lamb(3)
+    real(kind=8), intent(out) :: logl(3)
+    integer, intent(out) :: iret
+!
+!---------------------------------------------------------------------------------------------------
 !     CALCUL DES DEFORMATIONS LOGARITHMIQUES ET DES TERMES NECESSAIRES
 !     AU POST TRAITEMENT DES CONTRAINTES ET A LA RIGIDITE TANGENTE
 !     SUIVANT ARTICLE MIEHE APEL LAMBRECHT CMAME 2002
-! ----------------------------------------------------------------------
+! --------------------------------------------------------------------------------------------------
 !     IN    NDIM : dimension 2 ou 3
 !     IN    F gradient de la transformation calcule sur config initiale
 !     OUT   EPSL  deformation logarithmique + GN,LAMB,LOGL pour POSLOG
@@ -30,28 +48,25 @@ subroutine deflog(ndim, f, epsl, gn, lamb,&
 !     OUT   LAMB  valeurs propres du tenseur F
 !     OUT   LOGL  log des valeurs propres du tenseur F
 !     OUT   IRET  0=OK, 1=vp(Ft.F) trop petites (compression infinie)
-! ----------------------------------------------------------------------
-#include "asterc/r8miem.h"
-#include "asterfort/diago2.h"
-#include "asterfort/diagp3.h"
-#include "asterfort/lctr2m.h"
-#include "asterfort/pmat.h"
-#include "asterfort/r8inir.h"
-#include "asterfort/tnsvec.h"
-    real(kind=8) :: tr(6), gn(3, 3), epsl33(3, 3), tr2(3), ft(3, 3), gn2(2, 2)
-    real(kind=8) :: f(3, 3), epsl(6), lamb(3), logl(3), f33(3, 3)
-    integer :: nbvec, i, j, k, ndim, iret
-! ----------------------------------------------------------------------
+! --------------------------------------------------------------------------------------------------
 !
+    real(kind=8) :: tr(6), epsl33(3, 3), tr2(3), ft(3, 3), gn2(2, 2)
+    real(kind=8) :: C(3, 3)
+    integer :: i, j, k
+    integer, parameter :: nbvec = 3
+! --------------------------------------------------------------------------------------------------
+!
+    gn = 0.d0
+    lamb = 0.d0
+    logl = 0.d0
     iret  = 0
-    nbvec = 3
 !
-!     LE CALCUL DES VALEURS PROPRES N'A PAS ENCORE ETE FAIT
+!     LE CALCUL DES VALEURS PROPRES N'A PAS ENCORE ETE FAIT: On calcule C
     call lctr2m(3, f, ft)
-    call pmat(3, ft, f, f33)
+    call pmat(3, ft, f, C)
 ! --- VALEURS PRINCIPALES = VECTEURS PROPRES
 !  VECP : DIM1=I=COMPOSANTE DIM2=J=NUM VECTEUR ASSOCIE A LAMBP(J)
-    call tnsvec(3, 3, f33, tr, 1.d0)
+    call tnsvec(3, 3, C, tr, 1.d0)
 !
     if (ndim .eq. 3) then
 !
@@ -60,22 +75,22 @@ subroutine deflog(ndim, f, epsl, gn, lamb,&
 !     pour gagner du temps
 !     --------------------------------
 ! --- MATRICE TR = (XX XY XZ YY YZ ZZ) (POUR DIAGP3)
-        tr(1) = f33(1,1)
-        tr(2) = f33(1,2)
-        tr(3) = f33(1,3)
-        tr(4) = f33(2,2)
-        tr(5) = f33(2,3)
-        tr(6) = f33(3,3)
+        tr(1) = C(1,1)
+        tr(2) = C(1,2)
+        tr(3) = C(1,3)
+        tr(4) = C(2,2)
+        tr(5) = C(2,3)
+        tr(6) = C(3,3)
         call diagp3(tr, gn, lamb)
 !
     else if (ndim.eq.2) then
 !
-        tr2(1)=tr(1)
-        tr2(2)=tr(2)
-        tr2(3)=tr(4)
+        tr2(1) = tr(1)
+        tr2(2) = tr(2)
+        tr2(3) = tr(4)
         call diago2(tr2, gn2, lamb)
-        lamb(3)=tr(3)
-        call r8inir(9, 0.d0, gn, 1)
+        lamb(3) = tr(3)
+!
         do i = 1, 2
             do j = 1, 2
                 gn(i,j)=gn2(i,j)
@@ -86,7 +101,7 @@ subroutine deflog(ndim, f, epsl, gn, lamb,&
     endif
 !
     do i = 1, nbvec
-        if (lamb(i) .le. r8miem()) then
+        if (lamb(i) .le. r8prem()) then
             iret=1
             goto 999
         endif
@@ -94,8 +109,9 @@ subroutine deflog(ndim, f, epsl, gn, lamb,&
     end do
 !
 !     EPSL = DEFORMATION LOGARITHMIQUE
-    call r8inir(9, 0.d0, epsl33, 1)
-    call r8inir(6, 0.d0, epsl, 1)
+    epsl33 = 0.d0
+    epsl = 0.d0
+!
     do i = 1, 3
         do j = 1, 3
             do k = 1, nbvec
@@ -104,7 +120,9 @@ subroutine deflog(ndim, f, epsl, gn, lamb,&
             end do
         end do
     end do
+!
     call tnsvec(3, 3, epsl33, epsl, sqrt(2.d0))
 !
 999 continue
+!
 end subroutine
--- bibfor/algorith/poslog.F90
+++ bibfor/algorith/poslog.F90
@@ -20,11 +20,53 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
                   lgpg, vip, ndim, fp, g,&
                   dtde, sigm, cplan, fami, mate,&
                   instp, angmas, gn, lamb, logl,&
-                  sigp, dsidep, pk2m, pk2, codret)
+                  sigp, dsidep, pk2m, pk2p, codret)
+!
+    implicit none
+!
+#include "asterf_types.h"
+#include "asterc/r8prem.h"
+#include "asterfort/d1macp.h"
+#include "asterfort/deflg2.h"
+#include "asterfort/deflg3.h"
+#include "asterfort/lcdetf.h"
+#include "asterfort/lctr2m.h"
+#include "asterfort/pk2sig.h"
+#include "asterfort/pmat.h"
+#include "asterfort/symt46.h"
+#include "blas/daxpy.h"
+#include "blas/dcopy.h"
+!
+    aster_logical, intent(in) :: resi
+    aster_logical, intent(in) :: rigi
+    aster_logical, intent(in) :: cplan
+    real(kind=8), intent(in) :: tn(6)
+    real(kind=8), intent(in) :: tp(6)
+    real(kind=8), intent(in) :: fm(3, 3)
+    real(kind=8), intent(in) :: fp(3, 3)
+    integer, intent(in) :: ndim
+    integer, intent(in) :: lgpg
+    real(kind=8), intent(out) :: vip(lgpg)
+    integer, intent(in) :: g
+    real(kind=8), intent(in) :: dtde(6,6)
+    real(kind=8), intent(in) :: sigm(2*ndim)
+    character(len=*), intent(in) :: fami
+    integer, intent(in) :: mate
+    real(kind=8), intent(in) :: instp
+    real(kind=8), intent(in) :: angmas(*)
+    real(kind=8), intent(in) :: gn(3, 3)
+    real(kind=8), intent(in) :: lamb(3)
+    real(kind=8), intent(in) :: logl(3)
+    real(kind=8), intent(out) :: sigp(2*ndim)
+    real(kind=8), intent(out) :: dsidep(6, 6)
+    real(kind=8), intent(out) :: pk2m(6)
+    real(kind=8), intent(out) :: pk2p(6)
+    integer, intent(out) :: codret
+! --------------------------------------------------------------------------------------------------
 !     BUT:  POST TRAITEMENT GRANDES DEFORMATIONS 2D ET 3D LOG
 !     SUIVANT ARTICLE MIEHE APEL LAMBRECHT CMAME 2002
-!     CONFIGURATION LAGRANGIENNE
-! ----------------------------------------------------------------------
+!     CONFIGURATION LAGRANGIENNE (PK2)
+! --------------------------------------------------------------------------------------------------
 ! in  resi    : .true. si full_meca/raph_meca .false. si rigi_meca_tang
 ! in  rigi    : .true. si full_meca/rigi_meca_tang
 ! in  tn      : contraintes associees aux def. logarithmiques en t-
@@ -37,7 +79,7 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 ! in  pes     : operateur de transformation tn (ou tp) en pk2
 ! in  g       : numero du points de gauss
 ! in  dtde    : operateur tangent issu de nmcomp (6,6)
-! in  sigm    : contrainte de cauchy en t- 
+! in  sigm    : contrainte de cauchy en t-
 ! in  gn      : termes utiles au calcul de tl dans poslog
 ! in  feta    : termes utiles au calcul de tl dans poslog
 ! in  xi      : termes utiles au calcul de tl dans poslog
@@ -45,36 +87,21 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 ! out sigp    : contraintes de cauchy en t+
 !
 ! aslint: disable=W1504
-    implicit none
-#include "asterf_types.h"
-#include "asterfort/d1macp.h"
-#include "asterfort/deflg2.h"
-#include "asterfort/deflg3.h"
-#include "asterfort/lcdetf.h"
-#include "asterfort/lctr2m.h"
-#include "asterfort/pk2sig.h"
-#include "asterfort/pmat.h"
-#include "asterfort/r8inir.h"
-#include "asterfort/symt46.h"
-#include "blas/daxpy.h"
-#include "blas/dcopy.h"
-    integer :: ndim, i, j, kl, g, lgpg, ivtn, mate, codret
-    character(len=*) :: fami
-    real(kind=8) :: dtde(6, 6), trav(6, 6), trav2(6, 6)
-    real(kind=8) :: pes(6, 6), sigm(2*ndim), sigp(2*ndim), tp2(6), tn(6)
-    real(kind=8) :: fr(3, 3), vip(lgpg)
-    real(kind=8) :: detf, dsidep(6, 6), rac2, instp, angmas(*)
-    real(kind=8) :: pk2(6), tp(6), fp(3, 3), fm(3, 3), pk2m(6)
+!
+    integer :: i, j, kl, ivtn
+    real(kind=8) :: trav(6, 6), trav2(6, 6)
+    real(kind=8) :: pes(6, 6), tp2(6), fr(3, 3), detf
     real(kind=8) :: tl(3, 3, 3, 3), tls(6, 6), epse(4), d1(4, 4)
-    real(kind=8) :: gn(3, 3), feta(4), xi(3, 3), me(3, 3, 3, 3), lamb(3)
-    real(kind=8) :: logl(3)
-    aster_logical :: resi, rigi, cplan
+    real(kind=8) :: feta(4), xi(3, 3), me(3, 3, 3, 3)
+    real(kind=8), parameter :: rac2 = sqrt(2.d0)
 ! ---------------------------------------------------------------------
 !********************CONTRAINTE ET FORCES INTERIEURES******************
 !
-    rac2=sqrt(2.d0)
-    codret=0
-!
+    sigp = 0.d0
+    dsidep = 0.d0
+    pk2m = 0.d0
+    pk2p = 0.d0
+    codret = 0
 !
 !     CALCUL DES PRODUITS SYMETR. DE F PAR N
     if (resi) then
@@ -83,11 +110,11 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
         call dcopy(9, fm, 1, fr, 1)
     endif
 !
-!     DETERMINANT DE LA MATRICE F A L INSTANT T+
+!     DETERMINANT DE LA MATRICE Fr
     call lcdetf(ndim, fr, detf)
 !
 !     PERTINENCE DES GRANDEURS
-    if (detf .le. 1.d-2 .or. detf .gt. 1.d2) then
+    if (detf .le. r8prem()) then
         codret = 1
         goto 999
     endif
@@ -97,10 +124,9 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 ! CF. COMP. METHODES FOR PLASTICITY - DE SOUZA-NIETO, PERIC, OWEN p.603
 !
     if (cplan) then
-        call r8inir(4, 0.d0, epse, 1)
+        epse = 0.d0
         if (resi) then
-            call d1macp(fami, mate, instp, '+', g,&
-                        1, angmas, d1)
+            call d1macp(fami, mate, instp, '+', g, 1, angmas, d1)
             do i = 1, 4
                 do j = 1, 4
                     epse(i)=epse(i)+d1(i,j)*tp(j)
@@ -109,8 +135,7 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 !           EN ELASTICITE ISTROPE
             epse(3)=d1(1,2)*(tp(1)+tp(2))
         else
-            call d1macp(fami, mate, instp, '-', g,&
-                        1, angmas, d1)
+            call d1macp(fami, mate, instp, '-', g, 1, angmas, d1)
             do i = 1, 4
                 do j = 1, 4
                     epse(i)=epse(i)+d1(i,j)*tn(j)
@@ -124,18 +149,15 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 !
 ! ********************* TENSEUR DE PASSAGE DE T A PK2*******************
 !
-    call deflg2(gn, lamb, logl, pes, feta,&
-                xi, me)
-!
+    call deflg2(gn, lamb, logl, pes, feta,xi, me)
 !
 ! *********************MATRICE TANGENTE(SYMETRIQUE)*********************
     if (rigi) then
 !
 !        POUR LA RIGIDITE GEOMETRIQUE : CALCUL AVEC LES PK2
-        call r8inir(6, 0.d0, tp2, 1)
+        tp2 = 0.d0
         if (.not.resi) then
-            call pk2sig(ndim, fm, detf, pk2m, sigm,&
-                        -1)
+            call pk2sig(ndim, fm, detf, pk2m, sigm, -1)
             do kl = 4, 2*ndim
                 pk2m(kl)=pk2m(kl)*rac2
             end do
@@ -144,18 +166,15 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
             call dcopy(6, tp, 1, tp2, 1)
         endif
 !
-        call deflg3(gn, feta, xi, me, tp2,&
-                    tl)
+        call deflg3(gn, feta, xi, me, tp2, tl)
 !
         call symt46(tl, tls)
 !
-        call r8inir(36, 0.d0, dsidep, 1)
         call lctr2m(6, pes, trav)
         call pmat(6, trav, dtde, trav2)
         call pmat(6, trav2, pes, dsidep)
 !
-        call daxpy(36, 1.d0, tls, 1, dsidep,&
-                   1)
+        call daxpy(36, 1.d0, tls, 1, dsidep, 1)
 !
     endif
 !
@@ -163,19 +182,17 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 !
 !        TRANSFORMATION DU TENSEUR T EN PK2
 !
-        call r8inir(6, 0.d0, pk2, 1)
         do i = 1, 6
             do j = 1, 6
-                pk2(i)=pk2(i)+tp(j)*pes(j,i)
+                pk2p(i)=pk2p(i)+tp(j)*pes(j,i)
             end do
         end do
 !        CALCUL DES CONTRAINTES DE CAUCHY, CONVERSION LAGRANGE -> CAUCHY
-        call pk2sig(ndim, fp, detf, pk2, sigp,&
-                    1)
+        call pk2sig(ndim, fp, detf, pk2p, sigp, 1)
 !
-!C       --------------------------------
-!C       pour gagner du temps : on stocke TP comme variable interne
-!C       --------------------------------
+!       --------------------------------
+!       pour gagner du temps : on stocke TP comme variable interne
+!       --------------------------------
         ivtn=lgpg-6+1
         call dcopy(2*ndim, tp, 1, vip(ivtn), 1)
 !
