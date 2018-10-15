841ef05c5d5e2d38f6fd7dcb4398d08019ccdd3a
Jean-Pierre Lefebvre <j-pierre.lefebvre@edf.fr>
2018-10-15 11:22:38 +0200

    [#28041] Upate PETSc version 3.9.4 with superlu
    Former-commit-id: 02884a05a1707bbf2c1ab7672c612ed091ceb5eb

--- waftools/petsc.py
+++ waftools/petsc.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -63,8 +63,8 @@ def check_petsc(self):
     if opts.petsc_libs is None:
         opts.petsc_libs = 'petsc'
         # add optional libs
-        optlibs ='ml HYPRE stdc++'
-    if opts.petsc_libs:
+        optlibs ='ml HYPRE superlu stdc++'
+    if opts.petsc_libs:   
         self.check_petsc_libs(optlibs)
 
     self.check_petsc_headers()
