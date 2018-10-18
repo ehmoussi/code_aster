6899ddcc100849f7bfe11789537fdc2867d12dca
Nicolas Sellenet <nicolas.sellenet@edf.fr>
2018-10-18 14:37:51 +0200

    [#28158] modification of ischar_iden
    Former-commit-id: e3811450f5019ce1bbcfb748e5296fb7b962df81

--- bibfor/load_util/ischar.F90
+++ bibfor/load_util/ischar.F90
@@ -105,15 +105,13 @@ implicit none
 ! - Choice: all loads or not ?
 !
     if (present(i_load_)) then
-        ischar_iload = ischar_iden(v_load_info, i_load_, nb_load, load_type_1, load_type_2,&
-                                   load_name=v_load_name(i_load_))
+        ischar_iload = ischar_iden(v_load_info, i_load_, nb_load, load_type_1, load_type_2)
     else
         do i_load = 1, nb_load
 !
 ! --------- Identify type of load 
 !
-            ischar_iload = ischar_iden(v_load_info, i_load, nb_load, load_type_1, load_type_2,&
-                                       load_name=v_load_name(i_load))
+            ischar_iload = ischar_iden(v_load_info, i_load, nb_load, load_type_1, load_type_2)
 !
 ! --------- Flag for function
 !    
