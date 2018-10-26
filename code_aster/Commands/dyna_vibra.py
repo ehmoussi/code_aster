2f6eabb71e630d43bc740870a2170e0c36e5ccdc
Nicolas Sellenet <nicolas.sellenet@edf.fr>
2018-10-26 09:11:29 +0200

    [#28158] fix xxPostDynaModaT
    Former-commit-id: 271c5b742ec323d01f9dc1b17ffc0b014164b0f5

--- code_aster/Commands/dyna_vibra.py
+++ code_aster/Commands/dyna_vibra.py
@@ -60,7 +60,8 @@ class VibrationDynamics(ExecuteCommand):
             self._result.update()
         if keywords["BASE_CALCUL"] == "GENE":
             dofNum = keywords["MATR_RIGI"].getGeneralizedDOFNumbering()
-            self._result.setGeneralizedDOFNumbering(dofNum)
+            if isinstance(self._result, HarmoGeneralizedResultsContainer):
+                self._result.setGeneralizedDOFNumbering(dofNum)
 
 
 DYNA_VIBRA = VibrationDynamics.run
