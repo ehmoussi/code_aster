4e790a523b3bb4b4b84524b75cd43d1533500125
Nicolas Sellenet <nicolas.sellenet@edf.fr>
2018-10-31 15:02:35 +0100

    [#28190] fix getvc8 for 'RI' and 'MP' cases
    Former-commit-id: 720a711557ab9abf8e358ea71616580f20a87c44

--- bibfor/op/op0157.F90
+++ bibfor/op/op0157.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -212,6 +212,7 @@ subroutine op0157()
         call jedetr('&&OP0157.NUME_ORDRE')
         call jedetr('&&OP0157.DISCRET')
  10 end do
+    call ulopen(-ifi, ' ', fich, ' ', 'O')
 !
     call jedema()
 end subroutine
