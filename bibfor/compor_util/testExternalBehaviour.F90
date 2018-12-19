9b4c4a186957d83fe28eccc46da4a50585d2d7f5
Astrid Filiot <astrid.filiot@edf.fr>
2018-12-19 14:01:41 +0100

    [#28020] Not calling comp_meca_l in lcExternalStateVariable anymore
    Former-commit-id: ab9901c21c589e9172014b732c304e2068ebe244

---
+++ bibfor/compor_util/testExternalBehaviour.F90
@@ -0,0 +1,53 @@
+! --------------------------------------------------------------------
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+! This file is part of code_aster.
+!
+! code_aster is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! code_aster is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+! --------------------------------------------------------------------
+!
+subroutine testExternalBehaviour(carcri, l_mfront, l_umat)
+!
+implicit none
+!
+#include "asterf_types.h"
+!
+real(kind=8), intent(in) :: carcri(*)
+aster_logical, intent(out) :: l_mfront, l_umat
+!
+! --------------------------------------------------------------------------------------------------
+!
+! Behaviour
+!
+! Test if the behaviour is external (mfront or umat)
+!
+! --------------------------------------------------------------------------------------------------
+!
+! In  carcri           : parameters for comportment
+! Out l_mfront         : logical for mfront behaviour
+! Out l_umat           : logical for umat behaviour
+!
+! --------------------------------------------------------------------------------------------------
+!
+    l_mfront = ASTER_FALSE
+    l_umat = ASTER_FALSE
+!
+    if (nint(carcri(16)) .ne. 0) then
+        if (nint(carcri(15)) .ne. 0) then
+            l_mfront = ASTER_TRUE
+        else
+            l_umat = ASTER_TRUE
+        endif
+    endif
+!    
+end subroutine
