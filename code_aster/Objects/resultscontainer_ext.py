5167039090dbd88a0223202c64d0014adc0aab68
Nicolas Sellenet <nicolas.sellenet@edf.fr>
2018-12-20 16:16:46 +0100

    [#28057] add MaterialOnMesh in saved state of ResultsContainer
    Former-commit-id: d263c796d4bee24215240d795adbbcf406749a94

--- code_aster/Objects/resultscontainer_ext.py
+++ code_aster/Objects/resultscontainer_ext.py
@@ -39,7 +39,7 @@ class ExtendedResultsContainer(injector(ResultsContainer), ResultsContainer):
         Returns:
             dict: Internal state.
         """
-        return (True, self.getModel(), )
+        return (True, self.getModel(), self.getMaterialOnMesh(),)
 
     def __setstate__(self, state):
         """Restore internal state.
@@ -49,6 +49,8 @@ class ExtendedResultsContainer(injector(ResultsContainer), ResultsContainer):
         """
         if state[1] is not None:
             self.appendModelOnAllRanks(state[1])
+        if state[2] is not None:
+            self.appendMaterialOnMeshOnAllRanks(state[2])
 
     def LIST_VARI_ACCES (self):
         if not self.accessible():
