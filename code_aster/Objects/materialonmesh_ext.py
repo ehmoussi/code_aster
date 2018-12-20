0ff89099311a7b5be923d42367638c46630c3c02
Nicolas Sellenet <nicolas.sellenet@edf.fr>
2018-12-20 16:17:26 +0100

    [#28057] save internal state of MaterialOnMesh
    Former-commit-id: dd8dfc126c6d1aadfdf1f37d4108294b3c363eb2

--- code_aster/Objects/materialonmesh_ext.py
+++ code_aster/Objects/materialonmesh_ext.py
@@ -25,6 +25,7 @@
 
 import aster
 from libaster import MaterialOnMesh
+from libaster import EntityType
 
 from ..Utilities import injector
 
@@ -37,3 +38,68 @@ class ExtendedMaterialOnMesh(injector(MaterialOnMesh), MaterialOnMesh):
         object during unpickling.
         """
         return (self.getName(), self.getSupportMesh())
+
+    def __getstate__(self):
+        """Return internal state.
+
+        Returns:
+            dict: Internal state.
+        """
+        vecOfPartOfMaterialOnMesh = self.getVectorOfPartOfMaterialOnMesh()
+        state = ()
+        for part in self.getVectorOfPartOfMaterialOnMesh():
+            vecOfMater = part.getVectorOfMaterial()
+            partState = (len(vecOfMater),) + tuple(vecOfMater)
+            names = part.getMeshEntity().getNames()
+            type = part.getMeshEntity().getType()
+            if type is EntityType.AllMeshEntitiesType:
+                partState = partState + (0, names, )
+            elif type is EntityType.GroupOfElementsType:
+                partState = partState + (1, names, )
+            elif type is EntityType.ElementType:
+                partState = partState + (2, names, )
+            else:
+                assert False
+            state = state + partState
+        return state
+
+    def __setstate__(self, state):
+        """Restore internal state.
+
+        Arguments:
+            state (dict): Internal state.
+        """
+        if state is not ():
+            nbMater = len(state)
+
+            searchForSize = True
+            endMater = 0
+            listOfMater = []
+            type = -1
+            for i in range(nbMater):
+                if i <= endMater and not searchForSize:
+                    listOfMater.append(state[i])
+                if searchForSize:
+                    endMater = i + state[i]
+                    searchForSize = False
+                if i == endMater + 1:
+                    if state[i] == 0:
+                        type = EntityType.AllMeshEntitiesType
+                    elif state[i] == 1:
+                        type = EntityType.GroupOfElementsType
+                    elif state[i] == 2:
+                        type = EntityType.ElementType
+                    else:
+                        assert False
+                if i == endMater + 2:
+                    names =  state[i]
+                    if type is EntityType.AllMeshEntitiesType:
+                        self.addMaterialsOnAllMesh(listOfMater)
+                    elif type is EntityType.GroupOfElementsType:
+                        self.addMaterialsOnAllMesh(listOfMater, names)
+                    elif type is EntityType.ElementType:
+                        self.addMaterialsOnAllMesh(listOfMater, names)
+                    searchForSize = True
+                    endMater = 0
+                    listOfMater = []
+                    type = -1
