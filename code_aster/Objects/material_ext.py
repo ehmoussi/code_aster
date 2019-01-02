7a8671523c97403fc9241256c06b4cb9887b1410
Nicolas Sellenet <nicolas.sellenet@edf.fr>
2019-01-02 09:25:52 +0100

    [#28057] fix copyrigth
    Former-commit-id: feec1b11e122bdd5d4a56328fd9d903e0c12fa03

--- code_aster/Objects/material_ext.py
+++ code_aster/Objects/material_ext.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -57,4 +57,3 @@ class ExtendedMaterial(injector(Material), Material):
             UTMESS('F', 'SDVERI_5')
         # appel à l'interface Python/C
         return aster.rcvale(self.nom, phenomene, nompar, valpar, nomres, stop)
-
