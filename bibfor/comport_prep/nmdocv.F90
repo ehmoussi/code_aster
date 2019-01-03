a92bd46834501c8d42d3ec6221f6b3f8ba77d1a0
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-01-03 13:41:08 +0100

    [#28339] Fix uninitialized variable.
    Former-commit-id: db75abda26a92440c55431ecf253e565fd085a50

--- bibfor/comport_prep/nmdocv.F90
+++ bibfor/comport_prep/nmdocv.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -55,6 +55,7 @@ subroutine nmdocv(keywordfact, iocc, algo_inte, keyword, value)
 ! --------------------------------------------------------------------------------------------------
 !
     ASSERT(keyword(1:9).eq.'RESI_INTE'.or.keyword.eq.'ITER_INTE_MAXI')
+    value = 0.d0
 !
 ! - Get Values
 !
