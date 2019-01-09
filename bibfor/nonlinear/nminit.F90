c4320c54ad7f0669eea8fd335fa7eef52d7492d3
Micka?l Abbas <mickael.abbas@edf.fr>
2019-01-09 11:33:47 +0100

    [#28337] No POU_D_EM elements for CRIT_STAB and MODE_VIBR
    Former-commit-id: 03d0979adbd1018777886abdf7900686070b3331

--- bibfor/nonlinear/nminit.F90
+++ bibfor/nonlinear/nminit.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -189,7 +189,7 @@ type(ROM_DS_AlgoPara), intent(inout) :: ds_algorom
 !
 ! - Initializations for post-treatment at each time step
 !
-    call nonlinDSPostTimeStepInit(ds_inout%result,&
+    call nonlinDSPostTimeStepInit(ds_inout%result, model          ,&
                                   ds_algopara    , ds_constitutive, ds_posttimestep)
 !
 ! - Prepare list of loads (and late elements) for contact
