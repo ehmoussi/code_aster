1bd4d8bfecac02694f4147f3feaccdcb31f44d82
Etienne Cheignon <etienne.cheignon@necs.fr>
2019-01-20 23:43:25 +0100

    [#24373] New GLRC_DM model
    Former-commit-id: b8519c895e044939e8e5466679316bb15283cc50

--- bibfor/include/asterfort/dgseui.h
+++ bibfor/include/asterfort/dgseui.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -20,25 +20,23 @@
 !
 interface
     subroutine dgseui(em, num, ef, nuf, eb,&
-                      nub, sytb, h, icisai, syt,&
-                      syc, dxd, syf, drd, pelast,&
-                      pelasf, icompr)
+                      nub, ftj, h, icisai, syt,&
+                      dxd, syf, drd, pelast,&
+                      pelasf)
         real(kind=8) :: em
         real(kind=8) :: num
         real(kind=8) :: ef
         real(kind=8) :: nuf
         real(kind=8) :: eb
         real(kind=8) :: nub
-        real(kind=8) :: sytb
+        real(kind=8) :: ftj
         real(kind=8) :: h
         integer :: icisai
         real(kind=8) :: syt
-        real(kind=8) :: syc
         real(kind=8) :: dxd
         real(kind=8) :: syf
         real(kind=8) :: drd
         real(kind=8) :: pelast
         real(kind=8) :: pelasf
-        integer :: icompr
     end subroutine dgseui
 end interface
