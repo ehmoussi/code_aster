c5e641f30efb61f0020b6c6ed78c5212cc8a7f89
Vinicius Alves Fernandes <vinicius.alves-fernandes@edf.fr>
2019-02-04 21:42:15 +0100

    [#28471] Fix error message for CALC_ESSAI_GEOMECA on pressure convention signal
    Former-commit-id: 434e9611cdbcd2e4e515422319736aa4de1d103d

--- bibpyt/Messages/compor2.py
+++ bibpyt/Messages/compor2.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -190,7 +190,7 @@ Risques & conseils :
 
     32: _(u"""
   CALC_ESSAI_GEOMECA : Erreur dans la saisie du mot clef facteur <%(k1)s> (occurrence %(i1)d).
-  La valeur <%(r1)E> a été renseignée pour le mot clef simple <%(k2)s>, au lieu d'une valeur strictement négative.
+  La valeur <%(r1)E> a été renseignée pour le mot clef simple <%(k2)s>, au lieu d'une valeur strictement positive.
 """),
 
     33: _(u"""
