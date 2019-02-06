93dff98bf293c3e80649a56679e340eff4add8e5
Nicolas Sellenet <nicolas.sellenet@edf.fr>
2019-02-06 15:38:41 +0100

    [#28472] free logical unit file open by impr_co
    Former-commit-id: 62a334c5cc80a119a35a1a2b16178221f5f3f479

--- bibfor/op/op0017.F90
+++ bibfor/op/op0017.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -114,5 +114,7 @@ subroutine op0017()
         call utimsd(ifi, nivo, lattr, lcont, ' ',&
                     0, base, perm=perm)
     endif
+    flush(ifi)
+    call ulopen(-ifi, ' ', ' ', ' ', ' ')
     call jedema()
 end subroutine
