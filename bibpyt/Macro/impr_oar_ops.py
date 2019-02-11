8c3b09b43b85354e58569fc32846f6f1a114ad7f
Nicolas Sellenet <nicolas.sellenet@edf.fr>
2019-02-11 15:18:17 +0100

    [#28497] Change use of RESU_MECA keyword in impr_oar
    Former-commit-id: c758441c1bbad2b81dba934167f1b0c18e21a2bd

--- bibpyt/Macro/impr_oar_ops.py
+++ bibpyt/Macro/impr_oar_ops.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -714,6 +714,9 @@ class tuyauterie(OAR_element):
         self.nodeComp = XMLNode("TUYAUTERIE")
 
         self.para_resu_meca = args['RESU_MECA']
+        if len(self.para_resu_meca) > 1:
+            assert False
+        self.para_resu_meca = self.para_resu_meca[0]
         self.num_char = self.para_resu_meca['NUM_CHAR']
 
         # Gestion du maillage
