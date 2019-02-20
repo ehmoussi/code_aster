7c4f85e8eb379d8d610ad8372c22ad2739653ed3
Maximilien Siavelis <maximilien.siavelis@necs.fr>
2019-02-20 11:34:34 +0100

    [#28038] update for eole py3 prerequisites
    Former-commit-id: 1b099df9639d8720b8daf6b8fdb7f16a46977ccf

--- wafcfg/eole_std.py
+++ wafcfg/eole_std.py
@@ -29,6 +29,7 @@ waf install -p
 import os
 ASTER_ROOT = os.environ['ASTER_ROOT']
 YAMMROOT = os.environ['ROOT_SALOME']
+YAMMROOT_OLD = os.environ['ROOT_SALOME_OLD']
 
 import intel
 import official_programs
@@ -47,7 +48,7 @@ def configure(self):
     self.env.append_value('OPT_ENV', [
         'module unload mkl',
         'module load ifort/2016.0.047 icc/2016.0.047 mkl/2016.0.047',
-        'export PATH=' + YAMMROOT + '/prerequisites/Medfichier-331/bin:$PATH',
+        'export PATH=' + YAMMROOT_OLD + '/prerequisites/Medfichier-331/bin:$PATH',
         'export OPENBLAS_CORETYPE=SANDYBRIDGE'])
 
     TFELHOME = YAMMROOT + '/prerequisites/Mfront-TFEL311_aster'
@@ -56,21 +57,21 @@ def configure(self):
     self.env.TFELVERS = TFELVERS
 
     self.env.append_value('LIBPATH', [
-        YAMMROOT + '/prerequisites/Hdf5-1814/lib',
-        YAMMROOT + '/prerequisites/Medfichier-331/lib',
+        YAMMROOT_OLD + '/prerequisites/Hdf5-1814/lib',
+        YAMMROOT_OLD + '/prerequisites/Medfichier-331/lib',
         YAMMROOT + '/prerequisites/Metis_aster-510_aster4/lib',
         YAMMROOT + '/prerequisites/Scotch_aster-604_aster7/SEQ/lib',
-        YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/SEQ/lib',
+        YAMMROOT + '/prerequisites/Mumps-512_consortium_aster4/SEQ/lib',
         TFELHOME + '/lib',
     ])
 
     self.env.append_value('INCLUDES', [
-        YAMMROOT + '/prerequisites/Hdf5-1814/include',
-        YAMMROOT + '/prerequisites/Medfichier-331/include',
+        YAMMROOT_OLD + '/prerequisites/Hdf5-1814/include',
+        YAMMROOT_OLD + '/prerequisites/Medfichier-331/include',
         YAMMROOT + '/prerequisites/Metis_aster-510_aster4/include',
         YAMMROOT + '/prerequisites/Scotch_aster-604_aster7/SEQ/include',
-        YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/SEQ/include',
-        YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/SEQ/include_seq',
+        YAMMROOT + '/prerequisites/Mumps-512_consortium_aster4/SEQ/include',
+        YAMMROOT + '/prerequisites/Mumps-512_consortium_aster4/SEQ/include_seq',
         TFELHOME + '/include',
     ])
 
@@ -82,6 +83,6 @@ def configure(self):
     opts.enable_metis = True
     opts.enable_mumps = True
     opts.enable_scotch = True
-    opts.enable_mfront = True
+    opts.enable_mfront = False
 
     opts.enable_petsc = False
