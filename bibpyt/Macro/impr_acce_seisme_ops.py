63e4538ff0d017184fed98dddb19e04bca468a27
Nicolas Sellenet <nicolas.sellenet@edf.fr>
2019-03-06 15:45:44 +0100

    [#28609] change output file name in impr_acce_seisme
    Former-commit-id: d84d31e1cbb13bfd4c36a09e32623b5947f4c315

--- bibpyt/Macro/impr_acce_seisme_ops.py
+++ bibpyt/Macro/impr_acce_seisme_ops.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -177,7 +177,7 @@ def impr_acce_seisme_ops(self, **args):
     nom_comp = ["X","Y","Z"]
 
     tab_data = TABLE.EXTR_TABLE()
-    nom_table = TABLE.get_name()
+    nom_table = TABLE.getName()[:8]
 
     if not 'FONCTION' in tab_data.para:
         UTMESS('F','SEISME_88')
