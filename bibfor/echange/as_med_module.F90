6c5cab114e08689603a906a120b99a981c8d4d69
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-03-11 17:43:24 +0100

    [#28618] Add MED_VERSION keyword to select output version.
    Former-commit-id: 78b03b7d52c13cfbc59673dba54afd5980eb9438

--- bibfor/echange/as_med_module.F90
+++ bibfor/echange/as_med_module.F90
@@ -19,45 +19,65 @@
 module as_med_module
     implicit none
     private
-
-! default version to be used for output
-    integer, parameter :: DEF_BKWD_VERS(3) = (/3, 3, 1/)
-
-    ! currently used version
-    integer :: bkwd_vers(3) = (/0, 0, 0/)
-
+!
+! default version to be used for output for backward compatibility
+    integer, parameter :: bkwd_vers(3) = (/3, 3, 1/)
+!
 #include "asterc/write33header.h"
 #include "asterfort/as_mfiope.h"
 #include "asterfort/as_mfivop.h"
+#include "asterfort/utmess.h"
 
     public :: as_med_open
 
 contains
-
-    subroutine init()
-        bkwd_vers = DEF_BKWD_VERS
-    end subroutine init
-
-!>  Open med file
-    subroutine as_med_open(fid, nom, acces, cret)
+!
+!>  Open a med file in read, write or read+write mode.
+!>  In write mode (creation of a new file) the version of the med format may be
+!>  selected. The default version is (3, 3, 1).
+!>  Other possible value for 'vers' is (4, 0, 0).
+!>  The version value is ignored in other opening modes.
+!
+!>  @param[out]     fid     file identifier
+!>  @param[in]      nom     filename
+!>  @param[in]      acces   open mode (see med.h for details)
+!>  @param[out]     cret    exit code
+!>  @param[in]      vers    med version number to be used, only for creation (optional)
+subroutine as_med_open(fid, nom, acces, cret, vers)
 !
         aster_int, intent(out) :: fid
         character(len=*), intent(in) :: nom
         aster_int, intent(in) :: acces
         aster_int, intent(out) :: cret
+        aster_int, optional :: vers(3)
 !
         integer, parameter :: med_acc_rdwr = 1, med_acc_creat = 3
-        call init()
+        integer :: mode, uvers(3)
 !
-        if (acces .eq. med_acc_creat) then
-            call write33header(nom)
-            call as_mfivop(fid, nom, med_acc_rdwr, &
-                           bkwd_vers(1), bkwd_vers(2), bkwd_vers(3), &
+        mode = acces
+        if (.not. present(vers)) then
+            uvers = bkwd_vers
+        else
+            uvers = vers
+        endif
+        !
+        if (mode .eq. med_acc_creat) then
+            if (uvers(1) .eq. 4 .and. uvers(2) .eq. 0) then
+!               pass
+            elseif (uvers(1) .eq. 3 .and. uvers(2) .eq. 3) then
+                call write33header(nom)
+                mode = med_acc_rdwr
+            else
+                 call utmess('F', 'MED_9', ni=3, vali=uvers)
+            endif
+            call utmess('I', 'MED_8', ni=3, vali=uvers)
+            call as_mfivop(fid, nom, mode, &
+                           uvers(1), uvers(2), uvers(3), &
                            cret)
         else
             call as_mfiope(fid, nom, acces, cret)
         endif
 !
     end subroutine as_med_open
-
+!
 end module as_med_module
