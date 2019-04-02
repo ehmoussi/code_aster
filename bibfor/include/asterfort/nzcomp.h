4997bdf5ec570427ce443da91c04ba3037f31623
Micka?l Abbas <mickael.abbas@edf.fr>
2019-04-02 19:27:14 +0200

    [#28674] New constants for metallurgy
    Former-commit-id: 4ddd431c5dec98f193dd3a95aaa6775fd026b12e

--- bibfor/include/asterfort/nzcomp.h
+++ bibfor/include/asterfort/nzcomp.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -17,14 +17,14 @@
 ! --------------------------------------------------------------------
 !
 interface
-    subroutine nzcomp(jv_mater , metaPara , nume_comp,&
+    subroutine nzcomp(jv_mater , metaPara , nume_comp, nb_phase,&
                       dt10     , dt21     , inst2    ,&
                       tno0     , tno1     , tno2     ,&
                       meta_prev, meta_curr)
         use Metallurgy_type
         integer, intent(in) :: jv_mater
         type(META_MaterialParameters), intent(in) :: metaPara
-        integer, intent(in) :: nume_comp
+        integer, intent(in) :: nume_comp, nb_phase
         real(kind=8), intent(in) :: dt10, dt21, inst2
         real(kind=8), intent(in) :: tno0, tno1, tno2
         real(kind=8), intent(in) :: meta_prev(*)
