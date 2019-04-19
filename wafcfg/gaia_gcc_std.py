8f447f65e62597bcdaf993ad10955ef472f6b79b
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-04-19 14:45:08 +0200

    [#23037] Store GCC configuration for Gaia.
    Former-commit-id: 1076b79b7b69164f68b01a40ac6b20c6a170b47a

---
+++ wafcfg/gaia_gcc_std.py
@@ -0,0 +1,93 @@
+# coding=utf-8
+# --------------------------------------------------------------------
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# This file is part of code_aster.
+#
+# code_aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# code_aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------
+
+"""
+Configuration for Gaia
+
+. $HOME/dev/codeaster/devtools/etc/env_unstable.sh
+
+waf configure --use-config=gaia_gcc_std --prefix=../install/std
+waf install -p
+"""
+
+import os
+ASTER_ROOT = os.environ['ASTER_ROOT']
+YAMMROOT = os.environ['ROOT_SALOME'] 
+#YAMMROOT='/projets/simumeca/public_gcc'
+
+import official_programs
+
+
+def configure(self):
+    opts = self.options
+
+    official_programs.configure(self)
+    official_programs.check_prerequisites_package(self, YAMMROOT, '20190402')
+
+    self.env.append_value('CXXFLAGS', ['-D_GLIBCXX_USE_CXX11_ABI=0'])
+    # ADDMEM value is evaluated with DEBUT()/FIN() execution and looking
+    # at value reported at "MAXIMUM DE MEMOIRE UTILISEE PAR LE PROCESSUS".
+    self.env['ADDMEM'] = 1600
+
+    self.env.append_value('OPT_ENV', [
+        'export OPENBLAS_CORETYPE=SANDYBRIDGE',
+    ])
+
+    TFELHOME = YAMMROOT + '/prerequisites/Mfront-TFEL311_aster'
+    TFELVERS = '3.1.1'
+    self.env.TFELHOME = TFELHOME
+    self.env.TFELVERS = TFELVERS
+
+    self.env.append_value('LIBPATH', [
+        YAMMROOT + '/prerequisites/Hdf5-1103/lib',
+        YAMMROOT + '/prerequisites/Medfichier-400/lib',
+        YAMMROOT + '/prerequisites/Metis_aster-510_aster4/lib',
+        YAMMROOT + '/prerequisites/Scotch_aster-604_aster7/SEQ/lib',
+        YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/SEQ/lib',
+        TFELHOME + '/lib',
+    ])
+
+    self.env.append_value('INCLUDES', [
+        YAMMROOT + '/prerequisites/Hdf5-1103/include',
+        YAMMROOT + '/prerequisites/Medfichier-400/include',
+        YAMMROOT + '/prerequisites/Metis_aster-510_aster4/include',
+        YAMMROOT + '/prerequisites/Scotch_aster-604_aster7/SEQ/include',
+        YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/SEQ/include',
+        YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/SEQ/include_seq',
+        TFELHOME + '/include',
+    ])
+
+    # waflib/extras/boost.py:check_boost forces /usr/lib/x86_64-linux-gnu
+    self.env.INCLUDES_BOOST = YAMMROOT + '/prerequisites/Boost-1580/include'
+    self.env.LIBPATH_BOOST = [YAMMROOT + '/prerequisites/Boost-1580/lib']
+    self.env.LIB_BOOST = ['boost_python-mt']
+
+    self.env.append_value('LIB', ('pthread', 'util'))
+    self.env.append_value('LIB_SCOTCH', ('scotcherrexit'))
+    # to fail if not found
+    opts.enable_hdf5 = True
+    opts.enable_med = True
+    opts.enable_metis = True
+    opts.enable_mumps = True
+    opts.enable_scotch = True
+    opts.enable_mfront = True
+
+    opts.enable_petsc = False
+    # sphinx with python3 not available
+    opts.enable_doc = False
