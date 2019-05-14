d3a84cbe4deaaa2e391eb5870b535dbb5854c68f
Stefano Cherubini <stefano.cherubini@edf.fr>
2019-05-14 11:40:41 +0200

    [#28702, #28627] Update catalog absorbing and fluid elements
    Former-commit-id: 3b91344530ffae0fde05a9faa6fdcfa0ebea77a1

--- catalo/cataelem/Options/char_meca_hydr_r.py
+++ catalo/cataelem/Options/char_meca_hydr_r.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -51,6 +51,7 @@ CHAR_MECA_HYDR_R = Option(
     ),
     condition=(
         CondCalcul('+', ((AT.PHENO, 'ME'), (AT.BORD, '0'),)),
+        CondCalcul('-', ((AT.PHENO, 'ME'), (AT.ABSO,'OUI'),)),
         CondCalcul('-', ((AT.PHENO, 'ME'), (AT.INTERFACE, 'OUI'),)),
         CondCalcul('-', ((AT.PHENO, 'ME'), (AT.FLUIDE, 'OUI'),)),
         CondCalcul('-', ((AT.PHENO, 'ME'), (AT.MODELI, '3FI'),)),
