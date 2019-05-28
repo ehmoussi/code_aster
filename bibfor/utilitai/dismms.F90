586441d485b59b31c0be246a1bb35247629da46b
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-05-28 15:50:07 +0200

    [#28761] Fix 'ldist' argument calling 'echmat'.
    Former-commit-id: b39e4c4ba3ff6e4f46cd20482f8d01d3b4ea5544

--- bibfor/utilitai/dismms.F90
+++ bibfor/utilitai/dismms.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -178,8 +178,7 @@ subroutine dismms(questi, nomobz, repi, repkz, ierd)
             repk='NON'
         endif
 
-        else if((questi.eq.'CHAM_MATER').or. (questi.eq.'CARA_ELEM'))&
-    then
+    else if((questi.eq.'CHAM_MATER').or. (questi.eq.'CARA_ELEM')) then
         call jeveuo(nomob//'.LIME', 'L', vk24=lime)
         call jelira(nomob//'.LIME', 'LONMAX', nblime)
         p1=' '
