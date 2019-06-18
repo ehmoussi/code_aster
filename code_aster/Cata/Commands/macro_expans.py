fc251bea8049c725daea915f59183ee2049043fd
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-06-18 11:35:32 +0200

    [#28429] Fix/remove rules on keywords with default.          Remove rule (ARCHIVAGE in DYNA_VIBRA)
    Former-commit-id: c10880c9e4528ae0bbaec8c03f3223771710e9c4

--- code_aster/Cata/Commands/macro_expans.py
+++ code_aster/Cata/Commands/macro_expans.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -73,7 +73,7 @@ MACRO_EXPANS=MACRO(nom="MACRO_EXPANS",
                            REGUL   =SIMP(statut='f',typ='TXM',defaut="NON",into=("NON","NORM_MIN","TIK_RELA",) ),
                            b_regul =BLOC(condition="""not equal_to("REGUL", 'NON')""",
                                          regles=(PRESENT_ABSENT('COEF_PONDER','COEF_PONDER_F', ),),
-                                         COEF_PONDER   =SIMP(statut='f',typ='R',defaut=0.     ,max='**' ),
+                                         COEF_PONDER   =SIMP(statut='f',typ='R',max='**' ),
                                          COEF_PONDER_F =SIMP(statut='f',typ=(fonction_sdaster,nappe_sdaster,formule),max='**' ),
                                         ),
                                              ),
