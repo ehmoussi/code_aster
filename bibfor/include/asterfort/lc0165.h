1abfdaadd54c0e592b8b237f9f6aad958579b3c8
Micka?l Abbas <mickael.abbas@edf.fr>
2019-07-02 09:35:14 +0200

    [#26675] Suppress useless wkin in lcxxxx.F90 subroutines
    Former-commit-id: df76afbe498a79ac4604653b8095280840a09a76

--- bibfor/include/asterfort/lc0165.h
+++ bibfor/include/asterfort/lc0165.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -17,31 +17,20 @@
 ! --------------------------------------------------------------------
 !
 interface
-    subroutine lc0165(fami, kpg, ksp, ndim, imate,&
-                      compor, carcri, instam, instap, epsm,&
-                      deps, sigm, vim, option, angmas,&
-                      sigp, vip, wkin, typmod, &
-                      nvi, dsidep, codret)
+    subroutine lc0165(fami  , kpg   , ksp   , ndim  , imate ,&
+                      compor, instam, instap, epsm  ,&
+                      deps  , sigm  , vim   , option,&
+                      sigp  , vip   , typmod,&
+                      dsidep, codret)
         character(len=*), intent(in) :: fami
-        integer, intent(in) :: kpg
-        integer, intent(in) :: ksp
-        integer, intent(in) :: ndim
-        integer, intent(in) :: imate
+        integer, intent(in) :: kpg, ksp, ndim, imate
         character(len=16), intent(in) :: compor(*)
-        real(kind=8), intent(in) :: carcri(*)
-        real(kind=8), intent(in) :: instam
-        real(kind=8), intent(in) :: instap
-        real(kind=8), intent(in) :: epsm(6)
-        real(kind=8), intent(in) :: deps(6)
-        real(kind=8), intent(in) :: sigm(6)
+        real(kind=8), intent(in) :: instam, instap
+        real(kind=8), intent(in) :: epsm(6), deps(6), sigm(6)
         real(kind=8), intent(in) :: vim(*)
         character(len=16), intent(in) :: option
-        real(kind=8), intent(in) :: angmas(*)
-        real(kind=8), intent(out) :: sigp(6)
-        real(kind=8), intent(out) :: vip(*)
-        real(kind=8), intent(in) :: wkin(*)
+        real(kind=8), intent(out) :: sigp(6), vip(*)
         character(len=8), intent(in) :: typmod(*)
-        integer, intent(in) :: nvi
         real(kind=8), intent(out) :: dsidep(6,6)
         integer, intent(out) :: codret
     end subroutine lc0165
