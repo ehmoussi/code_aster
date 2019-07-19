bae8c894c1cf17158b89ebadb03d1409ca6ae637
Natacha B?reux <natacha.bereux@edf.fr>
2019-07-19 11:14:45 +0200

    [#28993] Use pcpiv and usersm in LDLT_SP
    Former-commit-id: a5a340ca23a2a2fcdcd0e864d71a8adaac23e142

--- bibfor/petsc/appcrs.F90
+++ bibfor/petsc/appcrs.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -54,10 +54,10 @@ use petsc_data_module
 !     VARIABLES LOCALES
     integer :: rang, nbproc
     integer :: jslvk, jslvr, jslvi, jnequ, jnequl, jprddl, jcoll, nloc
-    integer :: niremp, nsmdi
+    integer :: niremp, nsmdi, pcpiv
     mpi_int :: mpicou
 !
-    character(len=24) :: precon
+    character(len=24) :: precon, usersm
     character(len=19) :: nomat, nosolv
     character(len=14) :: nonu
 !
@@ -138,7 +138,9 @@ use petsc_data_module
     else if (precon.eq.'LDLT_SP') then
 !        CREATION SOLVEUR BIDON SIMPLE PRECISION
         spsomu = zk24(jslvk-1+3)(1:19)
-        call crsmsp(spsomu, nomat, 0, 'IN_CORE')
+        pcpiv = zi(jslvi-1+7)
+        usersm = zk24(jslvk-1+9)
+        call crsmsp(spsomu, nomat, pcpiv, usersm )
 !        CREATION DES VECTEURS TEMPORAIRES UTILISES DANS LDLT_SP
         if (lmd) then
             call jeveuo(nonu//'.NUME.NEQU', 'L', jnequ)
