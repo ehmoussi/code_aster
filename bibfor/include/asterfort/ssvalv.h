e598c2b93b18ba54b8bb89f380ee88b02e619dc5
Georges Devesa <georges-cc.devesa at edf.fr>
2019-09-12 14:35:35 +0200

    [#28966] condensation of evolutions of loads by MACR_ELEM_DYNA
    Former-commit-id: 73bedde40a466e043ec46291d9eb4ed977bc48a1

--- bibfor/include/asterfort/ssvalv.h
+++ bibfor/include/asterfort/ssvalv.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -20,7 +20,7 @@
 !
 interface
     subroutine ssvalv(statut, nomcas, mo, ma, isma,&
-                      idresl, long)
+                      idresl, long, instap)
         character(len=*) :: statut
         character(len=8) :: nomcas
         character(len=8) :: mo
@@ -28,5 +28,6 @@ interface
         integer :: isma
         integer :: idresl
         integer :: long
+        real(kind=8), optional :: instap
     end subroutine ssvalv
 end interface
