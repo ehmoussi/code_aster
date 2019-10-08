d33d71610abb087b846abbb6ea0bf488aa1ca551
Stefano Cherubini <stefano.cherubini@edf.fr>
2019-10-08 13:16:14 +0200

    [#29097] refe_forc_noda issue
    Former-commit-id: 742d38dc0d28f65621224a13c16a0f4a8d0750fc

--- catalo/cataelem/Options/refe_forc_noda.py
+++ catalo/cataelem/Options/refe_forc_noda.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -115,7 +115,8 @@ REFE_FORC_NODA = Option(
         SP.PVECTUR,
     ),
     condition=(
-      CondCalcul('+', ((AT.PHENO,'ME'),(AT.BORD,'0'),)),
+      CondCalcul('+', ((AT.PHENO,'ME'),(AT.BORD,'0'  ),)),
+      CondCalcul('-', ((AT.PHENO,'ME'),(AT.ABSO,'OUI'),)),
     ),
     comment="""  REFE_FORC_NODA : CALCUL DE LA REFERENCE DE FORCE INTERNE
         POUR LE CRITERE DE CONVERGENCE EN SUPER-RESIDU """,
