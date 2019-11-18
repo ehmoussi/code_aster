8bf0bdf07808d003475f2f32874d398431bd479a
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-11-18 12:50:42 +0100

    [#28197] Do not pickle 'file' objects.
    Former-commit-id: a00f042b2ff1b05c7b4b8cbb2dde4aa8eb6dc7af

--- code_aster/RunManager/Serializer.py
+++ code_aster/RunManager/Serializer.py
@@ -1,6 +1,22 @@
-# coding: utf-8
+# coding=utf-8
+# --------------------------------------------------------------------
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# This file is part of code_aster.
+#
+# code_aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# code_aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------
 
-# Copyright (C) 1991 - 2019  EDF R&D                www.code-aster.org
 #
 # This file is part of Code_Aster.
 #
@@ -34,6 +50,7 @@ The command :func:`~code_aster.Commands.debut.POURSUITE` does also the same.
 """
 
 import inspect
+from io import IOBase
 import os.path as osp
 import pickle
 import traceback
@@ -609,6 +626,8 @@ def _filteringContext(context):
         if not isinstance(obj, numpy.ndarray) and obj in ignored:
             continue
         if getattr(numpy, name, None) is obj: # see issue29282
+            continue
+        if isinstance(obj, IOBase):
             continue
         if type(obj) in (types.ModuleType, type,
                          types.MethodType, types.FunctionType,
