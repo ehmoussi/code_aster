b5068f666d806bb31ead567d956587c9864be623
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-11-18 13:56:02 +0100

    [#28197] Fix copyright after re-enabling checkers used in 'default' branch.
    Former-commit-id: 75b546d58e52ec5cd4506e6ef829833998826dd1

--- code_aster/Commands/factoriser.py
+++ code_aster/Commands/factoriser.py
@@ -6,7 +6,7 @@
 #
 # Code_Aster is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # Code_Aster is distributed in the hope that it will be useful,
@@ -29,7 +29,7 @@ class Factoriser(ExecuteCommand):
 
     def create_result(self, keywords):
         """Initialize the result.
-        
+
         Arguments:
             keywords (dict): Keywords arguments of user's keywords.
         """
--- code_aster/Commands/ther_lineaire.py
+++ code_aster/Commands/ther_lineaire.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 
-# Copyright (C) 1991 - 2018  EDF R&D                www.code-aster.org
+# Copyright (C) 1991 - 2019  EDF R&D                www.code-aster.org
 #
 # This file is part of Code_Aster.
 #
 # Code_Aster is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # Code_Aster is distributed in the hope that it will be useful,
@@ -38,7 +38,7 @@ class LinearThermalAnalysisBuild(ExecuteCommand):
             self._result = keywords["reuse"]
         else:
             self._result = EvolutiveThermalLoad()
-    
+
     def post_exec(self, keywords):
         """Execute the command.
 
