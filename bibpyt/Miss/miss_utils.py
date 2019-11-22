ac9d0191010b7fb7eb42fe04e63899966351d4f8
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-11-22 14:40:25 +0100

    [#29319] Get a new file in some commands.
    Former-commit-id: 5be9e8d4aab0275b82346cd3064c9ebdb7e243db

--- bibpyt/Miss/miss_utils.py
+++ bibpyt/Miss/miss_utils.py
@@ -146,13 +146,13 @@ class MISS_PARAMETER(object):
         if self.get('UNITE_RESU_IMPE') is None:
             self.set('_exec_Miss', True)
             self['UNITE_RESU_IMPE'] = self.UL.Libre(action='ASSOCIER',
-                 ascii=self._keywords['TYPE'] == 'ASCII')
+                 ascii=self._keywords['TYPE'] == 'ASCII', new=True)
         elif self['TYPE_RESU'] in ('TABLE_CONTROL', ):
             self.set('_exec_Miss', True)
 
         if self.get('UNITE_RESU_FORC') is None:
             self.set('_exec_Miss', True)
-            self['UNITE_RESU_FORC'] = self.UL.Libre(action='ASSOCIER')
+            self['UNITE_RESU_FORC'] = self.UL.Libre(action='ASSOCIER', new=True)
         elif self['TYPE_RESU'] in ('TABLE_CONTROL', ):
             self.set('_exec_Miss', True)
 
