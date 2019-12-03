dcf1510a64d414c1b15c9fff2a9b9ba983a11f19
Nicolas Pignet <nicolas.pignet@edf.fr>
2019-12-03 10:32:03 +0100

    [#29349] Modify ALARM when no group_ma are created
    Former-commit-id: f795f2b820112fb72a373b4cdacaaa89b58e5252

--- bibfor/include/asterfort/sscgma.h
+++ bibfor/include/asterfort/sscgma.h
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,13 +15,11 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
-!
 !
 interface
     subroutine sscgma(ma, nbgmp, nbgmin)
-        character(len=8) :: ma
-        integer :: nbgmp
-        integer :: nbgmin
+        character(len=8), intent(in) :: ma
+        integer, intent(in)          :: nbgmp
+        integer, intent(in)          :: nbgmin
     end subroutine sscgma
 end interface
