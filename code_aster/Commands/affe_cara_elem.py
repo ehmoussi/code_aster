21220971f2fd28d75ec6c51a7e7ebe52caea21c9
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-12-19 15:07:09 +0100

    [#23037] New AFFE_CARA_ELEM command for #29400.
    Former-commit-id: 2641859d3da1521f4aa73d01855d661e5d7d3c20

---
+++ code_aster/Commands/affe_cara_elem.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+# Copyright (C) 1991 - 2019  EDF R&D                www.code-aster.org
+#
+# This file is part of Code_Aster.
+#
+# Code_Aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Code_Aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Code_Aster.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from ..Objects import ElementaryCharacteristics
+from .ExecuteCommand import ExecuteCommand
+
+class EltCharacteristicsAssignment(ExecuteCommand):
+
+    """Command that assigns
+    :class:`~code_aster.Objects.ElementaryCharacteristics` on a model.
+    """
+    command_name = "AFFE_CARA_ELEM"
+
+    def create_result(self, keywords):
+        """Initialize the result.
+
+        Arguments:
+            keywords (dict): Keywords arguments of user's keywords.
+        """
+        self._result = ElementaryCharacteristics(keywords["MODELE"])
+
+AFFE_CARA_ELEM = EltCharacteristicsAssignment.run
