1dd759eb92c449103bfd5fba4a8d775dceeaaf84
Mathieu Courtois <mathieu.courtois@edf.fr>
2020-01-07 10:42:23 +0100

    [#29432] Fix invalid import.
    Former-commit-id: 69bea9685840ead5caf5ac7109b8d082e884605e

--- bibpyt/SD/sd_base_modale.py
+++ bibpyt/SD/sd_base_modale.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -19,10 +19,10 @@
 
 from SD import *
 
-from SD.resu_dyna import resu_dyna
+from SD.sd_resu_dyna import sd_resu_dyna
 
 
-class sd_base_modale(resu_dyna):
+class sd_base_modale(sd_resu_dyna):
 #--------------------------------------------------
     nomj = SDNom(fin=8)
     UTIL = Facultatif(AsVI(SDNom(debut=19)))
