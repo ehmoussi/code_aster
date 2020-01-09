18678847b287dd8a07f40ab01d65baa0a3a44f93
Micka?l Abbas <mickael.abbas@edf.fr>
2020-01-09 08:51:22 +0100

    [#29411] Enhanced measure of preparation of contact
    Former-commit-id: 26c28326355daff484b7571108f9ee20b37ec424

--- bibfor/nonlinear/nmprma.F90
+++ bibfor/nonlinear/nmprma.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -176,9 +176,10 @@ integer :: faccvg, ldccvg
     iterat = 0
     lcamor = ASTER_FALSE
 !
-! --- RE-CREATION DU NUME_DDL OU PAS
+! - Renumbering equations ?
 !
-    call nmrenu(modelz, fonact, lischa, ds_contact, numedd,&
+    call nmrenu(modelz    , fonact    , lischa,&
+                ds_measure, ds_contact, numedd,&
                 renume)
 !
 ! --- CHOIX DE REASSEMBLAGE DE LA MATRICE GLOBALE
