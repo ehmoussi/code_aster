791c117c0814d87452e0156cdb2c1c2131a7fc55
Mathieu Courtois <mathieu.courtois@edf.fr>
2020-01-16 13:36:53 +0100

    [#29474] CALC_MATR_ELEM/RIGI_GEOM may need CHAM_MATER (for cable elements).
    Former-commit-id: c2080045aa7679dd18fb3b5e13ae0a18b4d4b8cd

--- bibpyt/Macro/assemblage_ops.py
+++ bibpyt/Macro/assemblage_ops.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -99,7 +99,7 @@ def assemblage_ops(
             if CHARGE is not None:
                 if option[0:9] != 'RIGI_GEOM':
                     motscles['CHARGE'] = CHARGE
-            if CHAM_MATER is not None and option != 'RIGI_GEOM':
+            if CHAM_MATER:
                 motscles['CHAM_MATER'] = CHAM_MATER
             if CARA_ELEM is not None:
                 motscles['CARA_ELEM'] = CARA_ELEM
