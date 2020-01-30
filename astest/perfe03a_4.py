250c0fd410431e770d934085805904f2e67c218e
Mathieu Courtois <mathieu.courtois@edf.fr>
2020-01-30 12:02:50 +0100

    [#29526] Fix import of elements properties.
    Former-commit-id: 5167856cbbb4b809e027721062ea9f43aac0d522

--- astest/perfe03a_4.py
+++ astest/perfe03a_4.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -20,41 +20,39 @@ from code_aster.Commands import AFFE_CARA_ELEM
 import copy, math
 from random import *
 
-def F_AFFE_CARA_ELEM(MO, Ng, prefix_nomgr) : 
-        def DEFI_ANGLES(Ng,prefix_nomgr,**args):
-        ###################################################################
-        #affectation aleatoire des orientations de chaque grain
-        ###################################################################
-        #en entree :
-        #Ng : NOMBRE DE GERMES
-        #en sortie :
-        #LISTE_GROUP : MOT-CLE FACTEUR MASSIF de AFFE_CARA_ELEM
-        ###################################################################
+def DEFI_ANGLES(Ng,prefix_nomgr):
+###################################################################
+#affectation aleatoire des orientations de chaque grain
+###################################################################
+#en entree :
+#Ng : NOMBRE DE GERMES
+#en sortie :
+#LISTE_GROUP : MOT-CLE FACTEUR MASSIF de AFFE_CARA_ELEM
+###################################################################
 
 
-           # On boucle sur les arguments de la fonction pour
-           # creer une liste de mots-cle facteurs que l'on va
-           # retourner pour l'utiliser dans AFFE_CARA_ELEM
-           # Noter que comme MASSIF est un mot-cle facteur, il
-           # contient une liste de dictionnaires
-           
-           # coef=2*math.pi
-           coef=360.
+   # On boucle sur les arguments de la fonction pour
+   # creer une liste de mots-cle facteurs que l'on va
+   # retourner pour l'utiliser dans AFFE_CARA_ELEM
+   # Noter que comme MASSIF est un mot-cle facteur, il
+   # contient une liste de dictionnaires
 
-           LISTE_ANGLES=[]
-           for ig in range(Ng):
+   # coef=2*math.pi
+   coef=360.
 
-                  mon_dico={}
-                  mon_dico["GROUP_MA"]=prefix_nomgr+str(ig+1)
-                  angles=[coef*random(), coef/(2.*math.pi)*math.acos(2.*(random()-1./2.)), coef*random()]
-                  mon_dico["ANGL_EULER"]=angles
-                  LISTE_ANGLES.append(mon_dico)
-                  
-           return LISTE_ANGLES
-        ###################################################################
+   LISTE_ANGLES=[]
+   for ig in range(Ng):
+
+         mon_dico={}
+         mon_dico["GROUP_MA"]=prefix_nomgr+str(ig+1)
+         angles=[coef*random(), coef/(2.*math.pi)*math.acos(2.*(random()-1./2.)), coef*random()]
+         mon_dico["ANGL_EULER"]=angles
+         LISTE_ANGLES.append(mon_dico)
+
+   return LISTE_ANGLES
 
 
-        LISTE_ANGL=DEFI_ANGLES(Ng, prefix_nomgr)
-        ORIELEM=AFFE_CARA_ELEM(MODELE=MO,MASSIF=LISTE_ANGL)
+def F_AFFE_CARA_ELEM(MO, Ng, prefix_nomgr) :
+        LISTE_ANGL = DEFI_ANGLES(Ng, prefix_nomgr)
+        ORIELEM = AFFE_CARA_ELEM(MODELE=MO, MASSIF=LISTE_ANGL)
         return ORIELEM
-
