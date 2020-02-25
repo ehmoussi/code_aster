b0c9ac480e1afd902f87003fd71fc8a2294965c5
Etienne Cheignon <etienne.cheignon@necs.fr>
2020-02-25 14:40:26 +0100

    [#29532] Add keyword TOUT_PARA to RECU_TABLE
    Former-commit-id: 95c1fe1d9166d20f31c243606021d075a84b2487

--- bibfor/op/op0174.F90
+++ bibfor/op/op0174.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -31,10 +31,14 @@ subroutine op0174()
 #include "asterfort/getvtx.h"
 #include "asterfort/infmaj.h"
 #include "asterfort/jedema.h"
+#include "asterfort/jedetr.h"
+#include "asterfort/jeexin.h"
 #include "asterfort/jemarq.h"
+#include "asterfort/jeveuo.h"
 #include "asterfort/ltnotb.h"
 #include "asterfort/rsadpa.h"
 #include "asterfort/rsexpa.h"
+#include "asterfort/rsnopa.h"
 #include "asterfort/rsorac.h"
 #include "asterfort/tbajli.h"
 #include "asterfort/tbajpa.h"
@@ -44,7 +48,7 @@ subroutine op0174()
 #include "asterfort/wkvect.h"
     integer :: iret, lonord(1), iord, ipara, i, n, numord, ibid
     integer :: nbpara, inom, ityp, ilong, itabi, itabr, itabc
-    integer :: pi, pc, pr
+    integer :: pi, pc, pr, nbac, nbpa, inom2
     real(kind=8) :: rbid
     complex(kind=8) :: cbid
     character(len=8) :: table, concpt, typ
@@ -79,11 +83,7 @@ subroutine op0174()
 !   EXTRACTION DE PARAMETRES
 ! ----------------------------
 !
-!    NOMBRE DE PARAMETRES
-    call getvtx(' ', 'NOM_PARA', nbval=0, nbret=nbpara)
-    ASSERT(nbpara.ne.0)
-    nbpara = -nbpara
-!
+
 !
 ! -- LECTURE DES NUMEROS D'ORDRE
 !
@@ -94,47 +94,90 @@ subroutine op0174()
     call rsorac(concpt, 'TOUT_ORDRE', 0, rbid, kbid,&
                 cbid, rbid, kbid, zi(iord), lonord(1),&
                 ibid)
+
+!    TOUS LES PARAMETRES : MOT-CLE TOUT_PARA
+!    ---------------------------------------
+    call getvtx(' ', 'TOUT_PARA', scal=kbid, nbret=iret)
+    if (iret .ne. 0) then
+        ASSERT(kbid(1:3).eq.'OUI')
+
+! -- -- nombre de parametres présents
+        call rsnopa(concpt, 2, '&&OP0174.NOM_PAR2', nbac, nbpa)
+        nbpara = nbac + nbpa
+        if (nbpara .eq. 0) call utmess('F','UTILITAI3_3')
+
+! -- -- liste des parametres
+        call jeexin('&&OP0174.NOM_PAR2', iret)
+        ASSERT(iret .gt. 0)
+        call jeveuo('&&OP0174.NOM_PAR2', 'E', jadr=inom2)
+
+! -- -- ajout NUME_ORDRE en début de liste
+        call wkvect('&&OP0174.NOM_PARA', 'V V K16', nbpara+1, inom)
+        zk16(inom) = 'NUME_ORDRE'
+
+! -- -- sélection des types i, r et c uniquement 
+        call wkvect('&&OP0174.TYPE_PARA', 'V V K8 ', nbpara+1, ityp)
+        zk8(ityp) = 'I'
+        numord = zi(iord)
+        nbpa = 0
+        do i = 1, nbpara
+            nom = zk16(inom2-1+i)
+            call rsadpa(concpt, 'L', 1, nom, numord,&
+                        1, sjv=ipara, styp=typ, istop=0)
+            if (typ(1:1) .eq. 'R' .or. typ(1:1) .eq. 'I' &
+                                  .or. typ(1:1) .eq. 'C') then
+                nbpa = nbpa+1
+                zk16(inom+nbpa) = zk16(inom2-1+i)
+                zk8(ityp+nbpa) = typ(1:1)
+            endif
+        end do
+        nbpara = nbpa
+        call jedetr('&&OP0174.NOM_PAR2')
+    else
+
+!    NOMBRE DE PARAMETRES : MOT-CLE NOM_PARA
+!    ---------------------------------------
+        call getvtx(' ', 'NOM_PARA', nbval=0, nbret=nbpara)
+        ASSERT(nbpara.ne.0)
+        nbpara = -nbpara
+
+! -- -- noms des paramètres à extraire
+        call wkvect('&&OP0174.NOM_PARA', 'V V K16', nbpara+1, inom)
+        call wkvect('&&OP0174.LONG_NOM_PARA', 'V V I', nbpara, ilong)
 !
+        call getltx(' ', 'NOM_PARA', 1, 16, nbpara,&
+                    zi(ilong), ibid)
+        do i = 1, nbpara
+            if (zi(ilong-1+i) .gt. 16) then
+                call utmess('F', 'UTILITAI3_4')
+            endif
+        end do
 !
-! -- NOMS DES PARAMETRES A EXTRAIRE
+        zk16(inom) = 'NUME_ORDRE'
+        call getvtx(' ', 'NOM_PARA', nbval=nbpara, vect=zk16(inom+1), nbret=ibid)
 !
-    call wkvect('&&OP0174.NOM_PARA', 'V V K16', nbpara+1, inom)
-    call wkvect('&&OP0174.LONG_NOM_PARA', 'V V I', nbpara, ilong)
-!
-    call getltx(' ', 'NOM_PARA', 1, 16, nbpara,&
-                zi(ilong), ibid)
-    do 10 i = 1, nbpara
-        if (zi(ilong-1+i) .gt. 16) then
-            call utmess('F', 'UTILITAI3_4')
-        endif
-10  end do
-!
-    zk16(inom) = 'NUME_ORDRE'
-    call getvtx(' ', 'NOM_PARA', nbval=nbpara, vect=zk16(inom+1), nbret=ibid)
-!
-    do 20 i = 1, nbpara
-        nom = zk16(inom + i)
-        call rsexpa(concpt, 2, nom, iret)
-        if (iret .eq. 0) then
-            call utmess('F', 'UTILITAI3_5', sk=nom)
-        endif
-20  continue
-!
-!
-! -- TYPES DES PARAMETRES A EXTRAIRE
-!
-    call wkvect('&&OP0174.TYPE_PARA', 'V V K8 ', nbpara+1, ityp)
-    zk8(ityp) = 'I'
-    numord = zi(iord)
-    do 30 i = 1, nbpara
-        nom = zk16(inom + i)
-        call rsadpa(concpt, 'L', 1, nom, numord,&
-                    1, sjv=ipara, styp=typ, istop=0)
-        if (typ(1:1) .ne. 'R' .and. typ(1:1) .ne. 'I' .and. typ(1:1) .ne. 'C') then
-            call utmess('F', 'UTILITAI3_6')
-        endif
-        zk8(ityp+i) = typ(1:1)
-30  end do
+        do i = 1, nbpara
+            nom = zk16(inom + i)
+            call rsexpa(concpt, 2, nom, iret)
+            if (iret .eq. 0) then
+                call utmess('F', 'UTILITAI3_5', sk=nom)
+            endif
+        enddo
+
+! -- -- types des paramètres à extraire
+        call wkvect('&&OP0174.TYPE_PARA', 'V V K8 ', nbpara+1, ityp)
+        zk8(ityp) = 'I'
+        numord = zi(iord)
+        do i = 1, nbpara
+            nom = zk16(inom + i)
+            call rsadpa(concpt, 'L', 1, nom, numord,&
+                        1, sjv=ipara, styp=typ, istop=0)
+            if (typ(1:1) .ne. 'R' .and. typ(1:1) .ne. 'I' .and. typ(1:1) .ne. 'C') then
+                call utmess('F', 'UTILITAI3_6')
+            endif
+            zk8(ityp+i) = typ(1:1)
+        end do
+    endif
 !
 !
 ! -- INITIALISATION DE LA TABLE
@@ -150,7 +193,7 @@ subroutine op0174()
     call wkvect('&&OP0174.PARA_I', 'V V I', nbpara+1, itabi)
     call wkvect('&&OP0174.PARA_C', 'V V C', nbpara+1, itabc)
 !
-    do 40 n = 1, lonord(1)
+    do n = 1, lonord(1)
         numord = zi(iord-1+n)
 !
         pi = 0
@@ -160,7 +203,7 @@ subroutine op0174()
         zi(itabi+pi) = numord
         pi = pi+1
 !
-        do 50 i = 1, nbpara
+        do i = 1, nbpara
             nom = zk16(inom + i)
             call rsadpa(concpt, 'L', 1, nom, numord,&
                         1, sjv=ipara, styp=typ, istop=0)
@@ -174,13 +217,13 @@ subroutine op0174()
                 zc(itabc+pc) = zc(ipara)
                 pc = pc+1
             else
-                call utmess('F', 'UTILITAI3_7')
+                ASSERT(.false.)
             endif
-50      continue
+        enddo
 !
         call tbajli(table, 1+nbpara, zk16(inom), zi(itabi), zr(itabr),&
                     zc(itabc), kbid, 0)
-40  end do
+    end do
 !
 !
 !
