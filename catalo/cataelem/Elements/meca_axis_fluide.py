985f3efcb1b7d39578d15ba0c0773198ffb0ee6f
Micka?l Abbas <mickael.abbas@edf.fr>
2020-02-27 10:45:30 +0100

    [#29563] Fuse catalogs for boundary elements
    Former-commit-id: a035bb0aa1fea01d21873ccf640977373baa7fc3

--- catalo/cataelem/Elements/meca_axis_fluide.py
+++ catalo/cataelem/Elements/meca_axis_fluide.py
@@ -33,18 +33,19 @@ from cataelem.Options.options import OP
 NDEPLAC  = LocatedComponents(phys=PHY.DEPL_C, type='ELNO',
     components=('PRES','PHI',))
 
-
 DDL_MECA = LocatedComponents(phys=PHY.DEPL_R, type='ELNO',
     components=('PRES','PHI',))
-
 
 NGEOMER  = LocatedComponents(phys=PHY.GEOM_R, type='ELNO',
     components=('X','Y',))
 
-
 EGGEOP_R = LocatedComponents(phys=PHY.GEOM_R, type='ELGA', location='RIGI',
     components=('X','Y','W',))
 
+ESOURCR  = LocatedComponents(phys=PHY.SOUR_R, type='ELGA', location='RIGI',
+    components=('VNOR',))
+
+MVECTUR  = ArrayOfComponents(phys=PHY.VDEP_R, locatedComponents=DDL_MECA)
 
 MMATUUC  = ArrayOfComponents(phys=PHY.MDEP_C, locatedComponents=NDEPLAC)
 
@@ -148,4 +149,71 @@ class MEAXFLT6(MEAXFLQ4):
     meshType = MT.TRIA6
     elrefe =(
             ElrefeLoc(MT.TR6, gauss = ('RIGI=FPG6','FPG1=FPG1',), mater=('FPG1',),),
+        )
+
+#------------------------------------------------------------
+class MEAXFLS2(Element):
+    """Please document this element"""
+    meshType = MT.SEG2
+    elrefe =(
+            ElrefeLoc(MT.SE2, gauss = ('RIGI=FPG2','FPG1=FPG1',), mater=('FPG1',),),
+        )
+    calculs = (
+
+        OP.CHAR_MECA_ONDE(te=373,
+            para_in=((SP.PGEOMER, NGEOMER), (SP.PMATERC, LC.CMATERC),
+                     (SP.PONDECR, LC.EONDEPR), ),
+            para_out=((SP.PVECTUR, MVECTUR), ),
+        ),
+
+        OP.CHAR_MECA_VNOR(te=255,
+            para_in=((SP.PGEOMER, NGEOMER), (SP.PMATERC, LC.CMATERC),
+                     (SP.PSOURCR, ESOURCR), ),
+            para_out=((SP.PVECTUR, MVECTUR), ),
+        ),
+
+        OP.CHAR_MECA_VNOR_F(te=256,
+            para_in=((SP.PGEOMER, NGEOMER), (SP.PMATERC, LC.CMATERC),
+                     (SP.PSOURCF, LC.ESOURCF), ),
+            para_out=((SP.PVECTUR, MVECTUR), ),
+        ),
+
+        OP.COOR_ELGA(te=478,
+            para_in=((SP.PGEOMER, NGEOMER), ),
+            para_out=((OP.COOR_ELGA.PCOORPG, EGGEOP_R), ),
+        ),
+
+        OP.IMPE_MECA(te=258,
+            para_in=((SP.PGEOMER, NGEOMER), (SP.PIMPEDR, LC.EIMPEDR),
+                     (SP.PMATERC, LC.CMATERC), ),
+            para_out=((SP.PMATUUR, MMATUUR), ),
+        ),
+
+        OP.ONDE_FLUI(te=372,
+            para_in=((SP.PGEOMER, NGEOMER), (SP.PMATERC, LC.CMATERC),
+                     (SP.PONDECR, LC.EONDEPR), ),
+            para_out=((SP.PMATUUR, MMATUUR), ),
+        ),
+
+        OP.TOU_INI_ELGA(te=99,
+            para_out=((OP.TOU_INI_ELGA.PGEOM_R, EGGEOP_R), ),
+        ),
+
+        OP.TOU_INI_ELEM(te=99,
+            para_out=((OP.TOU_INI_ELEM.PGEOM_R, LC.CGEOM2D), ),
+        ),
+
+
+        OP.TOU_INI_ELNO(te=99,
+            para_out=((OP.TOU_INI_ELNO.PGEOM_R, NGEOMER), ),
+        ),
+
+    )
+
+#------------------------------------------------------------
+class MEAXFLS3(MEAXFLS2):
+    """Please document this element"""
+    meshType = MT.SEG3
+    elrefe =(
+            ElrefeLoc(MT.SE3, gauss = ('RIGI=FPG4','FPG1=FPG1',), mater=('FPG1',),),
         )
