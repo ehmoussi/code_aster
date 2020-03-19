06a11bcd8ecf8748b8bfa67cb5af1ff193b03b5c
Etienne Cheignon <etienne.cheignon@necs.fr>
2020-03-19 10:29:11 +0100

    [#29479] Change default value of ALPHA parameter in DYNA_NON_LINE/HHT
    Former-commit-id: af91781fc6659229c1a5eb94131dd511f7605440

--- code_aster/Cata/Commands/dyna_non_line.py
+++ code_aster/Cata/Commands/dyna_non_line.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -104,7 +104,7 @@ DYNA_NON_LINE=OPER(nom="DYNA_NON_LINE",op= 70,sd_prod=evol_noli,
                GAMMA        =SIMP(statut='f',typ='R',defaut= 0.5),),
 
             b_hht     = BLOC(condition="""equal_to("SCHEMA", 'HHT')""",
-               ALPHA        =SIMP(statut='f',typ='R',defaut= -0.3 ),
+               ALPHA        =SIMP(statut='f',typ='R',defaut= -0.1 ),
                MODI_EQUI    =SIMP(statut='f',typ='TXM',defaut="NON",into=("OUI","NON"),),),
 
             b_explicit= BLOC(condition="""equal_to("SCHEMA", 'TCHAMWA') or equal_to("SCHEMA", 'DIFF_CENT')""",
