af0b7c886d389abc1e4a9f97d16d97ff9574cef8
Micka?l Abbas <mickael.abbas@edf.fr>
2020-04-01 15:27:02 +0200

    [#29729] Compatibility of behaviour with strain model
    Former-commit-id: a839a7e23f3690c46d6b8f128c635e4fd1fdede2

--- bibpyt/Messages/algorith8.py
+++ bibpyt/Messages/algorith8.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -18,10 +18,6 @@
 # --------------------------------------------------------------------
 
 cata_msg = {
-
-    1 : _("""
- contraintes planes en grandes déformations non implantées
-"""),
 
     2 : _("""
  caractéristique fluage incomplet
