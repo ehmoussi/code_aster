fbbe2aa975368d9c5aba29a4e63ad9cb5206a0ac
Micka?l Abbas <mickael.abbas@edf.fr>
2020-04-16 09:49:31 +0200

    [#27890] Option IMPE_MECA for FSI / U-P-PHI
    Former-commit-id: 05bfacb38dcbf8ce92f659a3c95422bac9742d8d

--- bibfor/te/te0010.F90
+++ bibfor/te/te0010.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,125 +15,132 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
+! aslint: disable=W0413
+! => zr(jv_impe) is a real zero from AFFE_CHAR_MECA
+!
 subroutine te0010(option, nomte)
-!.......................................................................
-    implicit none
 !
-!     BUT: CALCUL DES MATRICES ELEMENTAIRES EN MECANIQUE
-!          CORRESPONDANT A UNE IMPEDANCE IMPOSEE
-!          SUR DES FACES D'ELEMENTS ISOPARAMETRIQUES 3D
-!
-!          OPTION : 'IMPE_MECA'
-!
-!     ENTREES  ---> OPTION : OPTION DE CALCUL
-!          ---> NOMTE  : NOM DU TYPE ELEMENT
-!.......................................................................
+implicit none
 !
 #include "jeveux.h"
 #include "asterfort/elrefe_info.h"
+#include "asterfort/teattr.h"
+#include "asterfort/assert.h"
 #include "asterfort/jevech.h"
-#include "asterfort/rcvalb.h"
+#include "asterfort/utmess.h"
+#include "asterfort/getFluidPara.h"
 !
-    integer :: icodre(1)
-    character(len=8) :: fami, poum
-    character(len=16) :: nomres(1), nomte, option
-    real(kind=8) :: nx, ny, nz, sx(9, 9), sy(9, 9), sz(9, 9), jac
-    real(kind=8) :: valres(1), rho, rho2
-    integer :: ipoids, ivf, idfdx, idfdy, igeom, imate
-    integer :: ndim, nno, ndi, ipg, npg2, imatuu, iimpe
+character(len=16), intent(in) :: option, nomte
+!
+! --------------------------------------------------------------------------------------------------
+!
+! Elementary computation
+!
+! Elements: 3D_FLUIDE (boundary)
+!           3D_FLUI_ABSO
+!
+! Options: IMPE_MECA
+!
+! --------------------------------------------------------------------------------------------------
+!
+! In  option           : name of option to compute
+! In  nomte            : type of finite element
+!
+! --------------------------------------------------------------------------------------------------
+!
+    real(kind=8) :: nx, ny, nz
+    real(kind=8) :: sx(9, 9), sy(9, 9), sz(9, 9), jac
+    real(kind=8) :: rho, rho2
+    integer :: ipoids, ivf, idfdx, idfdy
+    integer :: jv_geom, jv_mate, jv_matr, jv_impe
+    integer :: ndim, nno, ndi, ipg, npg
     integer :: idec, jdec, kdec, ldec
-    integer :: nnos, jgano, kpg, spt
-!
-!
-!-----------------------------------------------------------------------
     integer :: i, ii, ij, ino, j, jj, jno
-    integer :: mater
-!-----------------------------------------------------------------------
-    call elrefe_info(fami='RIGI',ndim=ndim,nno=nno,nnos=nnos,&
-  npg=npg2,jpoids=ipoids,jvf=ivf,jdfde=idfdx,jgano=jgano)
+    integer :: j_mater, iret
+    character(len=16) :: fsi_form
+!
+! --------------------------------------------------------------------------------------------------
+!
+    call teattr('S', 'FORMULATION', fsi_form, iret)
+!
+! - Get parameters of element
+!
+    call elrefe_info(fami='RIGI',&
+                     ndim=ndim, nno=nno, npg=npg,&
+                     jpoids=ipoids, jvf=ivf, jdfde=idfdx)
+    ASSERT(nno .le. 9)
     idfdy = idfdx + 1
-    ndi = nno*(2*nno+1)
-!
-    call jevech('PGEOMER', 'L', igeom)
-    call jevech('PMATERC', 'L', imate)
-    mater=zi(imate)
-    nomres(1)='RHO'
-    fami='FPG1'
-    kpg=1
-    spt=1
-    poum='+'
-    call rcvalb(fami, kpg, spt, poum, mater,&
-                ' ', 'FLUIDE', 0, ' ', [0.d0],&
-                1, nomres, valres, icodre, 1)
-    rho = valres(1)
-!
-    call jevech('PIMPEDR', 'L', iimpe)
-    call jevech('PMATUUR', 'E', imatuu)
-!
-! --- INITIALISATION DE LA MATRICE D'IMPEDANCE
-    do 10 i = 1, ndi
-        zr(imatuu+i-1) = 0.d0
-10  end do
-!
-    if (zr(iimpe) .eq. 0.d0) then
-        goto 999
+    if (fsi_form .eq. 'FSI_UPPHI') then
+        ndi = nno*(2*nno+1)
     else
+        call utmess('F', 'FLUID1_2', sk = fsi_form)
+    endif
 !
-!        CALCUL DES PRODUITS VECTORIELS OMI X OMJ
+! - Input fields
 !
-        do 1 ino = 1, nno
-            i = igeom + 3*(ino-1) -1
-            do 2 jno = 1, nno
-                j = igeom + 3*(jno-1) -1
+    call jevech('PGEOMER', 'L', jv_geom)
+    call jevech('PMATERC', 'L', jv_mate)
+    call jevech('PIMPEDR', 'L', jv_impe)
+!
+! - Get material properties for fluid
+!
+    j_mater = zi(jv_mate)
+    call getFluidPara(j_mater, rho)
+    rho2 = -rho*rho
+!
+! - Output field
+!
+    call jevech('PMATUUR', 'E', jv_matr)
+    do i = 1, ndi
+        zr(jv_matr+i-1) = 0.d0
+    end do
+!
+! - Compute
+!
+    if (zr(jv_impe) .ne. 0.d0) then
+        do ino = 1, nno
+            i = jv_geom + 3*(ino-1) -1
+            do jno = 1, nno
+                j = jv_geom + 3*(jno-1) -1
                 sx(ino,jno) = zr(i+2) * zr(j+3) - zr(i+3) * zr(j+2)
                 sy(ino,jno) = zr(i+3) * zr(j+1) - zr(i+1) * zr(j+3)
                 sz(ino,jno) = zr(i+1) * zr(j+2) - zr(i+2) * zr(j+1)
- 2          continue
- 1      continue
-!
-!        BOUCLE SUR LES POINTS DE GAUSS
-        rho2 = -rho*rho
-!
-        do 101 ipg = 1, npg2
+            end do
+        end do
+        do ipg = 1, npg
             kdec = (ipg-1)*nno*ndim
             ldec = (ipg-1)*nno
-!
+! --------- Compute normal
             nx = 0.0d0
             ny = 0.0d0
             nz = 0.0d0
-!
-!           CALCUL DE LA NORMALE AU POINT DE GAUSS IPG
-!
-            do 102 i = 1, nno
+            do i = 1, nno
                 idec = (i-1)*ndim
-                do 102 j = 1, nno
+                do j = 1, nno
                     jdec = (j-1)*ndim
-!
-                    nx = nx + zr(idfdx+kdec+idec) * zr(idfdy+kdec+ jdec) * sx(i,j)
-                    ny = ny + zr(idfdx+kdec+idec) * zr(idfdy+kdec+ jdec) * sy(i,j)
-                    nz = nz + zr(idfdx+kdec+idec) * zr(idfdy+kdec+ jdec) * sz(i,j)
-!
-102              continue
-!
-!           CALCUL DU JACOBIEN AU POINT DE GAUSS IPG
-!
+                    nx = nx + zr(idfdx+kdec+idec) * zr(idfdy+kdec+jdec) * sx(i,j)
+                    ny = ny + zr(idfdx+kdec+idec) * zr(idfdy+kdec+jdec) * sy(i,j)
+                    nz = nz + zr(idfdx+kdec+idec) * zr(idfdy+kdec+jdec) * sz(i,j)
+                end do
+            end do
+! --------- Compute jacobian
             jac = sqrt(nx*nx + ny*ny + nz*nz)
-!
-            do 103 i = 1, nno
-!
-                do 104 j = 1, i
-                    ii=2*i
-                    jj=2*j
-                    ij = (ii-1)*ii/2 + jj
-!
-                    zr(imatuu+ij-1) = zr(imatuu+ij-1) + jac * rho2 / zr(iimpe+ipg-1) * zr(ipoids+&
-                                      &ipg-1) * zr(ivf+ldec+ i-1) * zr(ivf+ldec+j-1)
-!
-104              continue
-103          continue
-!
-101      continue
+! --------- Compute matrix
+            if (fsi_form .eq. 'FSI_UPPHI') then
+                do i = 1, nno
+                    do j = 1, i
+                        ii = 2*i
+                        jj = 2*j
+                        ij = (ii-1)*ii/2 + jj
+                        zr(jv_matr+ij-1) = zr(jv_matr+ij-1) +&
+                                           jac * zr(ipoids+ipg-1) *&
+                                           rho2 / zr(jv_impe+ipg-1) *&
+                                           zr(ivf+ldec+i-1) * zr(ivf+ldec+j-1)
+                    end do
+                end do
+            else
+                call utmess('F', 'FLUID1_2', sk = fsi_form)
+            endif
+        end do
     endif
-999  continue
 end subroutine
--- bibfor/te/te0258.F90
+++ bibfor/te/te0258.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,109 +15,124 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
+! aslint: disable=W0413
+! => zr(jv_impe) is a real zero from AFFE_CHAR_MECA
+!
 subroutine te0258(option, nomte)
-!.......................................................................
-    implicit none
 !
-!     BUT: CALCUL DES MATRICES ELEMENTAIRES EN MECANIQUE
-!          CORRESPONDANT A UNE IMPEDANCE IMPOSEE
-!          SUR DES FACES D'ELEMENTS ISOPARAMETRIQUES 1D
+implicit none
 !
-!          OPTION : 'IMPE_MECA'
-!
-!     ENTREES  ---> OPTION : OPTION DE CALCUL
-!          ---> NOMTE  : NOM DU TYPE ELEMENT
-!.......................................................................
-!
-#include "asterf_types.h"
 #include "jeveux.h"
 #include "asterfort/elrefe_info.h"
+#include "asterfort/teattr.h"
+#include "asterfort/assert.h"
 #include "asterfort/jevech.h"
 #include "asterfort/lteatt.h"
-#include "asterfort/rcvalb.h"
 #include "asterfort/vff2dn.h"
+#include "asterfort/utmess.h"
+#include "asterfort/getFluidPara.h"
 !
-    integer :: icodre(1)
-    character(len=8) :: fami, poum
-    character(len=16) :: nomte, option
-    real(kind=8) :: nx, ny, poids, rho(1), rho2
-    integer :: ipoids, ivf, idfde, igeom, imate
-    integer :: ndi, nno, kp, npg, imatuu, iimpe
-    integer :: ldec, kpg, spt
-    aster_logical :: laxi
+character(len=16), intent(in) :: option, nomte
 !
+! --------------------------------------------------------------------------------------------------
 !
-!-----------------------------------------------------------------------
-    integer :: i, ii, ij, j, jgano, jj, ndim
-    integer :: nnos
+! Elementary computation
+!
+! Elements: 2D_FLUIDE, AXIS_FLUIDE (boundary)
+!           2D_FLUI_ABSO
+!
+! Options: IMPE_MECA
+!
+! --------------------------------------------------------------------------------------------------
+!
+! In  option           : name of option to compute
+! In  nomte            : type of finite element
+!
+! --------------------------------------------------------------------------------------------------
+!
+    real(kind=8) :: nx, ny
+    real(kind=8) :: poids
+    real(kind=8) :: rho, rho2
+    integer :: ipoids, ivf, idfde
+    integer :: jv_geom, jv_mate, jv_matr, jv_impe
+    integer :: ndim, nno, ndi, ipg, npg
+    integer :: ldec
+    integer :: i, ii, ij, j, jj
+    integer :: j_mater, iret
+    character(len=16) :: fsi_form
+    aster_logical :: l_axis
     real(kind=8) :: r
-!-----------------------------------------------------------------------
-    call elrefe_info(fami='RIGI', ndim=ndim, nno=nno, nnos=nnos, npg=npg,&
-                     jpoids=ipoids, jvf=ivf, jdfde=idfde, jgano=jgano)
-    ndi = nno* (2*nno+1)
 !
-    laxi = .false.
-    if (lteatt('AXIS','OUI')) laxi = .true.
+! --------------------------------------------------------------------------------------------------
 !
-    call jevech('PGEOMER', 'L', igeom)
-    call jevech('PMATERC', 'L', imate)
-    call jevech('PIMPEDR', 'L', iimpe)
-    call jevech('PMATUUR', 'E', imatuu)
+    call teattr('S', 'FORMULATION', fsi_form, iret)
+    l_axis = (lteatt('AXIS','OUI'))
 !
-    fami='FPG1'
-    kpg=1
-    spt=1
-    poum='+'
+! - Get parameters of element
 !
-    call rcvalb(fami, kpg, spt, poum, zi(imate),&
-                ' ', 'FLUIDE', 0, ' ', [0.d0],&
-                1, 'RHO', rho, icodre, 1)
-!
-! --- INITIALISATION DE LA MATRICE D'IMPEDANCE
-    do 10 i = 1, ndi
-        zr(imatuu+i-1) = 0.d0
- 10 end do
-!
-    if (zr(iimpe) .eq. 0.d0) then
-        goto 60
+    call elrefe_info(fami='RIGI',&
+                     ndim=ndim, nno=nno, npg=npg,&
+                     jpoids=ipoids, jvf=ivf, jdfde=idfde)
+    ASSERT(nno .le. 3)
+    if (fsi_form .eq. 'FSI_UPPHI') then
+        ndi = nno*(2*nno+1)
     else
+        call utmess('F', 'FLUID1_2', sk = fsi_form)
+    endif
 !
-!        BOUCLE SUR LES POINTS DE GAUSS
-        rho2 = -rho(1)*rho(1)
+! - Input fields
 !
-        do 50 kp = 1, npg
+    call jevech('PGEOMER', 'L', jv_geom)
+    call jevech('PMATERC', 'L', jv_mate)
+    call jevech('PIMPEDR', 'L', jv_impe)
 !
-            ldec = (kp-1)*nno
+! - Get material properties for fluid
 !
+    j_mater = zi(jv_mate)
+    call getFluidPara(j_mater, rho)
+    rho2 = -rho*rho
+!
+! - Output field
+!
+    call jevech('PMATUUR', 'E', jv_matr)
+    do i = 1, ndi
+        zr(jv_matr+i-1) = 0.d0
+    end do
+!
+! - Compute
+!
+    if (zr(jv_impe) .ne. 0.d0) then
+        do ipg = 1, npg
+            ldec = (ipg-1)*nno
+! --------- Compute normal
             nx = 0.0d0
             ny = 0.0d0
-!
-            call vff2dn(ndim, nno, kp, ipoids, idfde,&
-                        zr(igeom), nx, ny, poids)
-!%
-            if (laxi) then
+            call vff2dn(ndim, nno, ipg, ipoids, idfde,&
+                        zr(jv_geom), nx, ny, poids)
+! --------- Radius for axisymmetric model
+            if (l_axis) then
                 r = 0.d0
-                do 20 i = 1, nno
-                    r = r + zr(igeom+2* (i-1))*zr(ivf+ldec+i-1)
- 20             continue
+                do i = 1, nno
+                    r = r + zr(jv_geom+2*(i-1))*zr(ivf+ldec+i-1)
+                end do
                 poids = poids*r
             endif
-!%
-            do 40 i = 1, nno
-!
-                do 30 j = 1, i
-                    ii = 2*i
-                    jj = 2*j
-                    ij = (ii-1)*ii/2 + jj
-!
-                    zr(imatuu+ij-1) = zr(imatuu+ij-1) + poids*rho2/zr( iimpe+kp-1)* zr(ivf+ldec+i&
-                                      &-1)*zr(ivf+ldec+j-1)
-!
- 30             continue
- 40         continue
-!
- 50     continue
+! --------- Compute matrix
+            if (fsi_form .eq. 'FSI_UPPHI') then
+                do i = 1, nno
+                    do j = 1, i
+                        ii = 2*i
+                        jj = 2*j
+                        ij = (ii-1)*ii/2 + jj
+                        zr(jv_matr+ij-1) = zr(jv_matr+ij-1) +&
+                                           poids *&
+                                           rho2 / zr(jv_impe+ipg-1) *&
+                                           zr(ivf+ldec+i-1) * zr(ivf+ldec+j-1)
+                    end do
+                end do
+            else
+                call utmess('F', 'FLUID1_2', sk = fsi_form)
+            endif
+        end do
     endif
- 60 continue
 end subroutine
