3fa202dae88afff4467bed0cfd6ea7e007ba35c1
Mathieu Courtois <mathieu.courtois@edf.fr>
2020-04-20 09:17:00 +0200

    [#27107] Fix table_container created by CALC_CORR_SSD
    Former-commit-id: fa0637b09707211b76dc7d35903c457364870e20

--- bibpyt/Noyau/asojb.py
+++ bibpyt/Noyau/asojb.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -79,13 +79,11 @@ class AsBase(Type):
                     try:
                         v(checker)
                     except:
-                        mess = 60 * '-' + '\n'
-                        mess = mess + \
-                            'Erreur SDVERI (Attention : vérification incomplète)' + \
-                            '\n'
-                        mess = mess.join(
-                            traceback.format_tb(sys.exc_info()[2]))
-                        checker.err(self, mess)
+                        mess = [60 * '-',
+                                'Erreur SDVERI (Attention : vérification '
+                                'incomplète)',
+                                traceback.format_exc()]
+                        checker.err(self, '\n'.join(mess))
 
         checker.optional = optional
         return checker
