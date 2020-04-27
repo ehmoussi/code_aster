0b49211816f9c0c2d56b1be44d7ff050cf00b8cb
Ting Yu <ting.yu@edf.fr>
2020-04-27 18:12:28 +0200

    [#29699] non-initialized vale_calc in contact continue with RESOLUTION=NON
    Former-commit-id: ae300deae30a2aaab7b5d66476565142d9ae5a74

--- bibfor/nonlinear/InitTableCvg.F90
+++ bibfor/nonlinear/InitTableCvg.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -165,7 +165,7 @@ type(NL_DS_Print), intent(inout) :: ds_print
             call SetTableColumn(table_cvg, name_ = 'FROT_NOEU', flag_acti_ = ASTER_TRUE)
         endif
     endif
-    if (l_newt_cont) then
+    if (l_newt_cont .and. (.not.l_cont_all_verif)) then
         call SetTableColumn(table_cvg, name_ = 'CONT_NEWT', flag_acti_ = ASTER_TRUE)
     endif
     
