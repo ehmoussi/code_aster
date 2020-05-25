bbb225c602570ffaa70374ee801ad8c30714ccd2
Mathieu Courtois <mathieu.courtois@edf.fr>
2020-05-25 14:51:36 +0200

    [#29905] add support of ALARME/INFO keywords in INCLUDE
    Former-commit-id: 8d856e1c579798a26c23884dd8f9510b5543131b

--- code_aster/Commands/include.py
+++ code_aster/Commands/include.py
@@ -18,12 +18,16 @@
 # along with Code_Aster.  If not, see <http://www.gnu.org/licenses/>.
 
 # person_in_charge: mathieu.courtois@edf.fr
+# aslint: disable=C4007,C4009
+# C4007: INCLUDE() is also discouraged
+# C4009: in a string, imported here
 
 import inspect
 import os.path as osp
 
+from ..Messages import UTMESS
 from ..Supervis import ExecuteCommand
-from ..Utilities import ExecutionParameter
+from ..Utilities import ExecutionParameter, Options
 
 
 AUTO_IMPORT = """
@@ -48,14 +52,26 @@ class Include(ExecuteCommand):
         finally:
             del caller
 
+        if keywords.get("ALARME", "OUI"):
+            UTMESS('A', 'SUPERVIS_25', valk=("AsterStudy", "import"))
         if keywords.get("UNITE"):
             filename = 'fort.{0}'.format(keywords["UNITE"])
         else:
             rcdir = ExecutionParameter().get_option('rcdir')
             filename = osp.join(rcdir, 'tests_data', keywords["DONNEE"])
 
-        with open(filename) as fobj:
-            exec(compile(AUTO_IMPORT + fobj.read(), filename, 'exec'), context)
-
+        option = ExecutionParameter().option
+        show = (keywords.get("INFO", 0) >= 2 and
+                not option & Options.ShowChildCmd)
+        if show:
+            ExecutionParameter().enable(Options.ShowChildCmd)
+        try:
+            with open(filename) as fobj:
+                exec(compile(AUTO_IMPORT + fobj.read(), filename, 'exec'), context)
+        except Exception as exc:
+            UTMESS('F', 'FICHIER_2', valk=(filename, str(exc)))
+        finally:
+            if show:
+                ExecutionParameter().disable(Options.ShowChildCmd)
 
 INCLUDE = Include.run
