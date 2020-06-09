dd73403fb0a286d677761c1a0634c827afa55370
Code_Aster <code-aster@edf.fr>
2020-06-09 15:57:19 +0200

    [admin] fix unsafe import
    Former-commit-id: 6c0927d03c92836f6b42157880f903f3dcfb98fc

--- bibpyt/Execution/E_SUPERV.py
+++ bibpyt/Execution/E_SUPERV.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -317,7 +317,7 @@ class SUPERV:
         """Initialize the environment (language & encoding, paths...)"""
         # import after getting opts as is may change sys.path
         if self.coreopts.get_option('totalview') == 1:
-            from .E_utils import copierBase, lierRepertoire
+            from Execution.E_utils import copierBase, lierRepertoire
             curPID = os.getpid()
             pathOrigine = os.getcwd()
             pathDestination = osp.join(pathOrigine, "tv_" + str(curPID))
@@ -340,8 +340,8 @@ class SUPERV:
     @never_fail
     def Finish(self):
         """Allow to call cleanup functions."""
-        from E_utils import supprimerRepertoire
         if self.coreopts.get_option('totalview') == 1:
+            from Execution.E_utils import supprimerRepertoire
             supprimerRepertoire(os.getcwd())
         # post-run for testcases
         if 'vale_calc' in (self.coreopts.get_option("post") or []):
