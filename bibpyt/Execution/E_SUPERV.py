a36cdb20302a3942078411762a467a9e60e4fb0b
code_aster <code-aster@edf.fr>
2020-06-09 15:55:59 +0200

    [admin] fix unsafe import
    Former-commit-id: a99ccf5c7dca8339ec56cceb05ff83ed48874872

--- bibpyt/Execution/E_SUPERV.py
+++ bibpyt/Execution/E_SUPERV.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -340,7 +340,7 @@ class SUPERV:
     @never_fail
     def Finish(self):
         """Allow to call cleanup functions."""
-        from E_utils import supprimerRepertoire
+        from .E_utils import supprimerRepertoire
         if self.coreopts.get_option('totalview') == 1:
             supprimerRepertoire(os.getcwd())
         # post-run for testcases
