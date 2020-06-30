6c50bf2238f3d7d3d0b66fbbf4ce6411c6a7a5fb
Micka?l Abbas <mickael.abbas@edf.fr>
2020-06-30 08:18:14 +0200

    [#28593] Treatment of TE for MEMBRANE elements
    Former-commit-id: 8519b3dfdabe121857caa87a7271c89e9fa4c142

--- bibfor/te/te0435.F90
+++ bibfor/te/te0435.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2020 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -15,9 +15,13 @@
 ! You should have received a copy of the GNU General Public License
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
-
+!
 subroutine te0435(option, nomte)
-    implicit none
+!
+use Behaviour_module, only : behaviourOption
+!
+implicit none
+!
 #include "asterf_types.h"
 #include "asterfort/assert.h"
 #include "jeveux.h"
@@ -37,23 +41,26 @@ subroutine te0435(option, nomte)
 #include "asterfort/utmess.h"
 #include "asterfort/verift.h"
 #include "blas/dcopy.h"
+#include "asterfort/Behaviour_type.h"
 !
-    character(len=16) :: option, nomte
-! ----------------------------------------------------------------------
-!    - FONCTION REALISEE:  CALCUL DES OPTIONS DE COMPORTEMENT :
-!                                  - FULL_MECA
-!                                  - FULL_MECA_ELAS
-!                                  - RAPH_MECA
-!                                  - RIGI_MECA
-!                                  - RIGI_MECA_ELAS
-!                                  - RIGI_MECA_TANG
-!                                  - RIGI_MECA_IMPLEX
-!                                  - RIGI_MECA_PRSU_R
-!                          POUR LES MEMBRANES
-!    - ARGUMENTS :
-!        DONNEES :      OPTION       -->  OPTION DE CALCUL
-!                       NOMTE        -->  NOM DU TYPE ELEMENT
-! ----------------------------------------------------------------------
+character(len=16), intent(in) :: option, nomte
+!
+! --------------------------------------------------------------------------------------------------
+!
+! Elementary computation
+!
+! Elements: GRILLE_MEMBRANE / GRILLE_EXCENTRE
+!
+! Options: FULL_MECA_*, RIGI_MECA_*, RAPH_MECA
+!          RIGI_MECA
+!          RIGI_MECA_PRSU_R
+!
+! --------------------------------------------------------------------------------------------------
+!
+! In  option           : name of option to compute
+! In  nomte            : type of finite element
+!
+! --------------------------------------------------------------------------------------------------
 !
     character(len=4) :: fami
     integer :: nddl, nno, nnos, npg, ndim, ncomp, nvari
@@ -63,20 +70,18 @@ subroutine te0435(option, nomte)
     integer :: iinstm, iinstp, icontm, ideplm, ideplp, ivarim, ivarix
     integer :: ivectu, icontp, ivarip, jcret, imatuu, imatun, icontx, i_pres
     real(kind=8) :: dff(2, 9), alpha, beta, h, preten
-    aster_logical :: vecteu, matric, statnonline, pttdef, grddef
+    aster_logical :: lNonLine, lLine
+    aster_logical :: pttdef, grddef
+    aster_logical :: lVect, lMatr, lVari, lSigm
+    character(len=16) :: defo_comp, rela_comp
 !
+! --------------------------------------------------------------------------------------------------
 !
-! -----------------------------------------------------------------
-! ---              INITIALISATION DES VARIABLES                 ---
-! -----------------------------------------------------------------
-!
-! - BOOLEEN UTILES
-!
-    vecteu = ((option(1:9).eq.'FULL_MECA').or. (option .eq.'RAPH_MECA'))
-    matric = ((option(1:9).eq.'FULL_MECA').or. (option(1:9).eq.'RIGI_MECA'))
-
-    statnonline = ((option(1:9) .eq.'FULL_MECA').or. (option .eq.'RAPH_MECA')&
-    .or. ((option(1:10).eq.'RIGI_MECA_').and.(option .ne. 'RIGI_MECA_PRSU_R')))
+    lNonLine = (option(1:9).eq.'FULL_MECA').or.&
+               (option.eq.'RAPH_MECA').or.&
+               (option(1:10).eq.'RIGI_MECA_') .and. option .ne. 'RIGI_MECA_PRSU_R'
+    lLine    = option .eq. 'RIGI_MECA'
+    cod      = 0
 !
 ! - NOMBRE DE COMPOSANTES DES TENSEURS
 !
@@ -89,18 +94,18 @@ subroutine te0435(option, nomte)
     call elrefe_info(fami='RIGI', ndim=ndim, nno=nno, nnos=nnos, npg=npg,&
                      jpoids=ipoids, jvf=ivf, jdfde=idfde, jgano=jgano)
 !
-! - PARAMETRES EN ENTREE
+! - Get input fields
 !
+    lVect = ASTER_FALSE
+    lVari = ASTER_FALSE
+    lSigm = ASTER_FALSE
+    lMatr = ASTER_FALSE
     call jevech('PGEOMER', 'L', igeom)
-
-! - ON UTILISE PCACOQU ET PMATERC POUR TOUT EXCEPTE LA PRESSION SUIVEUSE
     if (option .ne. 'RIGI_MECA_PRSU_R') then
         call jevech('PCACOQU', 'L', icacoq)
         call jevech('PMATERC', 'L', imate)
     endif
-!
-! - PARAMETRES UTILISE DANS LA COMMANDE STAT_NON_LINE
-    if (statnonline) then
+    if (lNonLine) then
         call jevech('PCOMPOR', 'L', icompo)
         call jevech('PCARCRI', 'L', icarcr)
         call jevech('PINSTMR', 'L', iinstm)
@@ -112,10 +117,29 @@ subroutine te0435(option, nomte)
         call jevech('PVARIMR', 'L', ivarim)
         call jevech('PVARIMP', 'L', ivarix)
     endif
-
     if (option .ne. 'RIGI_MECA') then
         call jevech('PDEPLMR', 'L', ideplm)
         call jevech('PDEPLPR', 'L', ideplp)
+    endif
+!
+    if (lNonLine) then
+! ----- Select objects to construct from option name
+        call behaviourOption(option, zk16(icompo),&
+                             lMatr , lVect ,&
+                             lVari , lSigm)
+! ----- Properties of behaviour
+        rela_comp = zk16(icompo-1+RELA_NAME)
+        defo_comp = zk16(icompo-1+DEFO)
+        pttdef = (defo_comp.eq.'PETIT')
+        grddef = (defo_comp.eq.'GROT_GDEP')
+        if (.not.pttdef .and. .not. grddef) then
+            call utmess('F', 'MEMBRANE_2', sk=defo_comp)
+        endif
+    endif
+    if (lLine) then
+        pttdef = ASTER_TRUE
+        grddef = ASTER_FALSE
+        lMatr  = ASTER_TRUE
     endif
 
 ! - PARAMETRES NECESSAIRE AU CALCUL DE LA MATRICE DE RIGITE POUR PRESSION SUIVEUSE
@@ -123,41 +147,29 @@ subroutine te0435(option, nomte)
         call jevecd('PPRESSR', i_pres, 0.d0)
     endif
 !
-! - PARAMETRES EN SORTIE
+! - Get output fields
 !
-    if ((option(1:9).eq.'RAPH_MECA') .or. (option(1:9).eq.'FULL_MECA')) then
+    if (lVect) then
         call jevech('PVECTUR', 'E', ivectu)
+    endif
+    if (lSigm) then
         call jevech('PCONTPR', 'E', icontp)
-        call jevech('PVARIPR', 'E', ivarip)
         call jevech('PCODRET', 'E', jcret)
-! ---   ESTIMATION VARIABLES INTERNES A L'ITERATION PRECEDENTE
+    endif
+    if (lVari) then
+        call jevech('PVARIPR', 'E', ivarip)
         call dcopy(npg*nvari, zr(ivarix), 1, zr(ivarip), 1)
     endif
-!
-
-    if ((option(1:9) .eq.'FULL_MECA').or.((option(1:9).eq.'RIGI_MECA').and.&
-        (option .ne. 'RIGI_MECA_PRSU_R'))) then
+    if (lMatr) then
         call jevech('PMATUUR', 'E', imatuu)
     endif
-
     if (option .eq. 'RIGI_MECA_PRSU_R') then
         call jevech('PMATUNS', 'E', imatun)
     endif
-
-!
-! - PARAMETRES EN SORTIE SUPPLEMENTAIRE POUR LA METHODE IMPLEX
     if (option .eq. 'RIGI_MECA_IMPLEX') then
         call jevech('PCONTXR', 'E', icontx)
-! ------ INITIALISATION DE LA CONTRAINTE INTERPOLE CONTX=CONTM
         call dcopy(npg*ncomp, zr(icontm), 1, zr(icontx), 1)
     endif
-!
-!
-! - INITIALISATION CODES RETOURS
-!
-    do kpg = 1, npg
-        cod(kpg)=0
-    end do
 !
 !
 ! -----------------------------------------------------------------
@@ -179,79 +191,41 @@ subroutine te0435(option, nomte)
         preten = zr(icacoq+3)/h
     endif
 !
-! -----------------------------------------------------------------
-! ---       DEBUT DE LA BOUCLE SUR LES POINTS DE GAUSS          ---
-! -----------------------------------------------------------------
-!
     do kpg = 1, npg
-!
-! --- MISE SOUS FORME DE TABLEAU DES VALEURS ET DES DERIVEES
-!     DES FONCTIONS DE FORME
-!
         do n = 1, nno
             dff(1,n)=zr(idfde+(kpg-1)*nno*2+(n-1)*2)
             dff(2,n)=zr(idfde+(kpg-1)*nno*2+(n-1)*2+1)
         end do
-!
-! ---   ON DISTINGUE LES PETITS ET GRANDS DEFORMATIONS
-! ---   ON FAIT LE CAS DE LA PRESSION A PART
-!
-
-        if (option .ne. 'RIGI_MECA_PRSU_R') then
-
-                if (statnonline) then
-                    pttdef = (zk16(icompo + 2).eq.'PETIT')
-                    grddef = (zk16(icompo + 2).eq.'GROT_GDEP')
-! ---------         AUTRE MESURE DE DEFORMATION
-                    if ((zk16(icompo + 2) .ne. 'GROT_GDEP').and. &
-                        (zk16(icompo + 2) .ne. 'PETIT')) then
-                        call utmess('F', 'MEMBRANE_2', sk=zk16(icompo+2))
-                    endif
-                elseif (option.eq.'RIGI_MECA') then
-                    pttdef = .true.
-                    grddef = .false.
-                else
-                    ASSERT(.false.)
-                endif
-
-
-!
-! ------       SEPARATION DES APPELS POUR LES GRANDES ET PETITES DEFORMATIONS ET LA PRESSION
-!
-               if (pttdef) then
-
-                    call mbxnlr(option,fami,nddl,nno,ncomp,kpg,ipoids,igeom,&
-                          imate,ideplm,ideplp,ivectu,icontp,&
-                          imatuu,dff,alpha,beta,&
-                          vecteu,matric)
-
-                elseif (grddef) then
-
-                    if (zk16 ( icompo ) ( 1 : 14 ) .eq. 'ELAS_MEMBRANE_') then
-
-                        if ((abs(alpha).gt.r8prem()) .or. (abs(beta).gt.r8prem())) then
-                            call utmess('A', 'MEMBRANE_6')
-                        endif
-
-                        call mbgnlr(option,vecteu,matric,nno,ncomp,imate,icompo,dff,alpha,beta,&
-                        h,preten,igeom,ideplm,ideplp,kpg,fami,ipoids,icontp,ivectu,imatuu)
-
-                    else
-                        call utmess('F', 'MEMBRANE_3')
-                    endif
-                endif
-
-        else
-                call nmprmb_matr(nno, npg, kpg, zr(ipoids+kpg), zr(ivf), dff,&
+        if (option .eq. 'RIGI_MECA_PRSU_R') then
+            call nmprmb_matr(nno, npg, kpg, zr(ipoids+kpg), zr(ivf), dff,&
                              igeom,ideplm,ideplp,i_pres, imatun)
+        else
+            if (pttdef) then
+                call mbxnlr(option, fami  ,&
+                            nddl  , nno   , ncomp , kpg   ,&
+                            ipoids, igeom , imate , ideplm,&
+                            ideplp, ivectu, icontp, imatuu,&
+                            dff   , alpha , beta  ,&
+                            lVect , lMatr)
+            elseif (grddef) then
+                if (rela_comp(1:14 ) .eq. 'ELAS_MEMBRANE_') then
+                    if ((abs(alpha).gt.r8prem()) .or. (abs(beta).gt.r8prem())) then
+                        call utmess('A', 'MEMBRANE_6')
+                    endif
+                    call mbgnlr(option, lVect , lMatr ,&
+                                nno   , ncomp , imate , icompo,&
+                                dff   , alpha , beta  , h     ,&
+                                preten, igeom , ideplm, ideplp,&
+                                kpg   , fami  , ipoids, icontp,&
+                                ivectu, imatuu)
+                else
+                    call utmess('F', 'MEMBRANE_3')
+                endif
+            endif
         endif
-
     end do
 !
-! - FIN DE LA BOUCLE SUR LES POINTS DE GAUSS
-!
-
-    if ((option(1:9).eq.'FULL_MECA') .or. (option(1:9).eq.'RAPH_MECA')) then
+    if (lSigm) then
         call codere(cod, npg, zi(jcret))
     endif
 !
--- catalo/cataelem/Elements/meca_membrane.py
+++ catalo/cataelem/Elements/meca_membrane.py
@@ -415,7 +415,10 @@ class MEMBTR3(Element):
                      (SP.PVARCMR, LC.ZVARCPG), (OP.RIGI_MECA_TANG.PVARCPR, LC.ZVARCPG),
                      (SP.PVARCRR, LC.ZVARCPG), (SP.PVARIMP, ZVARIPG),
                      (OP.RIGI_MECA_TANG.PVARIMR, ZVARIPG), ),
-            para_out=((SP.PMATUNS, MMATUNS), (SP.PMATUUR, MMATUUR), ),
+            para_out=((SP.PMATUNS, MMATUNS), (SP.PMATUUR, MMATUUR),
+                      (SP.PVECTUR, MVECTUR), (OP.RIGI_MECA_TANG.PCONTPR, ECONTPG),
+                      (SP.PCOPRED, LC.ECODRET), (SP.PCODRET, LC.ECODRET),
+                     ),
         ),
 
         OP.SIEF_ELGA(te=436,
