4f04e88d7c74217d99a13644f623a4c4b046c3cd
code_aster <code-aster@edf.fr>
2020-11-09 16:13:27 +0100

    [admin] increase memory addition on Eole
    Former-commit-id: 4b68551e2413ec0c97f2e9b7a5712015fa2c2eb5

--- env.d/eole_mpi.sh
+++ env.d/eole_mpi.sh
@@ -36,7 +36,7 @@ export PATH=${PREREQ_PATH}/prerequisites/Python-365/bin:${PATH}
 export LD_LIBRARY_PATH=${PREREQ_PATH}/prerequisites/Python-365/lib:${LD_LIBRARY_PATH}
 
 # custom configuration
-export CONFIG_PARAMETERS_addmem=2500
+export CONFIG_PARAMETERS_addmem=2800
 
 # prerequisites paths
 export PYPATH_NUMPY="${PREREQ_PATH}/prerequisites/Numpy-1151/lib/python3.6/site-packages"
