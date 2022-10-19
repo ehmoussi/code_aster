be2e4ea615681323f35a4714fead82c28baa6537
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 22:23:30 +0000

    Adding tarfile member sanitization to extractall()

--- code_aster/MacroCommands/macr_adap_mail_ops.py
+++ code_aster/MacroCommands/macr_adap_mail_ops.py
@@ -227,7 +227,26 @@ class HomardInfo:
         # Remarque : a partir de python 2.5 on pourra utiliser extractall
         #
             with tarfile.open(fichier_archive, "r") as tar:
-                tar.extractall()
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar)
 
             if INFO >= 3:
                 print(os.listdir(Rep_Calc_ASTER))
